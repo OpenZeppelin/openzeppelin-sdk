4f483e459838e3b6b70546f43cc1cd170f46f4f4
Alejandro Santander <Palebluedot@gmail.com>
2018-04-19 17:51:04 -0300

    Add test for upgrade-proxy command

---
+++ packages/cli/test/scripts/upgrade-proxy.test.js
@@ -0,0 +1,49 @@
+import init from "../../src/scripts/init.js";
+import addImplementation from "../../src/scripts/add-implementation.js";
+import sync from "../../src/scripts/sync.js";
+import newVersion from "../../src/scripts/new-version.js";
+import createProxy from "../../src/scripts/create-proxy.js";
+import upgradeProxy from "../../src/scripts/upgrade-proxy.js";
+import fs from 'fs';
+import PackageFilesInterface from '../../src/utils/PackageFilesInterface';
+
+const should = require('chai')
+      .use(require('chai-as-promised'))
+      .should();
+
+contract.only('create-proxy command', function([_, owner]) {
+
+  const from = owner;
+  const appName = "MyApp";
+  const contractName = "ImplV1";
+  const contractAlias = "Impl";
+  const defaultVersion = "0.1.0";
+  const version = "0.2.0";
+  const packageFileName = "package.test.zos.json";
+  const network = "test";
+  const networkPackageFileName = `package.zos.${network}.json`;
+  const files = new PackageFilesInterface(packageFileName);
+
+  beforeEach('setup', async function() {
+    await init(appName, defaultVersion, {packageFileName});
+    await addImplementation(contractName, contractAlias, {packageFileName});
+    await sync({ packageFileName, network, from });
+    await createProxy(contractAlias, {packageFileName, network, from});
+    await newVersion(version, {packageFileName});
+  });
+
+  afterEach('cleanup', function() {
+    console.log('cleaning up files...');
+    fs.unlinkSync(packageFileName);
+    fs.unlinkSync(networkPackageFileName);
+  });
+
+  it('should upgrade the version of a proxy', async function() {
+    let data = files.readNetworkFile(network);
+    let proxy = data.proxies[contractAlias][0];
+    await upgradeProxy(proxy.address, contractAlias {network, packageFileName, from});
+    data = files.readNetworkFile(network);
+    proxy = data.proxies[contractAlias][0];
+    proxy.version.should.eq(version);
+  });
+});
