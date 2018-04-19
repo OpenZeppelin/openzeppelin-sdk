67419945b5ce09f6de62ace7931d4f30f37c5d24
Alejandro Santander <Palebluedot@gmail.com>
2018-04-19 15:44:10 -0300

    Working add-implementation command tests

--- packages/cli/test/scripts/add-implementation.test.js
+++ packages/cli/test/scripts/add-implementation.test.js
@@ -22,9 +22,40 @@ contract.only('add-implementation', function(accounts) {
   });
 
   it('should add an implementation with an alias and a filename', function() {
-    addImplementation(contractName, contractAlias, [], {packageFileName});
+    addImplementation(contractName, contractAlias, {packageFileName});
     const data = files.read();
-    console.log(data);
     data.contracts[contractAlias].should.eq(contractName);
   });
+
+  it('should allow to change an existing implementation', function() {
+    addImplementation(contractName, contractAlias, {packageFileName});
+    const customFileName = "MyContract_v1.sol";
+    addImplementation(customFileName, contractAlias, {packageFileName});
+    const data = files.read();
+    data.contracts[contractAlias].should.eq(customFileName);
+  });
+
+  it('should handle multiple contracts', function() {
+    const customAlias1 = "MyContract";
+    const customFileName1 = "MyContract_v2.sol";
+    const customAlias2 = "MyOtherContract";
+    const customFileName2 = "MyOtherContract_v0.sol";
+    addImplementation(customFileName1, customAlias1, {packageFileName});
+    addImplementation(customFileName2, customAlias2, {packageFileName});
+    const data = files.read();
+    data.contracts[customAlias1].should.eq(customFileName1);
+    data.contracts[customAlias2].should.eq(customFileName2);
+  });
+
+  // TODO: implement
+  it.skip('should use a default alias if one is not provided', function() {
+    addImplementation(contractName, null, {packageFileName});
+    const data = files.read();
+    const expectedAlias = contractName.split('.')[0];
+    console.log(data);
+    data.contracts[expectedAlias].should.eq(contractName);
+  });
+
+  // TODO: test for invalid alias names
+  // TODO: test for invalid solidity file names
 });
