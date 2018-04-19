a87d604118ccf14c09fffc6c164cb72cce3330df
Santiago Palladino <spalladino@gmail.com>
2018-04-19 12:02:49 -0300

    Move scripts to src and executable to bin

--- packages/cli/bin/zos-cli.js
+++ packages/cli/bin/zos-cli.js
@@ -4,7 +4,7 @@ const shelljs = require('shelljs')
 
 function run() {
   const node = 'npx babel-node'
-  const script = `./scripts/${process.argv[2]}.js`
+  const script = `../src/scripts/${process.argv[2]}.js`
   const args = process.argv.slice(3).join(' ')
   const command = `${node} ${script} ${args}`
 
