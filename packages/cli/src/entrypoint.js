7f788243ade10435733216e9f6449c41cab695ec
Santiago Palladino <spalladino@gmail.com>
2018-04-20 15:39:07 -0300

    Init args should be an array

--- packages/cli/src/entrypoint.js
+++ packages/cli/src/entrypoint.js
@@ -9,14 +9,14 @@ module.exports = function(cb) {
     .option('-f, --from <from>', 'Sender')
     .option('-n, --network <network>', 'Truffle network')
     .option('-s, --stdlib <stdlib>', 'Standard library to use')
-    .option('-i, --init <init>', 'Initialize arguments')
+    .option('-i, --init <arg1,arg2,...>', 'Initialize arguments')
     .parse(process.argv)
 
   const script = `./scripts/${program.args[2]}.js`
   const args = program.args.slice(3)
   require(script)(...args, {
     network: program.network,
-    initArgs: program.init,
+    initArgs: program.init.split(","),
     from: program.from || web3.eth.accounts[0],
     stdlib: program.stdlib
   }).then(cb).catch(cb)
