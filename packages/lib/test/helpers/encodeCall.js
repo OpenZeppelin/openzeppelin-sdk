3e2e7294988eb46e4dbcabd2d0b97fc7f5c8a865
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-04-20 12:11:35 -0300

    Improve encodecall to work with big numbers

--- packages/lib/test/helpers/encodeCall.js
+++ packages/lib/test/helpers/encodeCall.js
@@ -1,6 +1,7 @@
 const abi = require('ethereumjs-abi')
 
-function encodeCall(name, arguments = [], values = []) {
+function encodeCall(name, arguments = [], rawValues = []) {
+  const values = rawValues.map(value => value.toString()) // convert BigNumbers to string
   const methodId = abi.methodID(name, arguments).toString('hex');
   const params = abi.rawEncode(arguments, values).toString('hex');
   return '0x' + methodId + params;
