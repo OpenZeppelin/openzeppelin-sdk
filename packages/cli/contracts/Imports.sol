5437900c24dcb9ef54609bd230fb226173f448ad
Martín Triay <martriay@gmail.com>
2018-04-20 16:32:37 -0300

    Fix import error

--- packages/cli/contracts/Imports.sol
+++ packages/cli/contracts/Imports.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.21;
 
+import "zos-kernel/contracts/Release.sol";
 import "zos-lib/contracts/application/versioning/Package.sol";
 import "zos-lib/contracts/application/versioning/ContractDirectory.sol";
 import "zos-lib/contracts/application/management/AppDirectory.sol";
