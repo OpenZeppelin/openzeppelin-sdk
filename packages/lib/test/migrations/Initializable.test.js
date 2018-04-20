2299aea7297e61a2e0c9ac9a726c51308b0f1edf
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-04-20 13:03:10 -0300

    Add initializable test

--- packages/lib/test/migrations/Initializable.test.js
+++ packages/lib/test/migrations/Initializable.test.js
@@ -6,15 +6,25 @@ contract('Initializable', ([]) => {
     this.initializable = await InitializableMock.new()
   })
 
-  describe('initialize', function () {
-    it('can be called once', async function () {
-      await this.initializable.initialize(42)
-      const x = await this.initializable.x()
+  it('starts uninitialized', async function () {
+    const isInitialized = await this.initializable.isInitialized()
+    assert.isFalse(isInitialized)
+  })
 
+  describe('initialize', function () {
+    beforeEach(async function () {
+      await this.initializable.initialize(42)
+    })
+
+    it('can be called only once', async function () {
+      const x = await this.initializable.x()
       assert.equal(x, 42)
       await assertRevert(this.initializable.initialize(100))
     })
 
-    it('becomes initialized')
+    it('becomes initialized', async function () {
+      const isInitialized = await this.initializable.isInitialized()
+      assert.isTrue(isInitialized)
+    })
   })
 })
