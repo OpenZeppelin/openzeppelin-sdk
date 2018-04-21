27bcf1a4308f6f56ecfb4e317ebf9c919e2e544a
Francisco Giordano <frangio.1@gmail.com>
2018-04-20 23:46:51 -0300

    add Initializable back

---
+++ packages/lib/contracts/migrations/Initializable.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.21;
+
+contract Initializable {
+  bool private _initialized;
+
+  modifier isInitializer() {
+    require(!_initialized);
+    _;
+    _initialized = true;
+  }
+
+  function isInitialized() public view returns (bool) {
+    return _initialized;
+  }
+}
