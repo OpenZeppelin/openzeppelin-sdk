acfb5ebe02e065babd0cf40ed03820fbdc17995a
Francisco Giordano <frangio.1@gmail.com>
2018-04-26 13:13:46 -0300

    Merge pull request zeppelinos/zos-lib#73 from zeppelinos/fix-decodeLogs-1
    Fix decodeLogs when logs include events from other contracts

--- packages/lib/lib/decodeLogs.js
+++ packages/lib/lib/decodeLogs.js
@@ -1,10 +1,17 @@
 const SolidityEvent = require('web3/lib/web3/event.js');
 
 function decodeLogs (logs, contract) {
-  return logs.map(log => {
-    const event = new SolidityEvent(null, contract.events[log.topics[0]], '0x0');
-    return event.decode(log);
-  });
+  const decodedLogs = [];
+  
+  for (const log of logs) {
+    const eventName = log.topics[0];
+    if (eventName in contract.events) {
+      const eventType = new SolidityEvent(null, contract.events[eventName], '0x0');
+      decodedLogs.push(eventType.decode(log));
+    }
+  }
+        
+  return decodedLogs;
 }
 
 module.exports = decodeLogs
