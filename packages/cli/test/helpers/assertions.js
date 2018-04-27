2a5b9e6731e54514e91fea72ad2caed07b47fde0
Santiago Palladino <spalladino@gmail.com>
2018-04-27 19:03:53 -0300

    Add custom assertion for non zero addresses

---
+++ packages/cli/test/helpers/assertions.js
@@ -0,0 +1,13 @@
+module.exports = function (chai, utils) {
+  const Assertion = chai.Assertion
+  Assertion.addProperty('nonzeroAddress', function () {
+    this.assert(
+        this._obj 
+          && this._obj.length === 42 
+          && this._obj.startsWith('0x') 
+          && this._obj !== '0x0000000000000000000000000000000000000000'
+      , 'expected #{this} to be a non-zero address'
+      , 'expected #{this} to not be a non-zero address'
+    );
+  });
+};
