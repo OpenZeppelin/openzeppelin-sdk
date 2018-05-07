f589c0c2ee71759a24fd08d366a2ef3b65ceb202
Francisco Giordano <frangio.1@gmail.com>
2018-05-07 16:53:05 -0300

    Merge pull request zeppelinos/zos-lib#78 from elopio/docs/upgradeability-assembly
    docs: explain the upgradeability assembly code

--- packages/lib/contracts/upgradeability/Proxy.sol
+++ packages/lib/contracts/upgradeability/Proxy.sol
@@ -19,12 +19,18 @@ contract Proxy {
     address _impl = implementation();
 
     assembly {
+      // 0x40 contains the value for the next available free memory pointer.
       let ptr := mload(0x40)
+      // Copy msg.data.
       calldatacopy(ptr, 0, calldatasize)
+      // Call the implementation.
+      // out and outsize are 0 because we don't know the size yet.
       let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+      // Copy the returned data.
       returndatacopy(ptr, 0, returndatasize)
 
       switch result
+      // delegatecall returns 0 on error.
       case 0 { revert(ptr, returndatasize) }
       default { return(ptr, returndatasize) }
     }
