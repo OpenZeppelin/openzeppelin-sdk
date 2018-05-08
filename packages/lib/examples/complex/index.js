6b1a23ae5eb07379e8b2203c3846912765a1cca3
ajs <palebluedot@gmail.com>
2018-05-08 11:49:42 -0300

    Add zos-lib dependency and update openzeppelin-zos dependency

--- packages/lib/examples/complex/index.js
+++ packages/lib/examples/complex/index.js
@@ -13,19 +13,19 @@ const txParams = {
 module.exports = async function setupAppManager() {
 
   // On-chain, single entry point of the entire application.
-  log("<< Setting up AppManager >>")
+  log.info("<< Setting up AppManager >>")
   const initialVersion = '0.0.1'
   const appManager = AppManagerDeployer.call(initialVersion, txParams)
 
   // Register the first implementation of "Basil", and request a proxy for it.
-  log("\n<< Deploying version 1 >>")
+  log.info("\n<< Deploying version 1 >>")
   const DonationsV1 = ContractsProvider.getByName('DonationsV1')
   await appManager.setImplementation(DonationsV1, contractName);
   await appManager.createProxy(DonationsV1, contractName, 'initialize', [owner])
 
   // Create a new version of the app, liked to the zeppelin_os standard library.
   // Register a new implementation for "Basil" and upgrade it's proxy to use the new implementation.
-  log("\n<< Deploying version 2 >>")
+  log.info("\n<< Deploying version 2 >>")
   const secondVersion = '0.0.2'
   await appManager.newVersion(secondVersion, stdlib)
   const DonationsV2 = ContractsProvider.getByName('DonationsV2')
@@ -34,13 +34,13 @@ module.exports = async function setupAppManager() {
 
   // Add an ERC721 token implementation to the project, request a proxy for it,
   // and set the token on "Basil".
-  log(`Creating ERC721 token proxy to use in ${contractName}...`)
+  log.info(`Creating ERC721 token proxy to use in ${contractName}...`)
   const {receipt} = await this.appManager.create('MintableERC721Token', txParams)
   const UpgradeabilityProxyFactory = ContractsProvider.getByName('UpgradeabilityProxyFactory')
   const logs = decodeLogs([receipt.logs[1]], UpgradeabilityProxyFactory)
   const proxyAddress = logs.find(l => l.event === 'ProxyCreated').args.proxy
-  log(`Token proxy created at ${proxyAddress}`)
-  log("Setting application's token...")
+  log.info(`Token proxy created at ${proxyAddress}`)
+  log.info("Setting application's token...")
   await donations.setToken(proxyAddress, txParams)
-  log("Token set succesfully")
+  log.info("Token set succesfully")
 }
