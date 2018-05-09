9a8decce30fd3241d2b9e5f79ddf2c8a1e23f9ad
Leo Arias <yo@elopio.net>
2018-05-09 16:02:13 +0000

    tests: add test for the revert of UpgradeabilityProxyFactory.createProxyAndCall

--- packages/lib/test/upgradeability/UpgradeabilityProxyFactory.test.js
+++ packages/lib/test/upgradeability/UpgradeabilityProxyFactory.test.js
@@ -1,4 +1,5 @@
 import encodeCall from '../../src/helpers/encodeCall'
+import assertRevert from '../../src/helpers/assertRevert'
 
 const MigratableMock = artifacts.require('MigratableMock')
 const OwnedUpgradeabilityProxy = artifacts.require('OwnedUpgradeabilityProxy')
@@ -43,45 +44,59 @@ contract('UpgradeabilityProxyFactory', ([_, owner]) => {
     const value = 1e5
     const initializeData = encodeCall('initialize', ['uint256'], [42])
 
-    beforeEach(async function () {
-      this.behavior = await MigratableMock.new()
-      const { logs } = await this.factory.createProxyAndCall(owner, this.behavior.address, initializeData, { value })
-      this.logs = logs
-      this.proxyAddress = logs.find(l => l.event === 'ProxyCreated').args.proxy
-      this.proxy = await OwnedUpgradeabilityProxy.at(this.proxyAddress)
+    context('when it fails', function () {
+      beforeEach(async function () {
+        this.behavior = await MigratableMock.new()
+      })
+
+      it('should revert if function reverts', async function () {
+        const failFunction = encodeCall('fail')
+        await assertRevert(this.factory.createProxyAndCall(owner, this.behavior.address, failFunction))
+      })
     })
 
-    it('creates a proxy pointing to the requested implementation', async function () {
-      const implementation = await this.proxy.implementation()
-      assert.equal(implementation, this.behavior.address)
-    })
+    context('when it succeeds', function () {
 
-    it('transfers the ownership to the requested owner', async function () {
-      const proxyOwner = await this.proxy.proxyOwner()
-      assert.equal(proxyOwner, owner)
-    })
+      beforeEach(async function () {
+        this.behavior = await MigratableMock.new()
+        const { logs } = await this.factory.createProxyAndCall(owner, this.behavior.address, initializeData, { value })
+        this.logs = logs
+        this.proxyAddress = logs.find(l => l.event === 'ProxyCreated').args.proxy
+        this.proxy = await OwnedUpgradeabilityProxy.at(this.proxyAddress)
+      })
 
-    it('emits an event', async function () {
-      assert.equal(this.logs.length, 1)
-      assert.equal(this.logs[0].event, 'ProxyCreated')
-      assert.equal(this.logs[0].args.proxy, this.proxyAddress)
-    })
+      it('creates a proxy pointing to the requested implementation', async function () {
+        const implementation = await this.proxy.implementation()
+        assert.equal(implementation, this.behavior.address)
+      })
 
-    it('calls "initialize" function', async function() {
-      const migratable = MigratableMock.at(this.proxyAddress);
-      const x = await migratable.x();
-      assert.equal(x, 42);
-    })
+      it('transfers the ownership to the requested owner', async function () {
+        const proxyOwner = await this.proxy.proxyOwner()
+        assert.equal(proxyOwner, owner)
+      })
 
-    it('sends given value to the delegated implementation', async function() {
-      const balance = await web3.eth.getBalance(this.proxyAddress)
-      assert(balance.eq(value))
-    })
+      it('emits an event', async function () {
+        assert.equal(this.logs.length, 1)
+        assert.equal(this.logs[0].event, 'ProxyCreated')
+        assert.equal(this.logs[0].args.proxy, this.proxyAddress)
+      })
 
-    it('uses the storage of the proxy', async function () {
-      // fetch the x value of Initializable at position 0 of the storage
-      const storedValue = await web3.eth.getStorageAt(this.proxyAddress, 1);
-      assert.equal(storedValue, 42);
+      it('calls "initialize" function', async function() {
+        const migratable = MigratableMock.at(this.proxyAddress);
+        const x = await migratable.x();
+        assert.equal(x, 42);
+      })
+
+      it('sends given value to the delegated implementation', async function() {
+        const balance = await web3.eth.getBalance(this.proxyAddress)
+        assert(balance.eq(value))
+      })
+
+      it('uses the storage of the proxy', async function () {
+        // fetch the x value of Initializable at position 0 of the storage
+        const storedValue = await web3.eth.getStorageAt(this.proxyAddress, 1);
+        assert.equal(storedValue, 42);
+      })
     })
   })
 })
