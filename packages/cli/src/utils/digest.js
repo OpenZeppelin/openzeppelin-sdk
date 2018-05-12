d0c794f090791284e37ae240c63938e4e2ad936e
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-05-11 21:50:48 -0300

    Split digest function in multiple lines

--- packages/cli/src/utils/digest.js
+++ packages/cli/src/utils/digest.js
@@ -1,5 +1,8 @@
 import crypto from 'crypto';
 
-export function bytecodeDigest(bytecode) {
-  return crypto.createHash('sha256').update(Buffer.from(bytecode.replace(/^0x/, ''), 'hex')).digest('hex');
+export function bytecodeDigest(rawBytecode) {
+  const bytecode = rawBytecode.replace(/^0x/, '')
+  const buffer = Buffer.from(bytecode, 'hex')
+  const hash = crypto.createHash('sha256')
+  return hash.update(buffer).digest('hex')
 }
