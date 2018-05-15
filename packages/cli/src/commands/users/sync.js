8b5c6b4a9bd96de6250f1dc48dacbf96112834c6
Manuel Aráoz <manuelaraoz@gmail.com>
2018-05-15 15:58:52 -0300

    Merge pull request zeppelinos/zos-cli#108 from zeppelinos/compile-on-sync
    Compile contracts before sync

--- packages/cli/src/commands/users/sync.js
+++ packages/cli/src/commands/users/sync.js
@@ -8,15 +8,16 @@ function registerSync(program) {
     .usage('--network <network> [options]')
     .option('-f, --from <from>', 'Set the transactions sender')
     .option('-n, --network <network>', 'Provide a network to be used')
+    .option('--skip-compile', 'Skips contract compilation')
     .option('--deploy-stdlib', 'Deploys a copy of the stdlib (if any) instead of using the one already published to the network by its author (useful in local testrpc networks)')
     .option('--reupload', 'Reuploads all contracts, regardless of not having been modified')
     .action(action)
 }
 
 function action(options) {
-  const { from, network, deployStdlib, reupload } = options
+  const { from, network, skipCompile, deployStdlib, reupload } = options
   const txParams = from ? { from } : {}
-  runWithTruffle(async () => await sync({ network, deployStdlib, reupload, txParams }), network)
+  runWithTruffle(async () => await sync({ network, deployStdlib, reupload, txParams }), network, ! skipCompile)
 }
 
 module.exports = registerSync
--- packages/cli/src/utils/runWithTruffle.js
+++ packages/cli/src/utils/runWithTruffle.js
@@ -1,12 +1,16 @@
 import Web3 from 'web3'
 import TruffleConfig from 'truffle-config'
+import TruffleCompile from 'truffle-workflow-compile'
 import TruffleEnvironment from 'truffle-core/lib/environment'
 import ContractsProvider from './ContractsProvider'
+import { Logger } from 'zos-lib'
+
+const log = new Logger('RunWithTruffle')
+const options = { logger: console }
+const config = TruffleConfig.detect(options)
 
 function initTruffle(network) {
   if(!network) throw Error('A network name must be provided to execute the requested action.')
-  const options = { logger: console }
-  const config = TruffleConfig.detect(options)
   config.network = network
   return new Promise((resolve, reject) => {
     TruffleEnvironment.detect(config, function (error) {
@@ -19,6 +23,17 @@ function initTruffle(network) {
   });
 }
 
-export default function runWithTruffle(script, network) {
+function compileWithTruffle() {
+  log.info("Compiling contracts with truffle")
+
+  const truffleCallback = (err, abstractions, paths) => {
+    if (err) log.error(err)
+  }
+
+  TruffleCompile.compile(config, truffleCallback)
+}
+
+export default function runWithTruffle(script, network, compile = false) {
+  if (compile) compileWithTruffle()
   initTruffle(network).then(script)
 }
