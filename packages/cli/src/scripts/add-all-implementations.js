06c6db22babb5e82efb7b5c3dc8b4af4682f616b
Santiago Palladino <spalladino@gmail.com>
2018-05-15 12:52:52 -0300

    Fix typo in default function name addAllImplementations

--- packages/cli/src/scripts/add-all-implementations.js
+++ packages/cli/src/scripts/add-all-implementations.js
@@ -1,6 +1,6 @@
 import AppController from "../models/AppController";
 
-export default function addAllImplementation({ packageFileName = undefined }) {
+export default function addAllImplementations({ packageFileName = undefined }) {
   const appController = new AppController(packageFileName)
   appController.addAllImplementations()
   appController.writePackage()
