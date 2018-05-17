2f6cd2888e89bff3038e615d4ed893a712008559
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-05-17 20:16:50 -0300

    Skip mocks from solcover

--- packages/lib/.solcover.js
+++ packages/lib/.solcover.js
@@ -1,4 +1,12 @@
 module.exports = {
   copyPackages: ['zeppelin-solidity'],
-  skipFiles: ['lifecycle/Migrations.sol']
+  skipFiles: [
+    'lifecycle/Migrations.sol',
+    'mocks/ClashingImplementation.sol',
+    'mocks/DummyImplementation.sol',
+    'mocks/InitializableMock.sol',
+    'mocks/MigratableMock.sol',
+    'mocks/MultipleInheritanceMigratableMocks.sol',
+    'mocks/SingleInheritanceMigratableMocks.sol',
+  ]
 };
