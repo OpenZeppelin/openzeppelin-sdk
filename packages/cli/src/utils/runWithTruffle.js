2653e469250b170400ed2426f47cef6763e844fc
Manuel Aráoz <manuelaraoz@gmail.com>
2018-05-17 18:23:30 -0300

    Merge pull request zeppelinos/zos-cli#163 from zeppelinos/feature/improve_zos_helper
    Improve runWithTruffle script

--- packages/cli/src/utils/runWithTruffle.js
+++ packages/cli/src/utils/runWithTruffle.js
@@ -1,19 +1,29 @@
-import Web3 from 'web3'
-import TruffleConfig from 'truffle-config'
-import TruffleCompile from 'truffle-workflow-compile'
-import TruffleEnvironment from 'truffle-core/lib/environment'
 import { Logger } from 'zos-lib'
-
 const log = new Logger('RunWithTruffle')
-const options = { logger: console }
-const config = TruffleConfig.detect(options)
+
+export default function runWithTruffle(script, network, compile = false) {
+  try {
+    require('truffle-config').detect({ logger: console })
+  } catch (error) {
+    throw Error('You have to provide a truffle.js file, please remember to initialize your project running "truffle init".')
+  }
+
+  if (compile) compileWithTruffle()
+  initTruffle(network).then(script)
+}
 
 function initTruffle(network) {
   if(!network) throw Error('A network name must be provided to execute the requested action.')
+
+  const TruffleConfig = require('truffle-config')
+  const config = TruffleConfig.detect({ logger: console })
   config.network = network
+
   return new Promise((resolve, reject) => {
+    const TruffleEnvironment = require('truffle-core/lib/environment')
     TruffleEnvironment.detect(config, function (error) {
       if (error) throw error
+      const Web3 = require('web3')
       global.web3 = new Web3(config.provider)
       global.artifacts = config.resolver
       resolve()
@@ -23,15 +33,10 @@ function initTruffle(network) {
 
 function compileWithTruffle() {
   log.info("Compiling contracts with truffle")
-
   const truffleCallback = (err, abstractions, paths) => {
     if (err) log.error(err)
   }
 
+  const TruffleCompile = require('truffle-workflow-compile')
   TruffleCompile.compile(config, truffleCallback)
-}
-
-export default function runWithTruffle(script, network, compile = false) {
-  if (compile) compileWithTruffle()
-  initTruffle(network).then(script)
 }
