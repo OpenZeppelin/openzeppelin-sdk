811bd474c7689947d07842527e84f5f2285c6cc0
Facundo Spagnuolo <facuspagnuolo@users.noreply.github.com>
2018-05-17 16:30:37 -0300

    Handle unknown commands (zeppelinos/zos-cli#160)

--- packages/cli/src/bin/errors.js
+++ packages/cli/src/bin/errors.js
@@ -3,4 +3,9 @@ import ErrorHandler from '../models/ErrorHandler'
 module.exports = function registerErrorHandler(program) {
   process.on('unhandledRejection', reason => { throw Error(reason); })
   process.on('uncaughtException', error => new ErrorHandler(error, program).call())
+
+  program.on('command:*', function () {
+    console.error(`Invalid command: ${program.args.join(' ')}\nSee --help for a list of available commands.`)
+    process.exit(1)
+  })
 }
