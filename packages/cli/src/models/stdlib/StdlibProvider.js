c7f009606f5b4bfab6b68c5c6271cd0aeb80fd07
Facundo Spagnuolo <facuspagnuolo@users.noreply.github.com>
2018-06-26 22:17:48 -0300

    Merge pull request zeppelinos/zos-cli#270 from zeppelinos/improve_stdlib_provider_error_handling
    Improve StdlibProvider error handling

--- packages/cli/src/models/stdlib/StdlibProvider.js
+++ packages/cli/src/models/stdlib/StdlibProvider.js
@@ -3,6 +3,7 @@ import { FileSystem as fs } from 'zos-lib'
 const StdlibProvider = {
   from(name, version, network) {
     const filename = `node_modules/${name}/zos.${network}.json`
+    if(!fs.exists(filename)) throw Error(`Could not find a zos file for network '${network}' for the requested stdlib ${name}@${version}. Please make sure it is provided or at least self-deployed if you are in development mode.`)
     const networkInfo = fs.parseJson(filename)
     if(version !== networkInfo.version) throw Error(`Requested stdlib version ${version} does not match stdlib network package version ${networkInfo.version}`)
     return networkInfo.provider.address
--- packages/cli/test/models/Stdlib.test.js
+++ packages/cli/test/models/Stdlib.test.js
@@ -4,31 +4,67 @@ require('../setup')
 import Stdlib from '../../src/models/stdlib/Stdlib'
 
 contract('Stdlib', function () {
-  const greeterName = 'Greeter'
   const packageName = 'mock-stdlib'
 
-  beforeEach("initializing stdlib", async function () {
-    this.stdlib = new Stdlib(packageName)
+  describe('fetch', function () {
+    describe('when there is a network file for the requested network', function () {
+      const network = 'test'
+
+      describe('when the given version matches the stdlib package one', function () {
+        const version = '1.1.0'
+
+        it('should return the address of the stdlib', function () {
+          Stdlib.fetch(packageName, version, network).should.be.nonzeroAddress
+        })
+      })
+
+      describe('when the given version does not match the stdlib package one', function () {
+        const version = '2.0.0'
+
+        it('throws an error', function () {
+          expect(() => Stdlib.fetch(packageName, version, network))
+            .to.throw('Requested stdlib version 2.0.0 does not match stdlib network package version 1.1.0')
+        })
+      })
+    })
+
+    describe('when there is no network file for the requested network', function () {
+      const network = 'cosmos'
+      const version = '1.1.0'
+
+      it('throws an error', function () {
+        expect(() => Stdlib.fetch(packageName, version, network))
+          .to.throw('Could not find a zos file for network \'cosmos\' for the requested stdlib mock-stdlib@1.1.0. Please make sure it is provided or at least self-deployed if you are in development mode.')
+      })
+    })
   })
 
-  it('should set a name', async function () {
-    this.stdlib.name.should.eq(packageName)
-  })
-  
-  it('should set a version', async function () {
-    this.stdlib.version.should.eq('1.1.0')
-  })
+  describe('stdlib', function () {
+    const greeterName = 'Greeter'
 
-  it('should list all provided contracts', async function () {
-    Object.keys(this.stdlib.contracts()).should.have.members([greeterName])
-  })
+    beforeEach('initializing stdlib', async function () {
+      this.stdlib = new Stdlib(packageName)
+    })
 
-  it('should tell if it has a contract', async function () {
-    this.stdlib.hasContract(greeterName).should.be.true
-    this.stdlib.hasContract('anotherContract').should.be.false
-  })
+    it('should set a name', async function () {
+      this.stdlib.name.should.eq(packageName)
+    })
 
-  it('should retrieve a contract name', async function () {
-    this.stdlib.contract(greeterName).should.be.equal('GreeterImpl')
+    it('should set a version', async function () {
+      this.stdlib.version.should.eq('1.1.0')
+    })
+
+    it('should list all provided contracts', async function () {
+      Object.keys(this.stdlib.contracts()).should.have.members([greeterName])
+    })
+
+    it('should tell if it has a contract', async function () {
+      this.stdlib.hasContract(greeterName).should.be.true
+      this.stdlib.hasContract('anotherContract').should.be.false
+    })
+
+    it('should retrieve a contract name', async function () {
+      this.stdlib.contract(greeterName).should.be.equal('GreeterImpl')
+    })
   })
 })
