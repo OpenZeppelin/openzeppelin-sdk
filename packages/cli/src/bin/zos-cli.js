d360168a3bb2ed58d50ce824ad36d8c68b79724b
Facundo Spagnuolo <facuspagnuolo@users.noreply.github.com>
2018-07-05 09:56:47 -0300

    Merge pull request zeppelinos/zos-cli#286 from zeppelinos/feature/cli-lock-file
    Ensure no more than one instance of CLI is running simultaneously

--- packages/cli/src/bin/zos-cli.js
+++ packages/cli/src/bin/zos-cli.js
@@ -1,7 +1,17 @@
 #! /usr/bin/env node
 
 import { Logger } from 'zos-lib'
+import { lockSync } from 'lockfile';
 import program from './program'
+
+// Acquire file lock to ensure no other instance is running
+const LOCKFILE = '.zos.lock';
+try { 
+  lockSync(LOCKFILE, { retries: 0 }) 
+} catch (e) { 
+  console.error(`Cannot run more than one instance of 'zos' at the same time.\nIf you are sure that no other instances are actually running, manually remove the file ${LOCKFILE} and try again.`);
+  process.exit(1); 
+}
 
 Logger.silent(false)
 program.parse(process.argv)
