f48202c65bb20aa677378acf78da70ca20d8b54a
Santiago Palladino <spalladino@gmail.com>
2018-07-05 12:42:55 -0300

    Fix error when creating new lib project

--- packages/cli/src/scripts/init-lib.js
+++ packages/cli/src/scripts/init-lib.js
@@ -1,4 +1,5 @@
 import LocalLibController from  '../models/local/LocalLibController'
+import ZosPackageFile from "../models/files/ZosPackageFile"
 
 export default async function initLib({ name, version, force = false, packageFile = new ZosPackageFile() }) {
   if (!name) throw Error('A project name must be provided to initialize the project.')
--- packages/cli/src/scripts/init.js
+++ packages/cli/src/scripts/init.js
@@ -1,5 +1,5 @@
 import LocalAppController from  '../models/local/LocalAppController'
-import ZosPackageFile from "../models/files/ZosPackageFile";
+import ZosPackageFile from "../models/files/ZosPackageFile"
 
 export default async function init({ name, version, stdlibNameVersion = undefined, installLib = false, force = false, packageFile = new ZosPackageFile() }) {
   if (!name) throw Error('A project name must be provided to initialize the project.')
