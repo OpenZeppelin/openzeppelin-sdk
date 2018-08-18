d5f8a4ecdda22d467287546fb645187bfde46110
Santiago Palladino <spalladino@gmail.com>
2018-08-18 20:41:22 -0300

    Rename stdlib to dependency in CLI

--- packages/cli/src/models/dependency/DependencyProvider.js
+++ packages/cli/src/models/dependency/DependencyProvider.js
@@ -1,12 +1,12 @@
 import { FileSystem as fs } from 'zos-lib'
-import Stdlib from './Stdlib';
+import Dependency from './Dependency';
 
-const StdlibProvider = {
+const DependencyProvider = {
   from(name, version, network) {
     const filename = `node_modules/${name}/zos.${network}.json`
     if(!fs.exists(filename)) throw Error(`Could not find a zos file for network '${network}' for the requested stdlib ${name}@${version}. Please make sure it is provided or at least self-deployed if you are in development mode.`)
     const networkInfo = fs.parseJson(filename)
-    Stdlib.validateSatisfiesVersion(networkInfo.version, version)
+    Dependency.validateSatisfiesVersion(networkInfo.version, version)
     return {
       address: networkInfo.provider.address,
       version: networkInfo.version
@@ -14,4 +14,4 @@ const StdlibProvider = {
   }
 }
 
-export default StdlibProvider
+export default DependencyProvider
