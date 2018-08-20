f74d6091e464f18c8e9f1e385cb825a8e22b71bf
Santiago Palladino <spalladino@gmail.com>
2018-08-20 15:49:09 -0300

    Update test mocks to new format and add new undeployed stdlib

--- packages/lib/test/setup.js
+++ packages/lib/test/setup.js
@@ -3,7 +3,7 @@
 process.env.NODE_ENV = 'test'
 
 require('chai')
-  .use(require('chai-as-promised'))
+  .use(require('chai-as-promised')) // TODO: Remove this dependency
   .use(require('chai-bignumber')(web3.BigNumber))
   .use(require('../src/test/helpers/assertions'))
   .should()
