7984cf91df8e37371a86252c7ec25aed2c0c30b0
Santiago Palladino <spalladino@gmail.com>
2018-08-21 20:06:55 -0300

    Handle package names with slashes

--- packages/cli/src/utils/naming.js
+++ packages/cli/src/utils/naming.js
@@ -5,9 +5,10 @@ export function toContractFullName(packageName, contractName) {
   return [packageName, contractName].join('/')
 }
 
-export function fromContractFullName(contractFullName, defaultPackageName = null) {
+export function fromContractFullName(contractFullName) {
   // FIXME: Support package names with slash (i.e. org/name)
-  const [packageName, contractName] = contractFullName.split('/')
-  if (!contractName) return { contract: packageName, package: defaultPackageName }
-  return { contract: contractName, package: packageName }
+  const fragments = contractFullName.split('/')
+  const contractName = fragments.pop()
+  if (fragments.length === 0) return { contract: contractName }
+  else return { contract: contractName, package: fragments.join('/') }
 }
