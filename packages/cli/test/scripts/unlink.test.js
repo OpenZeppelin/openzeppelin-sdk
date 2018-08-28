3fdf4234db4433248254c6923eb61f1c0b9cf629
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-08-28 15:52:16 -0300

    Add assertion to unlink multiple deps test - Add an assertion to verify that after unlinking a group of dependencies, the other ones still remain linked - Refactor unlink tests structure

--- packages/cli/test/scripts/unlink.test.js
+++ packages/cli/test/scripts/unlink.test.js
@@ -9,37 +9,49 @@ contract('unlink script', function() {
     this.packageFile = new ZosPackageFile('test/mocks/packages/package-with-multiple-stdlibs.zos.json')
   })
 
-  it('throws an error if no libs are provided', async function () {
-    const libNames = []
-    await unlink({ libNames, packageFile: this.packageFile })
-      .should.be.rejectedWith('At least one library name must be provided.')
+  describe('without valid parameters', function() {
+    it('throws an error if no libs are provided', async function () {
+      const libNames = []
+      await unlink({ libNames, packageFile: this.packageFile })
+        .should.be.rejectedWith('At least one library name must be provided.')
+    })
+
+    it('throws an error if project is a library', async function () {
+      this.packageFile.lib = true
+      const libNames = ['mock-stdlib', 'mock-stdlib-2']
+      await unlink({ libNames, packageFile: this.packageFile })
+        .should.be.rejectedWith('Libraries do not use stdlibs.')
+    })
+
+    it('throws an error if project library does not exist', async function () {
+      const libName = 'bulbasaur-lib2'
+      await unlink({ libNames: [libName], packageFile: this.packageFile })
+        .should.be.rejectedWith(`Could not find a zos.json file for '${libName}'. Make sure it is provided by the npm package.`)
+    })
   })
 
-  it('throws an error if project is a library', async function () {
-    this.packageFile.lib = true
-    const libNames = ['mock-stdlib', 'mock-stdlib-2']
-    await unlink({ libNames, packageFile: this.packageFile })
-      .should.be.rejectedWith('Libraries do not use stdlibs.')
-  })
+  describe('with valid parameters', function() {
+    it('unlinks a dependency', async function () {
+      const { dependencies } = this.packageFile
+      const libToUnlink = 'mock-stdlib'
+      const remainingLibs = ['mock-stdlib-2', 'mock-stdlib-undeployed']
 
-  it('throws an error if project library does not exist', async function () {
-    const libName = 'bulbasaur-lib2'
-    await unlink({ libNames: [libName], packageFile: this.packageFile })
-      .should.be.rejectedWith(`Could not find a zos.json file for '${libName}'. Make sure it is provided by the npm package.`)
-  })
+      await unlink({ libNames: [libToUnlink], packageFile: this.packageFile })
 
-  it('unlinks a dependency in a list of dependencies', async function () {
-    const { dependencies } = this.packageFile
-    const libName = 'mock-stdlib'
-    await unlink({ libNames: [libName], packageFile: this.packageFile })
-    dependencies.should.not.have.property(libName)
-  })
+      dependencies.should.not.have.all.keys(libToUnlink)
+      dependencies.should.have.all.keys(remainingLibs)
+    })
 
-  it('unlinks multiple dependencies', async function () {
-    const { dependencies } = this.packageFile
-    const libNames = ['mock-stdlib', 'mock-stdlib-2']
-    await unlink({ libNames, packageFile: this.packageFile })
-    dependencies.should.not.have.property(...libNames)
+    it('unlinks multiple dependencies', async function () {
+      const { dependencies } = this.packageFile
+      const libNames = ['mock-stdlib', 'mock-stdlib-2']
+      const remainingLib = 'mock-stdlib-undeployed'
+
+      await unlink({ libNames, packageFile: this.packageFile })
+
+      dependencies.should.not.have.all.keys(libNames)
+      dependencies.should.have.all.keys(remainingLib)
+    })
   })
 
 })
