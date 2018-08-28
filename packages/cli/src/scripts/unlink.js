b70624c93663f7ffa5c5e2292d4db52d1494d5c7
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-08-27 21:03:16 -0300

    Fix in unlink error message

--- packages/cli/src/scripts/unlink.js
+++ packages/cli/src/scripts/unlink.js
@@ -4,7 +4,7 @@ import stdout from '../utils/stdout'
 import ControllerFor from "../models/local/ControllerFor"
 
 export default async function unlink({ libNames = [], packageFile = undefined }) {
-  if (!libNames.length) throw Error('At least one library name and version must be provided.')
+  if (!libNames.length) throw Error('At least one library name must be provided.')
   const controller = ControllerFor(packageFile)
   if (controller.isLib) throw Error('Libraries do not use stdlibs.')
 
