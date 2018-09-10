38903fccc2b3fd1ef06e8265e5ce8a3b56667e8c
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-09-10 12:12:35 -0300

    TestApp fix to work with multiple deps models (#82)
    * Add mock package with multiple stdlibs
    
    * Return controller AppProject instead of App
    
    * Rename TestApp to ProjectApp
    - Rename TestApp to ProjectApp
    - Fix most of TestApp tests
    
    * Rename testApp to ProjectApp
    
    * Switch from deployed deps to non-deployed deps
    
    * Fix remaining tests
    
    * Implement TestLib function
    Implemented TestLib function to test Lib projects
    
    * Unified TestLib and TestApp in favor of TestHelper

--- packages/cli/src/models/TestHelper.js
+++ packages/cli/src/models/TestHelper.js
@@ -1,14 +1,15 @@
 import ControllerFor from '../models/network/ControllerFor';
 
 /**
- * Initializes a zOS application for testing, deploying it to the test network,
+ * Initializes a zOS application or stdlib project for testing, deploying it to the test network,
  * along with its standard library (if specified)
  * @param txParams optional txParams (from, gas, gasPrice) to use on every transaction
  * @param networkFile optional `ZosNetworkFile` object to use, instead of zos.test.json
  */
-export default async function testApp(txParams = {}, networkFile = undefined) {
+export default async function(txParams = {}, networkFile = undefined) {
   const controller = new ControllerFor('test', txParams, networkFile)
-  await controller.deployStdlib();
-  await controller.push();
-  return controller.app;
+  if (!controller.isLib) await controller.deployLibs()
+  await controller.push()
+
+  return controller.project
 }
