40a1e52af191c91b806355e103c8dcd04711da63
Santiago Palladino <spalladino@gmail.com>
2018-09-13 20:13:23 -0300

    Refactor simple-example to use new simple project and add tests

--- examples/lib-simple/contracts/MyContract_v0.sol
+++ examples/lib-simple/contracts/MyContract_v0.sol
@@ -1,11 +1,8 @@
 pragma solidity ^0.4.21;
 
-
-import "zos-lib/contracts/migrations/Initializable.sol";
-
+import "zos-lib/contracts/Initializable.sol";
 
 contract MyContract_v0 is Initializable {
-
   uint256 public value;
   
   function initialize(uint256 _value) initializer public {
--- examples/lib-simple/contracts/MyContract_v1.sol
+++ examples/lib-simple/contracts/MyContract_v1.sol
@@ -1,16 +1,13 @@
 pragma solidity ^0.4.21;
 
-
-import "zos-lib/contracts/migrations/Initializable.sol";
-
+import "zos-lib/contracts/Initializable.sol";
 
 contract MyContract_v1 is Initializable {
-  uint256 public x;
+  uint256 public value;
   
   function initialize(uint256 _value) initializer public {
     value = _value;
   }
-
 
   function add(uint256 _value) public {
     value = value + _value;
