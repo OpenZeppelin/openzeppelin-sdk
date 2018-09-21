9d69946c92f901c618672ac9cdd34bcc538afff7
Santiago Palladino <spalladino@gmail.com>
2018-09-21 17:13:54 -0300

    Implement suggestions by facuspagnuolo

--- packages/cli/src/models/network/NetworkBaseController.js
+++ packages/cli/src/models/network/NetworkBaseController.js
@@ -6,6 +6,7 @@ import Verifier from '../Verifier'
 import { flattenSourceCode } from '../../utils/contracts'
 import { allPromisesOrError } from '../../utils/async';
 import { getStorageLayout, getBuildArtifacts, compareStorageLayouts } from 'zos-lib/src';
+import { logStorageLayoutDiffs } from '../../interface/Validations';
 
 const log = new Logger('NetworkController');
 
@@ -151,85 +152,10 @@ export default class NetworkBaseController {
     const contract = Contracts.getFromLocal(contractName);
     const updatedStorageInfo = getStorageLayout(contract, buildArtifacts)
     const storageDiff = compareStorageLayouts(originalStorageInfo, updatedStorageInfo)
-    if (!_.isEmpty(storageDiff)) {
-      this._logStorageLayoutDiffs(contract, storageDiff, originalStorageInfo, updatedStorageInfo, buildArtifacts)
-      log.info('Read more at https://docs.zeppelinos.org/docs/advanced.html#preserving-the-storage-structure')
-    }
+    logStorageLayoutDiffs(storageDiff, originalStorageInfo, updatedStorageInfo, this.log)
 
+    // Validation passes only if all diff kinds are appends
     return _.every(storageDiff, diff => diff.action === 'append')
-  }
-
-  _logStorageLayoutDiffs(contract, storageDiff, originalStorageInfo, updatedStorageInfo, buildArtifacts) {
-    storageDiff.forEach(({ updated, original, action }) => {
-      const updatedSourceCode = updated && fs.exists(updated.path) && fs.read(updated.path)
-      const updatedVarType = updated && updatedStorageInfo.types[updated.type];
-      const updatedVarSource = updated && [updated.path, this._srcToLineNumber(updated.path, updated.src)].join(':');
-      const updatedVarDescription = updated && 
-        (this._tryGetSourceFragment(updatedSourceCode, updatedVarType.src) 
-         || [updatedVarType.label, updated.label].join(' '));
-      
-      const originalVarType = original && originalStorageInfo.types[original.type];
-      const originalVarDescription = original && [originalVarType.label, original.label].join(' ');
-
-      switch (action) {
-        case 'insert':
-          log.error(`- New variable '${updatedVarDescription}' was added in contract ${updated.contract} in ${updatedVarSource}\n` +
-                    `  This pushes all variables after ${updated.label} to a higher position in storage, `+
-                    `causing the updated contract to read incorrect initial values. Only add new variables at the `+
-                    `end of your contract to prevent this issue.`);
-          break;
-        case 'delete':
-          log.error(`- Variable '${originalVarDescription}' was removed from contract ${original.contract}.\n`+
-                    `  This will move all variables after ${original.label} to a lower position in storage, `+
-                    `causing the updated contract to read incorrect initial values. Avoid deleting existing ` +
-                    `variables to prevent this issue, and rename them to communicate that they are not to be used.`);
-          break;
-        case 'append':
-          log.info(`- New variable '${updatedVarDescription}' was added in contract ${updated.contract} in ${updatedVarSource} `+
-                   `at the end of the contract.\n` +
-                   `  This does not alter the original storage, and should be a safe change.`)
-          break;
-        case 'pop':
-          log.warn(`- Variable '${originalVarDescription}' was removed from the end of contract ${original.contract}.\n`+
-                   `  Though this will not alter the positions in storage of other variables, if a new variable is added ` +
-                   `at the end of the contract, it will have the initial value of ${original.label} when upgrading. ` +
-                   `Avoid deleting existing variables to prevent this issue, and rename them to communicate that they are not to be used.`);
-          break;
-        case 'rename':
-          log.warn(`- Variable '${originalVarDescription}' in contract ${original.contract} was renamed to ${updated.label} in ${updatedVarSource}.\n` +
-                   `  Note that the new variable ${updated.label} will have the value of ${original.label} after upgrading. ` +
-                   `If this is not the desired behavior, add a new variable ${updated.label} at the end of your contract instead.`)
-          break;
-        case 'typechange':
-          log.warn(`- Variable '${original.label}' in contract ${original.contract} was changed from ${originalVarType.label} ` +
-                   `to ${updatedVarType.label} in ${updatedVarSource}.\n` + 
-                   `  If ${updatedVarType.label} is not compatible with ${originalVarType.label}, ` +
-                   `then ${updated.label} could be initialized with an invalid value after upgrading. Avoid changing types of existing ` +
-                   `variables to prevent this issue, and declare new ones at the end of your contract instead.`)
-          break;
-        case 'replace':
-          log.warn(`- Variable '${originalVarDescription}' in contract ${original.contract} was replaced with '${updatedVarDescription}' ` +
-                   `in ${updatedVarSource}.\nThis will cause ${updated.label} to be initialized with the value of ${original.label}. ` +
-                   `  If type ${updatedVarType.label} is not compatible with ${originalVarType.label}, then ${updated.label} could be `+
-                   `initialized with an invalid value after upgrading. Avoid changing types of existing ` +
-                   `variables to prevent this issue, and declare new ones at the end of your contract instead.`)
-          break;
-        default:
-          log.error(`Unexpected layout changeset: ${action}`)
-      }
-    });
-  }
-
-  _srcToLineNumber(sourceCode, srcFragment) {
-    if (!sourceCode || !srcFragment) return null;
-    const [begin] = srcFragment.split(':', 1);
-    return sourceCode.substr(0, begin).split('\n').length
-  }
-
-  _tryGetSourceFragment(sourceCode, src) {
-    if (!src || !sourceCode) return null;
-    const [begin, count] = src.split(':');
-    return sourceCode.substr(begin, count);
   }
 
   checkContractDeployed(packageName, contractAlias, throwIfFail = false) {
