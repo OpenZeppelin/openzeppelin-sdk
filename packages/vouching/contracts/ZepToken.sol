ecfc2e65e3a1484e077f991259205b6476bc0fc2
Martin Abbatemarco <tinchoabbate@users.noreply.github.com>
2018-09-24 15:44:55 -0300

    ZepToken and related tests update
    * Tests now use chai (should and bignumber)
    
    * Deleted unused import in contract
    
    * Made total supply definition clearer in contract

--- packages/vouching/contracts/ZepToken.sol
+++ packages/vouching/contracts/ZepToken.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 /**
@@ -14,8 +13,8 @@ contract ZepToken is StandardToken, PausableToken {
   string public constant symbol = "ZEP";
   uint8 public constant decimals = 18;
 
-  uint256 public constant TOTAL_SUPPLY = 100000000 * (10 ** uint256(decimals));
-  
+  uint256 public constant TOTAL_SUPPLY = 1e8 * (10 ** uint256(decimals));
+
   /**
    * @dev Constructor that gives msg.sender all of existing tokens, and sets it as the owner
    */
@@ -26,5 +25,5 @@ contract ZepToken is StandardToken, PausableToken {
     emit Transfer(address(0), msg.sender, TOTAL_SUPPLY);
   }
 
-  
+
 }
--- packages/vouching/test/ZepToken.test.js
+++ packages/vouching/test/ZepToken.test.js
@@ -2,6 +2,10 @@ const BigNumber = web3.BigNumber;
 
 const ZepToken = artifacts.require('ZepToken');
 
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
 contract('ZepToken', ([ _, owner]) => {
   beforeEach(async function () {
     this.zepToken = await ZepToken.new();
@@ -9,17 +13,17 @@ contract('ZepToken', ([ _, owner]) => {
 
   it('has a name', async function () {
     const name = await this.zepToken.name();
-    assert.equal(name, "Zep Token");
+    name.should.be.equal('Zep Token');
   });
 
   it('has a symbol', async function () {
     const symbol = await this.zepToken.symbol();
-    assert.equal(symbol, "ZEP");
+    symbol.should.be.equal('ZEP');
   });
 
   it('has an amount of decimals', async function () {
     const decimals = await this.zepToken.decimals();
-    assert.equal(decimals, 18);
+    decimals.should.be.bignumber.equal(18);
   });
 
   it('has the correct total supply', async function () {
