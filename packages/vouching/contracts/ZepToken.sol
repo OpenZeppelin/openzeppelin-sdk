bf64a6417ca828c536dfc91b50ef60c43e9bfbf7
Alejo Salles <alejosalles@gmail.com>
2018-09-25 19:33:09 -0300

    adds Pausable functionality to ZepToken

--- packages/vouching/contracts/ZepToken.sol
+++ packages/vouching/contracts/ZepToken.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-zos/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-zos/contracts/token/ERC20/ERC20Pausable.sol";
 import './TPL/TPLToken.sol';
 
 /**
  * @title ZepToken
  * @dev ZEP token contract including mintable, pausable and burnable functionalities
  */
-contract ZepToken is Initializable, TPLToken, ERC20Detailed {
+contract ZepToken is Initializable, TPLToken, ERC20Detailed, ERC20Pausable {
 
   function initialize(
     AttributeRegistry _jurisdictionAddress,
@@ -19,6 +20,7 @@ contract ZepToken is Initializable, TPLToken, ERC20Detailed {
     uint8 decimals = 18;
     uint256 totalSupply = 1e8 * (10 ** uint256(decimals));
 
+    ERC20Pausable.initialize();
     ERC20Detailed.initialize("Zep Token", "ZEP", decimals);
     TPLToken.initialize(_jurisdictionAddress, _validRecipientAttributeId);
     
