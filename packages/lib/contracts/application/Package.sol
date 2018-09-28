fb5b3badc837a2c6226211d0d36df7bcbf526dbc
Santiago Palladino <spalladino@gmail.com>
2018-09-28 12:08:07 -0300

    Add inline docs for Package

--- packages/lib/contracts/application/Package.sol
+++ packages/lib/contracts/application/Package.sol
@@ -5,14 +5,17 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * @title Package
- * @dev Collection of contracts grouped into versions.
- * Contracts with the same name can have different implementation addresses in different versions.
+ * @dev A package is composed by a set of versions, identified via semantic versioning,
+ * where each version has a contract address that refers to a reusable implementation,
+ * plus an optional content URI with metadata. Note that the semver identifier is restricted
+ * to major, minor, and patch, as prerelease tags are not supported.
  */
 contract Package is Ownable {
   /**
    * @dev Emitted when a version is added to the package.
    * @param semanticVersion Name of the added version.
-   * @param contractAddress ImplementationProvider associated with the version.
+   * @param contractAddress Contract associated with the version.
+   * @param contentURI Optional content URI with metadata of the version.
    */
   event VersionAdded(uint64[3] semanticVersion, address contractAddress, bytes contentURI);
 
@@ -22,33 +25,38 @@ contract Package is Ownable {
     bytes contentURI; 
   }
 
-  /*
-   * @dev Mapping associating versions and their implementation providers.
-   */
   mapping (bytes32 => Version) internal versions;
   mapping (uint64 => bytes32) internal majorToLatestVersion;
   uint64 internal latestMajor;
 
   /**
-   * @dev Returns a version.
-   * @param semanticVersion Name of the version.
-   * @return the version.
+   * @dev Returns a version given its semver identifier.
+   * @param semanticVersion Semver identifier of the version.
+   * @return Contract address and content URI for the version, or zero if not exists.
    */
   function getVersion(uint64[3] semanticVersion) public view returns (address contractAddress, bytes contentURI) {
     Version storage version = versions[semanticVersionHash(semanticVersion)];
     return (version.contractAddress, version.contentURI); 
   }
 
+  /**
+   * @dev Returns a contract for a version given its semver identifier.
+   * This method is equivalent to `getVersion`, but returns only the contract address.
+   * @param semanticVersion Semver identifier of the version.
+   * @return Contract address for the version, or zero if not exists.
+   */
   function getContract(uint64[3] semanticVersion) public view returns (address contractAddress) {
     Version storage version = versions[semanticVersionHash(semanticVersion)];
     return version.contractAddress;
   }
 
   /**
-   * @dev Adds the implementation provider of a new version to the package.
-   * @param semanticVersion Name of the version.
-   * @param contractAddress ImplementationProvider associated with the version.
-   * @param contentURI content URI.
+   * @dev Adds a new version to the package. Only the Owner can add new versions.
+   * Reverts if the specified semver identifier already exists. 
+   * Emits a `VersionAdded` event if successful.
+   * @param semanticVersion Semver identifier of the version.
+   * @param contractAddress Contract address for the version, must be non-zero.
+   * @param contentURI Optional content URI for the version.
    */
   function addVersion(uint64[3] semanticVersion, address contractAddress, bytes contentURI) public onlyOwner {
     require(contractAddress != address(0), "Contract address is required");
@@ -81,18 +89,32 @@ contract Package is Ownable {
 
   /**
    * @dev Checks whether a version is present in the package.
-   * @param semanticVersion Name of the version.
-   * @return true if the version is already in the package, false otherwise.
+   * @param semanticVersion Semver identifier of the version.
+   * @return true if the version is registered in this package, false otherwise.
    */
   function hasVersion(uint64[3] semanticVersion) public view returns (bool) {
     Version storage version = versions[semanticVersionHash(semanticVersion)];
     return address(version.contractAddress) != address(0);
   }
 
+  /**
+   * @dev Returns the version with the highest semver identifier registered in the package.
+   * For instance, if `1.2.0`, `1.3.0`, and `2.0.0` are present, will always return `2.0.0`, regardless 
+   * of the order in which they were registered. Returns zero if no versions are registered.
+   * @return Semver identifier, contract address, and content URI for the version, or zero if not exists.
+   */
   function getLatest() public view returns (uint64[3] semanticVersion, address contractAddress, bytes contentURI) {
     return getLatestByMajor(latestMajor);
   }
 
+  /**
+   * @dev Returns the version with the highest semver identifier for the given major.
+   * For instance, if `1.2.0`, `1.3.0`, and `2.0.0` are present, will return `1.3.0` for major `1`, 
+   * regardless of the order in which they were registered. Returns zero if no versions are registered
+   * for the specified major.
+   * @param major Major identifier to query
+   * @return Semver identifier, contract address, and content URI for the version, or zero if not exists.
+   */
   function getLatestByMajor(uint64 major) public view returns (uint64[3] semanticVersion, address contractAddress, bytes contentURI) {
     Version storage version = versions[majorToLatestVersion[major]];
     return (version.semanticVersion, version.contractAddress, version.contentURI); 
