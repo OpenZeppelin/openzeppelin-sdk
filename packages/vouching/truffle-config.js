04938a7d8202df919cc7e4f8b1846a5a92d1d40d
Alejandro Santander <Palebluedot@gmail.com>
2018-10-04 17:26:09 -0300

    Fixed ridiculously high gas price in ropsten.

--- packages/vouching/truffle-config.js
+++ packages/vouching/truffle-config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: 'localhost',
       port: 8565,
       network_id: 3,
-      gasPrice: 30 ** 9,
+      gasPrice: 2e9, // 2 Gwei
       gas: 6000000,
       from: '0x09902a56d04a9446601a0d451e07459dc5af0820'
     }
