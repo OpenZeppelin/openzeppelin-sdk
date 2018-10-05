28af43727185cd87dfd842d511afe8dab68a567e
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-06 00:13:05 +0200

    Improve ABI parsing for proxies initialization

--- packages/lib/src/utils/ABIs.js
+++ packages/lib/src/utils/ABIs.js
@@ -8,9 +8,28 @@ export function buildCallData(contractClass, methodName, args) {
 }
 
 export function getFunction(contractClass, methodName, args) {
-  const method = contractClass.abi.find(fn => fn.name === methodName && fn.inputs.length === args.length)
+  const baseMethod = findFunctionInBaseContract(contractClass, methodName)
+  const matchArgsTypes = fn => fn.inputs.every((input, index) => baseMethod.inputs[index] && baseMethod.inputs[index].type === input.type)
+  const matchNameAndArgsLength = fn => fn.name === methodName && fn.inputs.length === args.length
+  let method
+  if (baseMethod) method = contractClass.abi.find(fn => matchNameAndArgsLength(fn) && matchArgsTypes(fn))
+  if (!method) method = contractClass.abi.find(fn => matchNameAndArgsLength(fn))
   if (!method) throw Error(`Could not find method '${methodName}' with ${args.length} arguments in contract class`)
   return method
+}
+
+export function findFunctionInBaseContract(contractClass, methodName) {
+  const astContractNode = contractClass.ast.nodes.find(node => node.name === contractClass.contractName)
+  const initMethodNode = astContractNode.nodes.find(node => node.nodeType === "FunctionDefinition" && node.name === methodName)
+  if (initMethodNode) {
+    const parameters = initMethodNode.parameters.parameters
+    const inputs = parameters.map(parameter => {
+      const typeString = parameter.typeDescriptions.typeString
+      const type = typeString.includes('contract') ? 'address' : typeString
+      return { name: parameter.name , type }
+    })
+    return { name: methodName, inputs }
+  }
 }
 
 export function callDescription(method, args) {
