115ee82ae94b868a1233fa40d46a7e1a220f25d7
Facundo Spagnuolo <facuspagnuolo@users.noreply.github.com>
2018-10-06 00:07:40 +0200

    Provisional fix to ZEPToken parametrizing msg.sender for initialize (#185)
    * Fix integration tests using v2.0.0-alpha.2
    
    * Provisional fix to ZEPToken parametrizing msg.sender for initialize

--- packages/vouching/contracts/ZEPToken.sol
+++ packages/vouching/contracts/ZEPToken.sol
@@ -12,6 +12,7 @@ import "TPL-1.0-audit/contracts/TPLToken.sol";
 contract ZEPToken is Initializable, TPLToken, ERC20Detailed, ERC20Pausable {
 
   function initialize(
+    address _sender,
     AttributeRegistry _jurisdictionAddress,
     uint256 _validRecipientAttributeId
   ) 
@@ -24,8 +25,11 @@ contract ZEPToken is Initializable, TPLToken, ERC20Detailed, ERC20Pausable {
     ERC20Pausable.initialize();
     ERC20Detailed.initialize("ZeppelinOS Token", "ZEP", decimals);
     TPLToken.initialize(_jurisdictionAddress, _validRecipientAttributeId);
-    
-    _mint(msg.sender, totalSupply);
+
+    // TODO: Provisional fix. Until OZ initializable works with a parametrized msg.sender
+    _mint(_sender, totalSupply);
+    _removePauser(msg.sender);
+    _addPauser(_sender);
   }
 
 }
