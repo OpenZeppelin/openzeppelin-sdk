8021ec75c54f7e6f8c8bd69090b5e8bfe556210e
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-10 12:51:53 +0200

    Polish commands files

--- packages/vouching/src/bin/configure.js
+++ packages/vouching/src/bin/configure.js
@@ -1,17 +1,15 @@
 #! /usr/bin/env node
 
-import configure from '../scripts/configure';
-import runWithTruffle from 'zos/lib/utils/runWithTruffle';
-import parseArgs from 'minimist';
+import parseArgs from 'minimist'
+import configure from '../scripts/configure'
+import runWithTruffle from 'zos/lib/utils/runWithTruffle'
 
-const params = parseArgs(process.argv.slice(2), {
-  string: 'from'
-});
-const network = params.network;
-const from = params.from;
-if(!network) throw new Error('Please specify a network using --network=<network>.');
-if(!from) throw new Error('Please specify a sender address using --from=<addr>.');
+const params = parseArgs(process.argv.slice(2), { string: 'from' })
+const { network, from } = params
+
+if (!network) throw new Error('Please specify a network using --network=<network>.')
+if (!from)    throw new Error('Please specify a sender address using --from=<addr>.')
 
 runWithTruffle(options => configure(options), { network, from })
   .then(console.log)
-  .catch(console.error);
+  .catch(console.error)
--- packages/vouching/src/bin/validate.js
+++ packages/vouching/src/bin/validate.js
@@ -1,17 +1,15 @@
 #! /usr/bin/env node
 
-import validate from '../scripts/validate';
-import runWithTruffle from 'zos/lib/utils/runWithTruffle';
-import parseArgs from 'minimist';
+import parseArgs from 'minimist'
+import validate from '../scripts/verify'
+import runWithTruffle from 'zos/lib/utils/runWithTruffle'
 
-const params = parseArgs(process.argv.slice(2), {
-  string: 'from'
-});
-const network = params.network;
-const from = params.from;
-if(!network) throw new Error('Please specify a network using -network=<network>.');
-if(!from) throw new Error('Please specify a sender address using -from=<addr>.');
+const params = parseArgs(process.argv.slice(2), { string: 'from' })
+const { network, from } = params
+
+if (!network) throw new Error('Please specify a network using -network=<network>.')
+if (!from)    throw new Error('Please specify a sender address using -from=<addr>.')
 
 runWithTruffle(options => validate(options), { network, from })
   .then(console.log)
-  .catch(console.error);
+  .catch(console.error)
