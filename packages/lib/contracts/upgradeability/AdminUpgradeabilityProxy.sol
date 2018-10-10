a27807cee278770697b677368458b86416a279a1
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-10-10 12:43:18 -0300

    upgradeToAndCall now delegatecalls instead of externally calling.

--- packages/lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol
+++ packages/lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol
@@ -98,7 +98,7 @@ contract AdminUpgradeabilityProxy is UpgradeabilityProxy {
    */
   function upgradeToAndCall(address newImplementation, bytes data) payable external ifAdmin {
     _upgradeTo(newImplementation);
-    require(address(this).call.value(msg.value)(data));
+    require(newImplementation.delegatecall(data));
   }
 
   /**
