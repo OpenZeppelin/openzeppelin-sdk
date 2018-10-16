4a21bf6fa0eaaa3d75475e4aa077dbd31fa8cf26
Santiago Palladino <spalladino@gmail.com>
2018-10-16 10:40:48 +0200

    Use new ScriptError in publish command

--- packages/cli/src/scripts/publish.js
+++ packages/cli/src/scripts/publish.js
@@ -1,11 +1,14 @@
 import ControllerFor from "../models/network/ControllerFor";
+import ScriptError from '../models/errors/ScriptError';
 
 export default async function publish({ network, txParams = {}, networkFile = undefined }) {
   const controller = ControllerFor(network, txParams, networkFile);
   
   try {
     await controller.toFullApp();
-  } finally {
-    controller.writeNetworkPackage();
+    controller.writeNetworkPackageIfNeeded();
+  } catch(error) {
+    const cb = () => controller.writeNetworkPackageIfNeeded();
+    throw new ScriptError(error.message, cb);
   }
 }
