fdfb08723240b252b39a6b02ec128a717361b81b
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-10-17 09:28:19 -0300

    Check if project is published before freeze

--- packages/cli/src/models/network/NetworkBaseController.js
+++ packages/cli/src/models/network/NetworkBaseController.js
@@ -331,6 +331,7 @@ export default class NetworkBaseController {
   }
 
   async freeze() {
+    if (!this.appAddress) throw Error('Cannot freeze an unpublished project')
     await this.fetchOrDeploy(this.currentVersion)
     await this.project.freeze()
     this.networkFile.frozen = true
