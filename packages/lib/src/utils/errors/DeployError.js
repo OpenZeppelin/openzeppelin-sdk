a4f11471ab372e657dfddd3c7ed6a096a772c991
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-10-18 15:35:59 -0300

    Preserve stack trace when using DeployError

--- packages/lib/src/utils/errors/DeployError.js
+++ packages/lib/src/utils/errors/DeployError.js
@@ -1,8 +1,9 @@
 'use strict'
 
 export class DeployError extends Error {
-  constructor(message, props) {
+  constructor({ message, stack }, props) {
     super(message)
+    this.stack = stack
     Object.keys(props).forEach(prop => this[prop] = props[prop])
   }
 }
