15421f7577fd0bd670c93639af4cd033757181c7
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-18 18:54:12 +0200

    Fix vouching deployment script

--- packages/vouching/src/scripts/deploy.js
+++ packages/vouching/src/scripts/deploy.js
@@ -11,8 +11,8 @@ export default async function deploy(options) {
   const oneDay = 60 * 60 * 24
   session({ expires: oneDay, ...options })
   log.base(`Pushing ZeppelinOS app with options ${JSON.stringify(options, null, 2)}...`)
-  const isLocalOrTest = options.network === 'local' || options.network === 'test'
-  await push({ deployLibs: isLocalOrTest, ...options })
+  const deployLibs = true // ZeppelinOS only deploys requested packages if those are not deployed
+  await push({ deployLibs, ...options })
   stdout.silent(true)
   const { app, jurisdiction, validator, zepToken, vouching } = await createKernelContracts(options)
   await configureTPL(jurisdiction, validator, options)
