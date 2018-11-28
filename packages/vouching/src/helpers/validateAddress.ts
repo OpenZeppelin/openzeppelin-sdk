c43b3d174049eab77c2c572ec8c59301ed81c311
Alejandro Santander <Palebluedot@gmail.com>
2018-11-28 15:54:29 -0300

    Activated typescript in the vouching package.

--- packages/vouching/src/helpers/validateAddress.ts
+++ packages/vouching/src/helpers/validateAddress.ts
@@ -1,4 +1,6 @@
-export default function validateAddress(address) {
+import * as ethjs from 'ethjs-util';
+
+export default function validateAddress(address:string):boolean {
   if (!address) return false
   if (address === '0x0000000000000000000000000000000000000000') return false
   if (address.substring(0, 2) !== "0x") return false
@@ -7,14 +9,14 @@ export default function validateAddress(address) {
   if(!/^(0x)?[0-9a-f]{40}$/i.test(address)) return false
 
   // Checksum validation.
-  const raw = address.replace('0x','')
-  const allLowerCase = raw.toLowerCase() === raw
-  const allUppercase = raw.toUpperCase() === raw
+  const raw:string = address.replace('0x','')
+  const allLowerCase:boolean = raw.toLowerCase() === raw
+  const allUppercase:boolean = raw.toUpperCase() === raw
   if(allLowerCase || allUppercase) {
     return true // accepts addreses with no checksum data
   }
   else {
-    const checksum = ethjs.toChecksumAddress(address)
+    const checksum:string = ethjs.toChecksumAddress(address)
     if(address !== checksum) return false
   }
 
