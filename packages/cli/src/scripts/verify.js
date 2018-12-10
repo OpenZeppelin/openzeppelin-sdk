57ad1cc149f9fd963e63704f21569c852e8e3c03
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-10 14:51:49 -0300

    Update etherscan api key url

--- packages/cli/src/scripts/verify.js
+++ packages/cli/src/scripts/verify.js
@@ -2,7 +2,7 @@ import ControllerFor from '../models/network/ControllerFor'
 
 export default async function verify(contractAlias, { network = 'mainnet', txParams = {}, networkFile = undefined, optimizer = false, optimizerRuns = 200, remote = 'etherchain', apiKey = undefined }) {
   if (remote === 'etherscan' && !apiKey) {
-    throw new Error('Etherscan API key not specified. To get one, follow this link: https://etherscancom.freshdesk.com/support/solutions/articles/35000022163-i-need-an-api-key')
+    throw new Error('Etherscan API key not specified. To get one, follow this link: https://etherscan.io/myapikey')
   }
   const controller = ControllerFor(network, txParams, networkFile)
   controller.checkLocalContractDeployed(contractAlias, true)
