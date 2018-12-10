14fcb646cb2e469556c08ced18223ff708071fe9
Alejandro Santander <Palebluedot@gmail.com>
2018-12-10 20:39:57 -0300

    Added whitespace rules and removed some empty lines from Ownable.ts

--- packages/lib/src/test/behaviors/Ownable.ts
+++ packages/lib/src/test/behaviors/Ownable.ts
@@ -14,22 +14,16 @@ export default function shouldBehaveLikeOwnable(owner: string, anotherAccount: s
   describe('transferOwnership', function() {
     describe('when the proposed owner is not the zero address', function() {
       const newOwner = anotherAccount;
-
       describe('when the sender is the owner', function() {
         const from = owner;
-
         it('transfers the ownership', async function() {
-
           await this.ownable.transferOwnership(newOwner, { from });
-
           const contractOwner: string = await this.ownable.owner();
           assert.equal(contractOwner, anotherAccount);
         });
 
         it('emits an event', async function() {
-
           const { logs } = await this.ownable.transferOwnership(newOwner, { from });
-
           assert.equal(logs.length, 1);
           assert.equal(logs[0].event, 'OwnershipTransferred');
           assert.equal(logs[0].args.previousOwner, owner);
@@ -38,9 +32,7 @@ export default function shouldBehaveLikeOwnable(owner: string, anotherAccount: s
       });
 
       describe('when the sender is not the owner', function() {
-
         const from: string = anotherAccount;
-
         it('reverts', async function() {
           await assertRevert(this.ownable.transferOwnership(newOwner, { from }));
         });
@@ -48,9 +40,7 @@ export default function shouldBehaveLikeOwnable(owner: string, anotherAccount: s
     });
 
     describe('when the new proposed owner is the zero address', function() {
-
       const newOwner: number = 0x0;
-
       it('reverts', async function() {
         await assertRevert(this.ownable.transferOwnership(newOwner, { from: owner }));
       });
