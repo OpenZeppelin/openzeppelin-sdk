6095fd4ecea7f1f029e1fd678432d9fd3c0adc60
Alejandro Santander <Palebluedot@gmail.com>
2018-12-11 09:44:37 -0300

    Ported Instructions.ts

--- packages/lib/src/validations/Instructions.ts
+++ packages/lib/src/validations/Instructions.ts
@@ -1,28 +1,29 @@
-import Contracts from "../utils/Contracts";
+import Contracts from '../utils/Contracts';
+import ContractFactory from '../artifacts/ContractFactory';
 
-export function hasSelfDestruct(contractClass) {
-  return hasTypeIdentifier(contractClass, "t_function_selfdestruct_nonpayable$_t_address_$returns$__$")
+export function hasSelfDestruct(contractClass:ContractFactory):boolean {
+  return hasTypeIdentifier(contractClass, 't_function_selfdestruct_nonpayable$_t_address_$returns$__$');
 }
 
-export function hasDelegateCall(contractClass) {
-  return hasTypeIdentifier(contractClass, "t_function_baredelegatecall_nonpayable$__$returns$_t_bool_$")
+export function hasDelegateCall(contractClass:ContractFactory):boolean {
+  return hasTypeIdentifier(contractClass, 't_function_baredelegatecall_nonpayable$__$returns$_t_bool_$');
 }
 
-function hasTypeIdentifier(contractClass, typeIdentifier) {
+function hasTypeIdentifier(contractClass:ContractFactory, typeIdentifier:string):boolean {
   for (const node of contractClass.ast.nodes.filter((n) => n.name === contractClass.contractName)) {
-    if (hasKeyValue(node, "typeIdentifier", typeIdentifier)) return true
+    if (hasKeyValue(node, 'typeIdentifier', typeIdentifier)) { return true; }
     for (const baseContract of node.baseContracts || []) {
-      if (hasTypeIdentifier(Contracts.getFromLocal(baseContract.baseName.name), typeIdentifier)) return true
+      if (hasTypeIdentifier(Contracts.getFromLocal(baseContract.baseName.name), typeIdentifier)) { return true; }
     }
   }
-  return false
+  return false;
 }
 
-function hasKeyValue(data, key, value) {
-  if (!data) return false
-  if (data[key] === value) return true
+function hasKeyValue(data:any, key:string, value:string):boolean {
+  if (!data) { return false; }
+  if (data[key] === value) { return true; }
   for (const childKey in data) {
-    if (typeof(data[childKey]) === 'object' && hasKeyValue(data[childKey], key, value)) return true
+    if (typeof(data[childKey]) === 'object' && hasKeyValue(data[childKey], key, value)) { return true; }
   }
-  return false
+  return false;
 }
