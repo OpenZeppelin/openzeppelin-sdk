f61f45ea06778386cab7022ef487185db2941433
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-11 19:11:48 -0300

    Port Package.ts

--- packages/lib/src/application/Package.ts
+++ packages/lib/src/application/Package.ts
@@ -1,76 +1,86 @@
 import _ from 'lodash';
 
-import Logger from '../utils/Logger'
-import { deploy as deployContract, sendTransaction } from '../utils/Transactions'
-import ImplementationDirectory from '../directory/ImplementationDirectory';
+import Logger from '../utils/Logger';
 import Contracts from '../utils/Contracts';
+import ImplementationDirectory from '../directory/ImplementationDirectory';
+import { toSemanticVersion, SemanticVersion } from '../utils/Semver';
 import { toAddress, isZeroAddress } from '../utils/Addresses';
-import { toSemanticVersion } from '../utils/Semver';
+import ContractFactory, { ContractWrapper } from '../artifacts/ContractFactory';
+import { deploy as deployContract, sendTransaction } from '../utils/Transactions';
 
-const log = new Logger('Package')
+const log: any = new Logger('Package');
+
+interface PackageInterface extends ContractWrapper {
+  address: string;
+  hasVersion?;
+  addVersion?;
+  getContract?;
+}
 
 export default class Package {
+  private packageContract: PackageInterface;
+  private txParams: any;
 
-  static async fetch(address, txParams = {}) {
-    if (isZeroAddress(address)) return null
-    const Package = Contracts.getFromLib('Package')
-    const packageContract = await Package.at(address)
-    return new this(packageContract, txParams)
+  public static async fetch(address: string, txParams: any = {}): Promise<Package | null> {
+    if (isZeroAddress(address)) return null;
+    const PackageContact: ContractFactory = Contracts.getFromLib('Package');
+    const packageContract: ContractWrapper = await PackageContact.at(address);
+    return new this(packageContract, txParams);
   }
 
-  static async deploy(txParams = {}) {
-    log.info('Deploying new Package...')
-    const Package = Contracts.getFromLib('Package')
-    const packageContract = await deployContract(Package, [], txParams)
-    log.info(`Deployed Package ${packageContract.address}`)
-    return new this(packageContract, txParams)
+  public static async deploy(txParams: any = {}): Promise<Package> {
+    log.info('Deploying new Package...');
+    const PackageContract: ContractFactory = Contracts.getFromLib('Package');
+    const packageContract = await deployContract(PackageContract, [], txParams);
+    log.info(`Deployed Package ${packageContract.address}`);
+    return new this(packageContract, txParams);
   }
 
-  constructor(packageContract, txParams = {}) {
-    this.packageContract = packageContract
-    this.txParams = txParams
+  constructor(packageContract: PackageInterface, txParams: any = {}) {
+    this.packageContract = packageContract;
+    this.txParams = txParams;
   }
 
-  get contract() {
-    return this.packageContract
+  get contract(): ContractWrapper {
+    return this.packageContract;
   }
 
-  get address() {
-    return this.packageContract.address
+  get address(): string {
+    return this.packageContract.address;
   }
 
-  async hasVersion(version) {
-    return this.packageContract.hasVersion(toSemanticVersion(version))
+  public async hasVersion(version: string): Promise<boolean> {
+    return this.packageContract.hasVersion(toSemanticVersion(version));
   }
 
-  async isFrozen(version) {
-    const directory = await this.getDirectory(version)
-    return directory.isFrozen()
+  public async isFrozen(version: string): Promise<boolean> {
+    const directory = await this.getDirectory(version);
+    return directory.isFrozen();
   }
 
-  async freeze(version) {
-    const directory = await this.getDirectory(version)
-    if (!directory.freeze) throw Error("Implementation directory does not support freezing")
-    return directory.freeze()
+  public async freeze(version: string): Promise<any> {
+    const directory = await this.getDirectory(version);
+    if (!directory.freeze) throw Error('Implementation directory does not support freezing');
+    return directory.freeze();
   }
 
-  async getImplementation(version, contractName) {
-    const directory = await this.getDirectory(version)
-    return directory.getImplementation(contractName)
+  public async getImplementation(version: string, contractName: string): Promise<string> | never {
+    const directory = await this.getDirectory(version);
+    return directory.getImplementation(contractName);
   }
 
-  async newVersion(version, content = "") {
-    log.info('Adding new version...')
-    const semver = toSemanticVersion(version)
-    const directory = await ImplementationDirectory.deploy(this.txParams)
-    await sendTransaction(this.packageContract.addVersion, [semver, directory.address, content], this.txParams)
-    log.info(`Added version ${semver.join('.')}`)
-    return directory
+  public async newVersion(version: string, content: string = ''): Promise<ImplementationDirectory> {
+    log.info('Adding new version...');
+    const semver: SemanticVersion = toSemanticVersion(version);
+    const directory: ImplementationDirectory = await ImplementationDirectory.deploy(this.txParams);
+    await sendTransaction(this.packageContract.addVersion, [semver, directory.address, content], this.txParams);
+    log.info(`Added version ${semver.join('.')}`);
+    return directory;
   }
 
-  async getDirectory(version) {
-    if (!version) throw Error("Cannot get a directory from a package without specifying a version")
-    const directoryAddress = await this.packageContract.getContract(toSemanticVersion(version))
-    return ImplementationDirectory.fetch(directoryAddress, this.txParams)
+  public async getDirectory(version: string): Promise<ImplementationDirectory> | never {
+    if (!version) throw Error('Cannot get a directory from a package without specifying a version');
+    const directoryAddress = await this.packageContract.getContract(toSemanticVersion(version));
+    return ImplementationDirectory.fetch(directoryAddress, this.txParams);
   }
 }
