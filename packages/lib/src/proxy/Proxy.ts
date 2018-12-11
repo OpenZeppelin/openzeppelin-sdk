4cb0d9e307d70d325e3132c0667afe9c8fa916b9
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-11 17:43:12 -0300

    Create Proxy.ts

--- packages/lib/src/proxy/Proxy.ts
+++ packages/lib/src/proxy/Proxy.ts
@@ -1,70 +1,58 @@
-import ZWeb3 from '../artifacts/ZWeb3';
-import Contracts from '../artifacts/Contracts';
-import { toAddress, uint256ToAddress } from '../utils/Addresses';
-import ContractFactory, { ContractWrapper } from '../artifacts/ContractFactory';
-import { deploy as deployContract, sendTransaction } from '../utils/Transactions';
-
-interface ProxyInterface extends ContractWrapper {
-  upgradeTo?;
-  upgradeToAndCall?;
-  changeAdmin?;
-}
+import ZWeb3 from '../artifacts/ZWeb3'
+import Contracts from '../artifacts/Contracts'
+import { toAddress, uint256ToAddress } from '../utils/Addresses'
+import { deploy as deployContract, sendTransaction } from '../utils/Transactions'
 
 export default class Proxy {
-  private contract: ProxyInterface;
-  private txParams: any;
-  public address: string;
-
-  public static at(address: string, txParams: any = {}): Proxy {
-    const ProxyContract: ContractFactory = Contracts.getFromLib('AdminUpgradeabilityProxy');
-    const contract: ProxyInterface = ProxyContract.at(toAddress(address));
-    return new this(contract, txParams);
+  static at(address, txParams = {}) {
+    const ProxyContract = Contracts.getFromLib('AdminUpgradeabilityProxy')
+    const contract = ProxyContract.at(toAddress(address))
+    return new this(contract, txParams)
   }
 
-  public static async deploy(implementation: string, initData: string | null, txParams: any = {}): Promise<Proxy> {
-    const ProxyContract: ContractFactory = Contracts.getFromLib('AdminUpgradeabilityProxy');
-    const contract: ProxyInterface = await deployContract(ProxyContract, [toAddress(implementation), initData || ''], txParams);
-    return new this(contract, txParams);
+  static async deploy(implementation, initData, txParams = {}) {
+    const contract = await deployContract(Contracts.getFromLib('AdminUpgradeabilityProxy'), [toAddress(implementation), initData || ''], txParams)
+    return new this(contract, txParams)
   }
 
-  constructor(contract: ProxyInterface, txParams: any = {}) {
-    this.address = toAddress(contract);
-    this.contract = contract;
-    this.txParams = txParams;
+  constructor(contract, txParams = {}) {
+    this.contract = contract
+    this.address = toAddress(contract)
+    this.txParams = txParams
   }
 
-  public async upgradeTo(address: string, migrateData: string | null): Promise<any> {
-    await this.checkAdmin();
+  async upgradeTo(address, migrateData) {
+    await this._checkAdmin()
     return migrateData
       ? sendTransaction(this.contract.upgradeToAndCall, [toAddress(address), migrateData], this.txParams)
-      : sendTransaction(this.contract.upgradeTo, [toAddress(address)], this.txParams);
+      : sendTransaction(this.contract.upgradeTo, [toAddress(address)], this.txParams)
   }
 
-  public async changeAdmin(newAdmin: string): Promise<any> {
-    await this.checkAdmin();
-    return sendTransaction(this.contract.changeAdmin, [newAdmin], this.txParams);
+  async changeAdmin(newAdmin) {
+    await this._checkAdmin()
+    return sendTransaction(this.contract.changeAdmin, [newAdmin], this.txParams)
   }
 
-  public async implementation(): Promise<string> {
-    const position: string = ZWeb3.sha3('org.zeppelinos.proxy.implementation');
-    return uint256ToAddress(await this.getStorageAt(position));
+  async implementation() {
+    const position = ZWeb3.sha3('org.zeppelinos.proxy.implementation')
+    return uint256ToAddress(await this.getStorageAt(position))
   }
 
-  public async admin(): Promise<string> {
-    const position: string = ZWeb3.sha3('org.zeppelinos.proxy.admin');
-    return uint256ToAddress(await this.getStorageAt(position));
+  async admin() {
+    const position = ZWeb3.sha3('org.zeppelinos.proxy.admin')
+    return uint256ToAddress(await this.getStorageAt(position))
   }
 
-  public async getStorageAt(position: string): Promise<string> {
-    return ZWeb3.getStorageAt(this.address, position);
+  async getStorageAt(position) {
+    return ZWeb3.getStorageAt(this.address, position)
   }
 
-  private async checkAdmin(): Promise<void | never> {
-    const currentAdmin: string = await this.admin();
-    const { from }: { from?: string } = this.txParams;
+  async _checkAdmin() {
+    const currentAdmin = await this.admin()
+    const from = this.txParams.from
     // TODO: If no `from` is set, load which is the default account and use it to compare against the current admin
     if (from && currentAdmin !== from) {
-      throw new Error(`Cannot modify proxy from non-admin account: current admin is ${currentAdmin} and sender is ${from}`);
+      throw new Error(`Cannot modify proxy from non-admin account: current admin is ${currentAdmin} and sender is ${from}`)
     }
   }
 }
