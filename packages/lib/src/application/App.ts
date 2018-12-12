1211ad953022399a284f7e5ed09ce1f5d6eea551
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-12 14:38:18 -0300

    Create App.ts

--- packages/lib/src/application/App.ts
+++ packages/lib/src/application/App.ts
@@ -1,158 +1,156 @@
-import _ from 'lodash';
+'use strict'
 
-import Logger from '../utils/Logger';
-import decodeLogs from '../helpers/decodeLogs';
-import copyContract from '../helpers/copyContract';
-import Contracts from '../artifacts/Contracts';
-import Package from '../application/Package';
-import ImplementationDirectory from '../application/ImplementationDirectory';
+import _ from 'lodash'
+
+import Logger from '../utils/Logger'
+import decodeLogs from '../helpers/decodeLogs'
+import copyContract from '../helpers/copyContract'
+import { deploy as deployContract, sendTransaction, sendDataTransaction } from '../utils/Transactions'
 import { isZeroAddress, toAddress } from '../utils/Addresses';
-import { buildCallData, callDescription, CalldataInfo } from '../utils/ABIs';
-import ContractFactory, { ContractWrapper } from '../artifacts/ContractFactory';
+import { buildCallData, callDescription } from '../utils/ABIs';
+import Contracts from '../utils/Contracts';
 import { toSemanticVersion, semanticVersionEqual } from '../utils/Semver';
-import { deploy as deployContract, sendTransaction, sendDataTransaction } from '../utils/Transactions';
+import { Package, ImplementationDirectory } from '..';
 
-const log: Logger = new Logger('App');
+const log = new Logger('App')
 
 export default class App {
-  private appContract: any;
-  private txParams: any;
 
-  public static async fetch(address: string, txParams: any = {}): Promise<App> {
-    const appContract: ContractWrapper = await this.getContractClass().at(address);
-    return new this(appContract, txParams);
+  static async fetch(address, txParams = {}) {
+    const appContract = await this.getContractClass().at(address)
+    return new this(appContract, txParams)
   }
 
-  public static async deploy(txParams: any = {}): Promise<App> {
-    log.info('Deploying new App...');
-    const appContract: ContractWrapper = await deployContract(this.getContractClass(), [], txParams);
-    log.info(`Deployed App at ${appContract.address}`);
-    return new this(appContract, txParams);
+  static async deploy(txParams = {}) {
+    log.info('Deploying new App...')
+    const appContract = await deployContract(this.getContractClass(), [], txParams)
+    log.info(`Deployed App at ${appContract.address}`)
+    return new this(appContract, txParams)
   }
 
-  public static getContractClass(): ContractFactory {
-    return Contracts.getFromLib('App');
+  static getContractClass() {
+    return Contracts.getFromLib('App')
   }
 
-  constructor(appContract: ContractWrapper, txParams: any = {}) {
-    this.appContract = appContract;
-    this.txParams = txParams;
+  constructor(appContract, txParams = {}) {
+    this.appContract = appContract
+    this.txParams = txParams
   }
 
-  public async getPackage(name): Promise<{ package: Package, version: string }> {
-    const [address, version] = await this.appContract.getPackage(name);
-    const thepackage = await Package.fetch(address, this.txParams);
-    return { package: thepackage, version };
+  async getPackage(name) {
+    const [address, version] = await this.appContract.getPackage(name)
+    const thepackage = await Package.fetch(address, this.txParams)
+    return { package: thepackage, version }
   }
 
-  public async hasPackage(name: string, expectedVersion?: string): Promise<boolean> {
-    const [address, version]: [string, string] = await this.appContract.getPackage(name);
-    return !isZeroAddress(address) &&
-      (!expectedVersion || semanticVersionEqual(expectedVersion, version));
+  async hasPackage(name, expectedVersion = undefined) {
+    const [address, version] = await this.appContract.getPackage(name)
+    return !isZeroAddress(address) && 
+      (!expectedVersion || semanticVersionEqual(expectedVersion, version))
   }
 
-  public async setPackage(name: string, packageAddress: string, version: string): Promise<any> {
-    return await sendTransaction(this.appContract.setPackage, [name, toAddress(packageAddress), toSemanticVersion(version)], this.txParams);
+  async setPackage(name, packageAddress, version) {
+    return await sendTransaction(this.appContract.setPackage, [name, toAddress(packageAddress), toSemanticVersion(version)], this.txParams)
   }
 
-  public async unsetPackage(name: string): Promise<any> {
-    return await sendTransaction(this.appContract.unsetPackage, [name], this.txParams);
+  async unsetPackage(name) {
+    return await sendTransaction(this.appContract.unsetPackage, [name], this.txParams)
   }
 
-  get address(): string {
-    return this.appContract.address;
+  get address() {
+    return this.appContract.address
   }
 
-  get contract(): ContractWrapper {
-    return this.appContract;
+  get contract() {
+    return this.appContract
   }
 
-  public async getImplementation(packageName: string, contractName: string): Promise<string> {
-    return this.appContract.getImplementation(packageName, contractName);
+  async getImplementation(packageName, contractName) {
+    return this.appContract.getImplementation(packageName, contractName)
   }
 
-  public async getProxyImplementation(proxyAddress: string): Promise<string> {
-    return this.appContract.getProxyImplementation(proxyAddress, this.txParams);
+  async getProxyImplementation(proxyAddress) {
+    return this.appContract.getProxyImplementation(proxyAddress, this.txParams)
   }
 
-  public async hasProvider(name: string): Promise<boolean> {
+  async hasProvider(name) {
     return (await this.getProvider(name) != null);
   }
 
-  public async getProvider(name: string): Promise<ImplementationDirectory> {
-    const address: string = await this.appContract.getProvider(name);
-    if (isZeroAddress(address)) return null;
-    return await ImplementationDirectory.fetch(address, this.txParams);
+  async getProvider(name) {
+    const address = await this.appContract.getProvider(name)
+    if (isZeroAddress(address)) return null
+    return await ImplementationDirectory.fetch(address, this.txParams)
   }
 
-  public async changeProxyAdmin(proxyAddress: string, newAdmin: string): Promise<void> {
-    log.info(`Changing admin for proxy ${proxyAddress} to ${newAdmin}...`);
-    await sendTransaction(this.appContract.changeProxyAdmin, [proxyAddress, newAdmin], this.txParams);
-    log.info(`Admin for proxy ${proxyAddress} set to ${newAdmin}`);
+  async changeProxyAdmin(proxyAddress, newAdmin) {
+    log.info(`Changing admin for proxy ${proxyAddress} to ${newAdmin}...`)
+    await sendTransaction(this.appContract.changeProxyAdmin, [proxyAddress, newAdmin], this.txParams)
+    log.info(`Admin for proxy ${proxyAddress} set to ${newAdmin}`)
   }
 
-  public async createContract(contractClass: ContractFactory, packageName: string, contractName: string, initMethodName: string, initArgs: string[]): Promise<ContractWrapper> {
-    const instance: ContractWrapper = await this._copyContract(packageName, contractName, contractClass);
-    await this._initNonUpgradeableInstance(instance, contractClass, packageName, contractName, initMethodName, initArgs);
-    return instance;
+  async createContract(contractClass, packageName, contractName, initMethodName, initArgs) {
+    const instance = await this._copyContract(packageName, contractName, contractClass)
+    await this._initNonUpgradeableInstance(instance, contractClass, packageName, contractName, initMethodName, initArgs)
+    return instance
   }
 
-  public async createProxy(contractClass: ContractFactory, packageName: string, contractName: string, initMethodName: string, initArgs?: string[]): Promise<ContractWrapper> {
+  async createProxy(contractClass, packageName, contractName, initMethodName, initArgs) {
     const { receipt } = typeof(initArgs) === 'undefined'
       ? await this._createProxy(packageName, contractName)
-      : await this._createProxyAndCall(contractClass, packageName, contractName, initMethodName, initArgs);
+      : await this._createProxyAndCall(contractClass, packageName, contractName, initMethodName, initArgs)
 
-    log.info(`TX receipt received: ${receipt.transactionHash}`);
-    const address: string = _.findLast(receipt.logs, (l) => l.event === 'ProxyCreated').args.proxy;
-    log.info(`${packageName} ${contractName} proxy: ${address}`);
-    return contractClass.at(address);
+    log.info(`TX receipt received: ${receipt.transactionHash}`)
+    const address = _.findLast(receipt.logs, l => l.event === 'ProxyCreated').args.proxy
+    log.info(`${packageName} ${contractName} proxy: ${address}`)
+    return contractClass.at(address)
   }
 
-  public async upgradeProxy(proxyAddress: string, contractClass: ContractFactory, packageName: string, contractName: string, initMethodName: string, initArgs: any): Promise<ContractWrapper> {
-    const { receipt }: any = typeof(initArgs) === 'undefined'
+  async upgradeProxy(proxyAddress, contractClass, packageName, contractName, initMethodName, initArgs) {
+    const { receipt } = typeof(initArgs) === 'undefined'
       ? await this._upgradeProxy(proxyAddress, packageName, contractName)
-      : await this._upgradeProxyAndCall(proxyAddress, contractClass, packageName, contractName, initMethodName, initArgs);
-    log.info(`TX receipt received: ${receipt.transactionHash}`);
-    return contractClass.at(proxyAddress);
+      : await this._upgradeProxyAndCall(proxyAddress, contractClass, packageName, contractName, initMethodName, initArgs)
+    log.info(`TX receipt received: ${receipt.transactionHash}`)
+    return contractClass.at(proxyAddress)
   }
 
-  private async _createProxy(packageName: string, contractName: string): Promise<any> {
-    log.info(`Creating ${packageName} ${contractName} proxy without initializing...`);
-    const initializeData: string = '';
-    return sendTransaction(this.appContract.create, [packageName, contractName, initializeData], this.txParams);
+  async _createProxy(packageName, contractName) {
+    log.info(`Creating ${packageName} ${contractName} proxy without initializing...`)
+    const initializeData = ''
+    return sendTransaction(this.appContract.create, [packageName, contractName, initializeData], this.txParams)
   }
 
-  private async _createProxyAndCall(contractClass: ContractFactory, packageName: string, contractName: string, initMethodName: string, initArgs: any): Promise<any> {
-    const { method: initMethod, callData }: CalldataInfo = buildCallData(contractClass, initMethodName, initArgs);
-    log.info(`Creating ${packageName} ${contractName} proxy and calling ${callDescription(initMethod, initArgs)}`);
-    return sendTransaction(this.appContract.create, [packageName, contractName, callData], this.txParams);
+  async _createProxyAndCall(contractClass, packageName, contractName, initMethodName, initArgs) {
+    const { method: initMethod, callData } = buildCallData(contractClass, initMethodName, initArgs)
+    log.info(`Creating ${packageName} ${contractName} proxy and calling ${callDescription(initMethod, initArgs)}`)
+    return sendTransaction(this.appContract.create, [packageName, contractName, callData], this.txParams)
   }
 
-  private async _upgradeProxy(proxyAddress: string, packageName: string, contractName: string): Promise<any> {
-    log.info(`Upgrading ${packageName} ${contractName} proxy without running migrations...`);
-    return sendTransaction(this.appContract.upgrade, [proxyAddress, packageName, contractName], this.txParams);
+  async _upgradeProxy(proxyAddress, packageName, contractName) {
+    log.info(`Upgrading ${packageName} ${contractName} proxy without running migrations...`)
+    return sendTransaction(this.appContract.upgrade, [proxyAddress, packageName, contractName], this.txParams)
   }
 
-  private async _upgradeProxyAndCall(proxyAddress: string, contractClass: ContractFactory, packageName: string, contractName: string, initMethodName: string, initArgs: any): Promise<any> {
-    const { method: initMethod, callData }: CalldataInfo = buildCallData(contractClass, initMethodName, initArgs);
-    log.info(`Upgrading ${packageName} ${contractName} proxy and calling ${callDescription(initMethod, initArgs)}...`);
-    return sendTransaction(this.appContract.upgradeAndCall, [proxyAddress, packageName, contractName, callData], this.txParams);
+  async _upgradeProxyAndCall(proxyAddress, contractClass, packageName, contractName, initMethodName, initArgs) {
+    const { method: initMethod, callData } = buildCallData(contractClass, initMethodName, initArgs)
+    log.info(`Upgrading ${packageName} ${contractName} proxy and calling ${callDescription(initMethod, initArgs)}...`)    
+    return sendTransaction(this.appContract.upgradeAndCall, [proxyAddress, packageName, contractName, callData], this.txParams)
   }
 
-  private async _copyContract(packageName: string, contractName: string, contractClass: ContractFactory): Promise<ContractWrapper> {
-    log.info(`Creating new non-upgradeable instance of ${packageName} ${contractName}...`);
-    const implementation: string = await this.getImplementation(packageName, contractName);
-    const instance: ContractWrapper = await copyContract(contractClass, implementation, this.txParams);
-    log.info(`${packageName} ${contractName} instance created at ${instance.address}`);
+  async _copyContract(packageName, contractName, contractClass) {
+    log.info(`Creating new non-upgradeable instance of ${packageName} ${contractName}...`)
+    const implementation = await this.getImplementation(packageName, contractName)
+    const instance = await copyContract(contractClass, implementation, this.txParams)
+    log.info(`${packageName} ${contractName} instance created at ${instance.address}`)
     return instance;
   }
 
-  private async _initNonUpgradeableInstance(instance: ContractWrapper, contractClass: ContractFactory, packageName: string, contractName: string, initMethodName: string, initArgs?: string[]): Promise<any> {
+  async _initNonUpgradeableInstance(instance, contractClass, packageName, contractName, initMethodName, initArgs) {
     if (typeof(initArgs) !== 'undefined') {
       // this could be front-run, waiting for new initializers model
-      const { method: initMethod, callData }: CalldataInfo = buildCallData(contractClass, initMethodName, initArgs);
-      log.info(`Initializing ${packageName} ${contractName} instance at ${instance.address} by calling ${callDescription(initMethod, initArgs)}`);
-      await sendDataTransaction(instance, Object.assign({}, this.txParams, { data: callData }));
+      const { method: initMethod, callData } = buildCallData(contractClass, initMethodName, initArgs)
+      log.info(`Initializing ${packageName} ${contractName} instance at ${instance.address} by calling ${callDescription(initMethod, initArgs)}`)
+      await sendDataTransaction(instance, Object.assign({}, this.txParams, {data: callData}))
     }
   }
 }
