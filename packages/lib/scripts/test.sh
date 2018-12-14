40008bf34a190c04029874a201677ab3b0cc8edc
Alejandro Santander <Palebluedot@gmail.com>
2018-12-14 12:40:50 -0300

    Tighten up tsc compiler settings for lib, and use a different config file for tests.

--- packages/lib/scripts/test.sh
+++ packages/lib/scripts/test.sh
@@ -9,5 +9,5 @@ if [ "$SOLIDITY_COVERAGE" = true ]; then
   fi
 else
   node_modules/.bin/truffle compile
-  node_modules/.bin/truffle test "$@"
+  TS_NODE_PROJECT="tsconfig.test.json" node_modules/.bin/truffle test "$@"
 fi
