cf476f0e85c177cfae99656794ccaf5c80501d7b
Alejandro Santander <Palebluedot@gmail.com>
2018-12-18 17:16:22 -0300

    Ported files in the cli's utils folder.

--- packages/cli/src/utils/input.ts
+++ packages/cli/src/utils/input.ts
@@ -1,22 +1,22 @@
-export function parseArgs(args) {
+export function parseArgs(args: string): string[] | never {
   if (typeof args !== 'string') throw Error(`Cannot parse ${typeof args}`);
 
   // The following is inspired from ethereum/remix (Copyright (c) 2016)
   // https://github.com/ethereum/remix/blob/89f34fc4f35eca0c386379550c300205d35bfae0/remix-lib/src/execution/txFormat.js#L51-L53
   try {
-    const MATCH_HEX = /(^|([,[]\s*))(0[xX][0-9a-fA-F]+)/g
-    args = args.replace(MATCH_HEX, '$2"$3"') // replace non quoted hex string by quoted hex string
+    const MATCH_HEX = /(^|([,[]\s*))(0[xX][0-9a-fA-F]+)/g;
+    args = args.replace(MATCH_HEX, '$2"$3"'); // replace non quoted hex string by quoted hex string
 
-    const MATCH_WORDS = /(^|([,[]\s*))(\w+)/g
-    args = args.replace(MATCH_WORDS, '$2"$3"') // replace non quoted number by quoted number
+    const MATCH_WORDS = /(^|([,[]\s*))(\w+)/g;
+    args = args.replace(MATCH_WORDS, '$2"$3"'); // replace non quoted number by quoted number
 
-    return JSON.parse('[' + args + ']')
+    return JSON.parse('[' + args + ']');
   } catch (e) {
     throw Error(`Error parsing arguments: ${e}`);
   }
 }
 
-export function parseInit(options, defaultInit) {
+export function parseInit(options: any, defaultInit: any): { initMethod: string, initArgs: string[] } {
   let initMethod = options.init;
   if (typeof initMethod === 'boolean') initMethod = defaultInit;
   if (!initMethod && typeof options.args !== 'undefined') initMethod = defaultInit;
--- packages/cli/src/utils/stdout.ts
+++ packages/cli/src/utils/stdout.ts
@@ -1,13 +1,13 @@
-const state = {
+const state: { silent: boolean } = {
   silent: false
-}
+};
 
-export default function log() {
+export default function log(): void {
   if (!state.silent && process.env.NODE_ENV !== 'test') {
     console.log(...arguments);
   }
 }
 
-export function silent(value) {
+export function silent(value: boolean): void {
   state.silent = value;
 }
