1a1fd0b0b580d6407b0de5afbcac4b6b1c962848
Alejandro Santander <Palebluedot@gmail.com>
2018-12-19 16:58:29 -0300

    Ported TestHelper.ts

---
+++ packages/cli/src/models/TestHelper.ts
@@ -0,0 +1,17 @@
+import ControllerFor from '../models/network/ControllerFor';
+import ZosNetworkFile from '../models/files/ZosNetworkFile';
+import { SimpleProject, AppProject } from 'zos-lib';
+
+/**
+ * Initializes a zOS application testing and deploying it to the test network,
+ * along with dependencies (if specified)
+ * @param txParams optional txParams (from, gas, gasPrice) to use on every transaction
+ * @param networkFile optional `ZosNetworkFile` object to use, instead of zos.test.json
+ */
+export default async function(txParams: any = {}, networkFile?: ZosNetworkFile): Promise<SimpleProject | AppProject> {
+  const controller = ControllerFor('test', txParams, networkFile);
+  await controller.deployDependencies();
+  await controller.push();
+
+  return controller.project;
+}
