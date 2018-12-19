9e443e2639d04ef96aca7079ec9d06cd3362b056
Alejandro Santander <Palebluedot@gmail.com>
2018-12-19 15:15:31 -0300

    More specific type in ScriptError.ts

--- packages/cli/src/models/errors/ScriptError.ts
+++ packages/cli/src/models/errors/ScriptError.ts
@@ -3,7 +3,7 @@ export default class ScriptError extends Error {
   public stack: any;
   public cb: () => void;
 
-  constructor({ message, stack }: { message: string, stack: any }, cb: () => void) {
+  constructor({ message, stack }: { message: string, stack: string }, cb: () => void) {
     super(message);
     this.stack = stack;
     this.cb = cb;
