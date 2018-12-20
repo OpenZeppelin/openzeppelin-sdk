d114e9efc733b0b31a45dd0e8005612da89f5058
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-19 22:43:42 -0300

    Finish porting bin/errors

--- packages/cli/src/bin/errors.ts
+++ packages/cli/src/bin/errors.ts
@@ -1,11 +1,12 @@
-import ErrorHandler from '../models/errors/ErrorHandler'
+import ErrorHandler from '../models/errors/ErrorHandler';
 
-export default function registerErrorHandler(program) {
-  process.on('unhandledRejection', reason => new ErrorHandler(reason, program).call())
-  process.on('uncaughtException', error => new ErrorHandler(error, program).call())
+export default function registerErrorHandler(program): void {
+  const cb = (error: Error): void => new ErrorHandler(error, program).call()
+  process.on('unhandledRejection', cb);
+  process.on('uncaughtException', cb);
 
-  program.on('command:*', function () {
-    console.error(`Invalid command: ${program.args.join(' ')}\nSee --help for a list of available commands.`)
-    process.exit(1)
-  })
+  program.on('command:*', function(): void {
+    console.error(`Invalid command: ${program.args.join(' ')}\nSee --help for a list of available commands.`);
+    process.exit(1);
+  });
 }
