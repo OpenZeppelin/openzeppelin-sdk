c7b6d17f15c19166384cf7ccb447f939dee7d28e
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-20 14:27:40 -0300

    Port src/commands/init

---
+++ packages/cli/src/commands/init.ts
@@ -0,0 +1,29 @@
+import { Command } from 'commander';
+
+import push from './push';
+import init from '../scripts/init';
+
+const name: string = 'init';
+const signature: string = `${name} <project-name> [version]`;
+const description: string = `initialize your ZeppelinOS project. Provide a <project-name> and optionally an initial [version] name`;
+
+const register: (program: Command) => Command = (program) => program
+  .command(signature, undefined, { noHelp: true })
+  .usage('<project-name> [version]')
+  .description(description)
+  .option('--publish', 'automatically publishes your project upon pushing it to a network')
+  .option('--force', 'overwrite existing project if there is one')
+  .option('--link <dependency>', 'link to a dependency')
+  .option('--no-install', 'skip installing packages dependencies locally')
+  .withPushOptions()
+  .action(action);
+
+async function action(name: string, version: string, options: Command): Promise<void> {
+  const { publish, force, link, install: installDependencies } = options;
+
+  const dependencies = link ? link.split(',') : [];
+  await init({ name, version, dependencies, installDependencies, force, publish });
+  await push.tryAction(options);
+}
+
+export default { name, signature, description, register, action };
