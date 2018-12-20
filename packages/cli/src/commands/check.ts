d886b73749a24eca825e41db879baba30f1ed806
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2018-12-20 15:27:13 -0300

    Port src/commands/check

---
+++ packages/cli/src/commands/check.ts
@@ -0,0 +1,24 @@
+import { Command } from 'commander';
+
+import check from '../scripts/check';
+import Compiler from '../models/compiler/Compiler';
+import ConfigVariablesInitializer from '../models/initializer/ConfigVariablesInitializer';
+
+const name: string = 'check';
+const signature: string = `${name} [contract]`;
+const description: string = 'checks your contracts for potential issues';
+
+const register: (program: Command) => Command = (program) => program
+  .command(signature, undefined, { noHelp: true })
+  .usage('[contract] [options]')
+  .description(description)
+  .option('--skip-compile', 'skips contract compilation')
+  .action(action);
+
+async function action(contractAlias: string, options: Command): Promise<void> {
+  ConfigVariablesInitializer.initStaticConfiguration();
+  if (!options.skipCompile) await Compiler.call();
+  check({ contractAlias });
+}
+
+export default { name, signature, description, register, action };
