68e42bb0e35097f8988e71bc3c8f012760c23720
Alejandro Santander <Palebluedot@gmail.com>
2018-12-20 16:08:43 -0300

    Ports the utils folder of cli to Ts (#515)
    * Make function private in StatusComparator.ts
    
    * Ported files in the cli's utils folder.

---
+++ packages/cli/src/utils/async.ts
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+
+export async function allPromisesOrError(promisesWithObjects: any[], toErrorMessage: (error: any, object: any) => string): Promise<any[] | null | never> {
+  const failures = [];
+  const handlingFailure = async (item: any) => {
+    let promise;
+    let object = null;
+    try {
+      if (_.isArray(item)) {
+        [promise, object] = item;
+      } else {
+        promise = item;
+      }
+      return await promise;
+    } catch(error) {
+      failures.push({ error, object });
+      return null;
+    }
+  };
+
+  const results = await Promise.all(
+    _.map(promisesWithObjects, handlingFailure)
+  );
+
+  if(!_.isEmpty(failures)) {
+    if (failures.length === 1) throw failures[0].error;
+    const message = failures.map(({ error, object }) => toErrorMessage ? toErrorMessage(error, object) : (error.message || error)).join('\n');
+    throw Error(message);
+  }
+
+  return results;
+}
