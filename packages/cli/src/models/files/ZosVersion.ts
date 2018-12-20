da25b615c92430eaf559a2b3eb313f93eefb2178
Alejandro Santander <Palebluedot@gmail.com>
2018-12-20 09:29:43 -0300

    Fixes import problem in ZosVersion, and ports a small file in models/local.

--- packages/cli/src/models/files/ZosVersion.ts
+++ packages/cli/src/models/files/ZosVersion.ts
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 export const ZOS_VERSION = '2';
 
-export function checkVersion(version: string, where: any): boolean | never {
+export default function checkVersion(version: string, where: any): boolean | never {
   if (version === ZOS_VERSION) return true;
   else if (_.isUndefined(version)) {
     throw Error(`zos version identifier not found in ${where}. This means the project was built with an older version of zos (1.x), and needs to be upgraded. Please refer to the documentation at https://docs.zeppelinos.org for more info.`);
