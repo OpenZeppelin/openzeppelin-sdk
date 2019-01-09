1b149aa441f8e8db90b38ffe6515fad40201f397
Facundo Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-01-09 18:22:16 -0300

    Remove custom compiling process with solc (#570)
    * Remove custom compiling process with solc
    
    * Simplify Compiler return type

--- packages/cli/src/models/compiler/Compiler.ts
+++ packages/cli/src/models/compiler/Compiler.ts
@@ -1,36 +1,10 @@
 import { exec } from 'child_process';
 import { FileSystem, Contracts, Logger } from 'zos-lib';
-import Truffle from '../initializer/truffle/Truffle';
-import SolidityProjectCompiler from './solidity/SolidityProjectCompiler';
-import { CompilerOptions } from './solidity/SolidityContractsCompiler';
 
 const log = new Logger('Compiler');
 
-export default {
-  async call(): Promise<{ stdout: string, stderr: string } | void> {
-    return Truffle.isTruffleProject()
-      ? this.compileWithTruffle()
-      : this.compileWithSolc();
-  },
-
-  getSettings(): CompilerOptions {
-    return this.settings || {};
-  },
-
-  setSettings(settings: CompilerOptions): void {
-    this.settings = { ...this.getSettings(), ...settings };
-  },
-
-  async compileWithSolc(): Promise<void> {
-    const inputDir = Contracts.getLocalContractsDir();
-    const outputDir = Contracts.getLocalBuildDir();
-    const options = this.getSettings();
-    const projectCompiler = new SolidityProjectCompiler(inputDir, outputDir, options);
-    log.info('Compiling contracts with solc...');
-    await projectCompiler.call();
-  },
-
-  async compileWithTruffle(): Promise<{ stdout: string, stderr: string }> {
+const Compiler = {
+  async call(): Promise<{ stdout: string, stderr: string }> {
     log.info('Compiling contracts with Truffle...');
     return new Promise((resolve, reject) => {
       exec('npx truffle compile', (err, stdout, stderr) => {
@@ -38,5 +12,7 @@ export default {
         else resolve({ stdout, stderr });
       });
     });
-  },
+  }
 };
+
+export default Compiler;
