c937885bb28722d64051b446b3eb02fcb2c89ac4
Facundo Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-01-13 17:59:37 -0300

    Force compiling all contracts (#581)
    * Check truffle bin before compiling
    
    * Force compiling all contracts

--- packages/cli/src/models/compiler/Compiler.ts
+++ packages/cli/src/models/compiler/Compiler.ts
@@ -6,8 +6,11 @@ const log = new Logger('Compiler');
 const Compiler = {
   async call(): Promise<{ stdout: string, stderr: string }> {
     log.info('Compiling contracts with Truffle...');
+    const truffleBin = `${process.cwd()}/node_modules/.bin/truffle`;
+    if (!FileSystem.exists(truffleBin)) throw new Error(`Could not find truffle in ${truffleBin}. Please make sure you're placed in the right directory.`);
+
     return new Promise((resolve, reject) => {
-      exec(`${process.cwd()}/node_modules/.bin/truffle compile`, (error, stdout, stderr) => {
+      exec(`${truffleBin} compile --all`, (error, stdout, stderr) => {
         if (stdout) console.log(stdout)
         if (stderr) console.error(stderr)
         error ? reject(error) : resolve({ stdout, stderr })
