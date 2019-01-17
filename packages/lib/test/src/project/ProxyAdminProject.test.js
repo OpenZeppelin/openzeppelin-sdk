064145f6cf1b3205602673aee3f8c6ddbe04f1fb
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-01-17 19:46:02 -0300

    Add tests for ProxyAdminProject

--- packages/lib/test/src/project/ProxyAdminProject.test.js
+++ packages/lib/test/src/project/ProxyAdminProject.test.js
@@ -1,24 +1,37 @@
-'use strict'
-require('../../setup')
+'use strict';
+require('../../setup');
 
-import SimpleProject from '../../../src/project/SimpleProject'
+import ProxyAdmin from '../../../src/proxy/ProxyAdmin';
+import ProxyAdminProject from '../../../src/project/ProxyAdminProject';
 import shouldManageProxies from './ProxyProject.behaviour';
 import shouldManageDependencies from './DependenciesProject.behaviour';
 import shouldManageImplementations from './Implementations.behaviour';
-import { noop } from 'lodash';
 import Contracts from '../../../src/artifacts/Contracts';
+import { noop } from 'lodash';
 
 const ImplV1 = Contracts.getFromLocal('DummyImplementation');
 const ImplV2 = Contracts.getFromLocal('DummyImplementationV2');
 
-contract('SimpleProject', function ([_, owner, another]) {
-  const name = 'MyProject'
-  
-  beforeEach('initializing', async function () {
-    this.project = new SimpleProject(name, { from: owner })
-    this.adminAddress = owner
+contract('ProxyAdminProject', function([_, proxyAdminOwner, another]) {
+  const name = 'MyProxyAdminProject'
+  const txParams = { from: proxyAdminOwner };
+
+  beforeEach('initializing', async function() {
+    this.proxyAdminOwner = proxyAdminOwner;
+    this.proxyAdmin = await ProxyAdmin.deploy(txParams);
+    this.adminAddress = this.proxyAdmin.address;
+    this.project = new ProxyAdminProject(name, this.proxyAdmin, { from: proxyAdminOwner })
   });
-  
+
+  describe('class methods', function() {
+    describe('#fetchOrDeploy', function() {
+      it('returns ProxyAdminProject instance', async function() {
+        const project = await ProxyAdminProject.fetchOrDeploy(name, txParams, this.adminAddress);
+        project.should.be.instanceof(ProxyAdminProject)
+      });
+    });
+  });
+
   describe('without setImplementation', function () {
     shouldManageProxies({
       supportsNames: false,
@@ -43,7 +56,7 @@ contract('SimpleProject', function ([_, owner, another]) {
       this.project.implementations.should.not.have.key('DummyImplementation')
     })
   })
-  
+
   shouldManageDependencies();
   shouldManageImplementations();
-})
+});
