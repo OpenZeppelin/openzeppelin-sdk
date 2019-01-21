1aad03c6bdbccf336a8fc95602d4e7fec31937fd
Alejandro J. Santander <palebluedot@gmail.com>
2019-01-21 12:40:17 -0300

    Fix in lib's truffle config file.

--- packages/lib/truffle-config.js
+++ packages/lib/truffle-config.js
@@ -2,12 +2,6 @@ require('ts-node/register')
 
 module.exports = {
   networks: {
-    test: {
-      host: 'test',
-      network_id: '*',
-      gas: 6721975,
-      gasPrice: 100000000000
-    },
     coverage: {
       host: 'localhost',
       network_id: '*',
