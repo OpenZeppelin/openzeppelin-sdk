062acb69e2319485d7e4958ed14085218c2e9add
Santiago Palladino <spalladino@gmail.com>
2019-01-25 18:34:34 -0300

    Fixes in storage layout checks
    Fixes issues with storage layout checks:
    
    1- Certain append operations were not being properly recognized, they were identified as insertions and caused false positives. This was found during the latest deployment of openzeppelin-eth, and added as a regression test. The problem was that the edition distance matrix always preferred a diagonal path with zero cost, even if this meant an accumulated cost higher than taking another paths. This is typically not an issue with levenshtein, since diagonal paths are always optimal, but here we break that invariant as we have free appends.
    
    2- The algorithm was failing if either the initial or target contract had no storage whatsoever. Not only something different from a matrix was being returned, but also the cost of the first row of the matrix was being incorrectly calculated, as appends were not being taken into consideration.
    
    3- The first row of the distance matrix was being incorrectly calculated, since it must add insertion costs up until the point where the value stays constant due to appends.

--- packages/lib/src/validations/Layout.ts
+++ packages/lib/src/validations/Layout.ts
@@ -49,32 +49,30 @@ function levenshtein(originalStorage: StorageInfo[], updatedStorage: StorageInfo
   const a = originalStorage;
   const b = updatedStorage;
 
-  if (a.length === 0) return [[b.length * INSERTION_COST]];
-  if (b.length === 0) return [[a.length * DELETION_COST]];
-
   const matrix: any[] = Array(a.length + 1);
+
+  type CostFunction = (i: number, j: number) => number;
+  const insertionCost : CostFunction = (_i,j) => (j > a.length ? 0 : INSERTION_COST);
+  const diagonalCost : CostFunction = (i,j) => (areEqualFn(a[i-1], b[j-1]) ? 0 : SUBSTITUTION_COST);
+  const deletionCost : CostFunction = (_i,_j) => DELETION_COST;
 
   // increment along the first column of each row
   for (let i = 0; i <= a.length; i++) {
     matrix[i] = Array(b.length + 1);
-    matrix[i][0] = i * DELETION_COST;
+    matrix[i][0] = i * deletionCost(i, 0); 
   }
 
   // increment each column in the first row
-  for (let j = 0; j <= b.length; j++) matrix[0][j] = j * INSERTION_COST;
+  for (let j = 1; j <= b.length; j++) {
+    matrix[0][j] = matrix[0][j-1] + insertionCost(0, j);
+  }
 
   // fill in the rest of the matrix
   for (let i = 1; i <= a.length; i++) {
     for (let j = 1; j <= b.length; j++) {
-      if (areEqualFn(a[i - 1], b[j - 1])) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      }
-      else {
-        const insertionCost = j > a.length ? 0 : INSERTION_COST; // appending is free
-        matrix[i][j] = Math.min(matrix[i - 1][j - 1] + SUBSTITUTION_COST,
-                                matrix[i][j - 1] + insertionCost,
-                                matrix[i - 1][j] + DELETION_COST);
-      }
+      matrix[i][j] = Math.min(matrix[i-1][j-1] + diagonalCost(i,j),
+                              matrix[i][j-1] + insertionCost(i,j), 
+                              matrix[i-1][j] + deletionCost(i,j));
     }
   }
 
