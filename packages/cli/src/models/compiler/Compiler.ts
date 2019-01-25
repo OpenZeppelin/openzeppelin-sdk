aa9e685590fd581de6c6a50d759482d5275f97f6
Santiago Palladino <spalladino@gmail.com>
2019-01-25 17:14:49 -0300

    Use global truffle for compiling (#597)
    When compiling, if there is no local binary for truffle in node_modules, fall back to a global instance. Catch error 127 (file not found) and display a friendlier message.
    
    Fixes #579

--- packages/cli/src/models/compiler/Compiler.ts
+++ packages/cli/src/models/compiler/Compiler.ts
@@ -6,11 +6,19 @@ const log = new Logger('Compiler');
 const Compiler = {
   async call(): Promise<{ stdout: string, stderr: string }> {
     log.info('Compiling contracts with Truffle...');
-    const truffleBin = `${process.cwd()}/node_modules/.bin/truffle`;
-    if (!FileSystem.exists(truffleBin)) throw new Error(`Could not find truffle in ${truffleBin}. Please make sure you're placed in the right directory.`);
+    let truffleBin = `${process.cwd()}/node_modules/.bin/truffle`;
+    if (!FileSystem.exists(truffleBin)) truffleBin = 'truffle'; // Attempt to load global truffle if local was not found
 
     return new Promise((resolve, reject) => {
       exec(`${truffleBin} compile --all`, (error, stdout, stderr) => {
+        if (error) {
+          if (error.code === 127) console.error('Could not find truffle executable. Please install it by running: npm install truffle');
+          reject(error);
+        } else {
+          if (stdout) console.log(stdout);
+          if (stderr) console.error(stderr);
+          resolve({ stdout, stderr });  
+        }
         if (stdout) console.log(stdout);
         if (stderr) console.error(stderr);
         error ? reject(error) : resolve({ stdout, stderr });
