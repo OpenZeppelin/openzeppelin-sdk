33d30639a07300d7c06238490353d7931d0b2f2e
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-01-29 15:22:10 -0300

    Create migration function to migrate proxies The function receives the app address, the proxy admin address and the proxy address and changes the admin of the latter through the changeProxyAdmin function in the deprecated App contract, using encodeCall

---
+++ packages/lib/src/utils/Migrator.ts
@@ -0,0 +1,12 @@
+import Transactions from './Transactions';
+import encodeCall from '../helpers/encodeCall';
+import Logger from '../utils/Logger';
+
+const log: Logger = new Logger('Migrator');
+
+export default async function migrate(appAddress: string, proxyAddress: string, proxyAdminAddress: string, txParams: any = {}): Promise<void> {
+  const data = encodeCall('changeProxyAdmin', ['address', 'address'], [proxyAddress, proxyAdminAddress]);
+  log.info(`Migrating proxy at ${proxyAddress} from ${appAddress} to ${proxyAdminAddress}`)
+  await Transactions.sendRawTransaction(appAddress, data, { ...txParams });
+  log.info(`Proxy migrated successfully`);
+}
