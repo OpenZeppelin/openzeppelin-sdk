b6855d21dc928b113a881bf996bc4f011b5bd712
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-01-30 20:52:43 -0300

    Prompt user to migrate is necessary

---
+++ packages/cli/src/utils/prompt-migration.ts
@@ -0,0 +1,26 @@
+import readline from 'readline';
+import { isLatestZosversion } from '../models/files/ZosVersion';
+
+export async function willMigrateProjectIfNeeded(zosversion): Promise<boolean> {
+  if(!isLatestZosversion(zosversion)) {
+    const response = await new Promise((resolve) => {
+      const prompt = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+      prompt.question(`An old version of the ZeppelinOS project was detected and needs to be migrated to the latest one.\nDo you want to proceed? [y/n] `, (answer) => {
+        prompt.close()
+        resolve(answer)
+      });
+    });
+    if (!isValidResponse(response)) return willMigrateProjectIfNeeded(zosversion);
+    return response === 'y' || response === 'Y';
+  }
+  return true;
+}
+
+
+function isValidResponse(response): boolean {
+  return response == 'y' || response == 'Y' || response == 'n' || response == 'N';
+}
+
