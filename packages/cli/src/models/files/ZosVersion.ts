e8b69533f67e2a5010d48d55a08ff2774de540b8
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-01-30 20:48:26 -0300

    Enhance ZosVersion functions

--- packages/cli/src/models/files/ZosVersion.ts
+++ packages/cli/src/models/files/ZosVersion.ts
@@ -2,35 +2,35 @@ import isUndefined from 'lodash.isundefined';
 
 const CURRENT_ZOS_VERSION = ['2', '2'];
 const [CURRENT_MAJOR_VERSION, CURRENT_MINOR_VERSION] = CURRENT_ZOS_VERSION;
-export const ZOS_VERSION = stringifyCurrentZosVersion();
+export const ZOS_VERSION = stringifyCurrentZosversion();
 
 export function checkVersion(version: string, where: any): boolean | never {
   if (version === ZOS_VERSION) return true;
   else if (isUndefined(version)) {
     throw Error(`zos version identifier not found in ${where}. This means the project was built with an older version of zos (1.x), and needs to be upgraded. Please refer to the documentation at https://docs.zeppelinos.org for more info.`);
-  }
-  else {
+  } else if (!isCurrentMajor(version) && !isCurrentMinor(version)) {
     throw Error(`Unrecognized zos version identifier ${version} found in ${where}. This means the project was built with an unknown version of zos. Please refer to the documentation at https://docs.zeppelinos.org for more info.`);
   }
 }
 
-export function isLatestZosVersion(version: string): boolean {
-  return isUndefined(version) && isCurrentVersion(version);
+export function isLatestZosversion(version: string): boolean {
+  return !isUndefined(version) && isCurrentVersion(version);
 }
 
-function stringifyCurrentZosVersion(): string {
+function stringifyCurrentZosversion(): string {
   return CURRENT_ZOS_VERSION.join('.');
 }
 
 function isCurrentVersion(version: string): boolean {
-  const [major, minor] = version.split('.');
-  return isCurrentMinor(minor) && isCurrentMajor(major);
+  return isCurrentMinor(version) && isCurrentMajor(version);
 }
 
-function isCurrentMinor(minor: string): boolean {
+function isCurrentMinor(version: string): boolean {
+  const [major, minor] = version.split('.');
   return minor === CURRENT_MINOR_VERSION
 }
 
-function isCurrentMajor(major: string): boolean {
+function isCurrentMajor(version: string): boolean {
+  const [major, minor] = version.split('.');
   return major === CURRENT_MAJOR_VERSION;
 }
