56f684b87b5142b060b8f0d4fd2304351a420c85
Alejandro J. Santander <palebluedot@gmail.com>
2019-02-01 13:01:19 -0300

    Progress converting ZosContract.ts into an injector instead of a class.

--- packages/lib/src/artifacts/ZosContract.ts
+++ packages/lib/src/artifacts/ZosContract.ts
@@ -6,14 +6,19 @@ import { TransactionReceipt } from 'web3/types';
 import Contracts, { ZosContractSchema } from './Contracts';
 import _ from 'lodash';
 
-export default class ZosContract {
-  public schema: ZosContractSchema;
+interface ZosContract {
+  [id: string]: any; // TODO: define.
+}
 
-  constructor(schema: ZosContractSchema) {
-    this.schema = schema;
+export default function createZosContract(schema: ZosContractSchema, instance: Contract): ZosContract {
+
+  // Add an `.address` getter to the instance.
+  // TODO: Should be removed when Web3 restores the `.address` getter.
+  function addAddressGetter(anInstance: Contract): void {
+    Object.defineProperty(anInstance, 'address', { get: () => anInstance.options.address });
   }
 
-  public async new(args: any[] = [], options: any = {}): Promise<Contract> {
+  instance.new = async function(args: any[] = [], options: any = {}): Promise<Contract> {
     if(!this.schema.linkedBytecode) throw new Error(`${this.schema.contractName} bytecode contains unlinked libraries.`);
     const contract = ZWeb3.contract(this.schema.abi, null, await Contracts.getDefaultTxParams());
     const self = this;
@@ -27,31 +32,30 @@ export default class ZosContract {
         .on('error', (error) => reject(error))
         .on('receipt', (receipt) => injectedData.deployment.transactionReceipt = receipt)
         .on('transactionHash', (hash) => injectedData.deployment.transactionHash = hash)
-        .then((instance) => {
-          // Inject custom data to the instance.
-          Object.assign(instance, injectedData);
-          // Add an `.address` getter to the instance.
-          // TODO: Should be removed when Web3 restores the `.address` getter.
-          Object.defineProperty(instance, 'address', { get: () => instance.options.address });
-          resolve(instance);
+        .then((deployedInstance) => {
+          Object.assign(deployedInstance, injectedData);
+          addAddressGetter(deployedInstance);
+          resolve(deployedInstance);
         })
         .catch((error) => reject(error));
     });
-  }
+  };
 
-  public at(address: string): Contract | never {
+  instance.at = function(address: string): Contract | never {
     const defaultOptions = Contracts.getArtifactsDefaults();
-    const instance = ZWeb3.contract(this.schema.abi, address, defaultOptions);
-    instance.address = instance.options.address;
-    return instance;
-  }
+    const retrievedInstance = ZWeb3.contract(this.schema.abi, address, defaultOptions);
+    addAddressGetter(retrievedInstance);
+    return retrievedInstance;
+  };
 
-  public link(libraries: { [libAlias: string]: string }): void {
+  instance.link = function(libraries: { [libAlias: string]: string }): void {
     Object.keys(libraries).forEach((name: string) => {
       const address = libraries[name].replace(/^0x/, '');
       const regex = new RegExp(`__${name}_+`, 'g');
       this.schema.linkedBytecode = this.schema.bytecode.replace(regex, address);
       this.schema.linkedDeployedBytecode = this.schema.deployedBytecode.replace(regex, address);
     });
-  }
+  };
+
+  return instance;
 }
