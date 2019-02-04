505399b14c9d1d3e03542ef027b343e67e052a12
Alejandro Santander <Palebluedot@gmail.com>
2019-02-04 13:24:38 -0300

    Parse scientific notation numbers in input.ts. (#567)
    * Parse scientific notation numbers in input.ts.
    
    * Use npx for truffle in lib's scripts (for CI tests to pass).
    
    * Accoung for negative scientific numbers.
    
    * Added bignumber.js dependency to cli.
    
    * Improved regex matching in input.ts.
    
    * Minor change in how dynamic regex-es are built in input.ts.
    
    * Fix web3 to beta 37.
    
    * Remove npx usage in package.json files.

--- packages/cli/src/utils/input.ts
+++ packages/cli/src/utils/input.ts
@@ -1,14 +1,25 @@
+import BN from 'bignumber.js';
+
 export function parseArgs(args: string): string[] | never {
   if (typeof args !== 'string') throw Error(`Cannot parse ${typeof args}`);
 
   // The following is inspired from ethereum/remix (Copyright (c) 2016)
   // https://github.com/ethereum/remix/blob/89f34fc4f35eca0c386379550c300205d35bfae0/remix-lib/src/execution/txFormat.js#L51-L53
   try {
-    const MATCH_HEX = /(^|([,[]\s*))(0[xX][0-9a-fA-F]+)/g;
-    args = args.replace(MATCH_HEX, '$2"$3"'); // replace non quoted hex string by quoted hex string
+    const START_LOOKBEHIND = '(?<=^|([,[]\\s*))'; // Character before match is start of line, comma or opening bracket.
+    const END_LOOKAHEAD = '(?=$|([,\\]]\\s*))'; // Character after match is end of line, comma or closing bracket.
 
-    const MATCH_WORDS = /(^|([,[]\s*))(\w+)/g;
-    args = args.replace(MATCH_WORDS, '$2"$3"'); // replace non quoted number by quoted number
+     // Replace non quoted hex string by quoted hex string.
+    const MATCH_HEX = new RegExp(START_LOOKBEHIND + '(0[xX][0-9a-fA-F]+)'+ END_LOOKAHEAD, 'g');
+    args = args.replace(MATCH_HEX, '"$2"');
+
+    // Replace scientific notation numbers by regular numbers.
+    const MATCH_SCIENTIFIC = new RegExp(START_LOOKBEHIND + '(\\s*[-]?\\d+(\\.\\d+)?e(\\+)?\\d+\\s*)'+ END_LOOKAHEAD, 'g');
+    args = args.replace(MATCH_SCIENTIFIC, (val) => `${(new BN(val)).toString(10)}`);
+
+     // Replace non quoted number by quoted number.
+    const MATCH_WORDS = new RegExp(START_LOOKBEHIND + '([-]?\\w+)'+ END_LOOKAHEAD, 'g');
+    args = args.replace(MATCH_WORDS, '"$2"');
 
     // Parse string to array.
     const parsedArgs = JSON.parse('[' + args + ']');
--- packages/cli/test/utils/input.test.js
+++ packages/cli/test/utils/input.test.js
@@ -14,6 +14,11 @@ describe('input', function () {
     it('should parse a FALSE boolean', testFn("FALSE", false));
     it('should parse a TRUE boolean', testFn("TRUE", true));
     it('should parse a number', testFn("42", "42"));
+    it('should parse a positive scientific notation number', testFn("1e2", "100"));
+    it('should parse a negative scientific notation number', testFn("-1e2", "-100"));
+    it('should parse a consecutive scientific notation numbers', testFn("1e2,2e2", "100", "200"));
+    it('should not parse a scientific notation number followed by non numeric chars as a number', testFn("1e2lala", "1e2lala"));
+    it('should parse another scientific notation number', testFn("1.5e20", "150000000000000000000"));
     it('should parse a string', testFn("foo", 'foo'));
     it('should parse a number+string', testFn("42pepe", "42pepe"));
     it('should parse a quoted string', testFn('"foo"', 'foo'));
@@ -22,10 +27,13 @@ describe('input', function () {
     it('should parse a quoted number+string', testFn('"42foo"', '42foo'));
     it('should parse an array', testFn('[1, 2, 3]', ["1", "2", "3"]));
     it('should parse nested arrays', testFn("[1,[2,3],4]", ["1",["2","3"],"4"]));
-    it('should parse multiple arguments', testFn('42,43,"foo",[1,2,"bar"]', '42', '43', 'foo', ['1', '2', 'bar']));
-    it('should parse an address', testFn("0x1234", "0x1234"));
-    it('should parse a quoted address', testFn('"0x1234"', "0x1234"));
-    it('should parse multiple addresses', testFn('0x1234,0x1235', "0x1234", "0x1235"));
+    it('should parse a hex string', testFn("0x1234", "0x1234"));
+    it('should parse an address', testFn("0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e", "0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e"));
+    it('should not parse something that looks like an address but is followed by non hex chars as an address', testFn("0x39af68cF04Abb0eF8f9d8191E1blalala", "0x39af68cF04Abb0eF8f9d8191E1blalala"));
+    it('should parse a quoted address', testFn('"0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e"', "0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e"));
+    it('should parse multiple addresses', testFn('0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e,0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e', "0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e", "0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e"));
+    it('should parse an array of non quoted addresses', testFn('[0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e,0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e]', ["0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e", "0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e"]));
+    it('should parse multiple arguments', testFn('1e2,42,0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e,43,1.5e20,"foo",[1e2,1,2,"bar",1e3]', '100', '42', '0x39af68cF04Abb0eF8f9d8191E1bD9c041E80e18e', '43', '150000000000000000000', 'foo', ['100', '1', '2', 'bar', '1000']));
   });
 
   describe('parseInit', function () {
