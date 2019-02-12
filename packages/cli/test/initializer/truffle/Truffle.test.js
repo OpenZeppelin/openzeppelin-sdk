d4c275a8e940d62da0fda907c7accb414cd46bc4
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-02-12 14:05:11 -0300

    Ignore Truffle gas default value

--- packages/cli/test/initializer/truffle/Truffle.test.js
+++ packages/cli/test/initializer/truffle/Truffle.test.js
@@ -15,7 +15,6 @@ contract('Truffle', () => {
   })
 
   describe('existsTruffleConfig', function () {
-
     it('returns true when there is a truffle.js file', function () {
       FileSystem.write(`${testDir}/truffle.js`, 'dummy')
 
@@ -81,6 +80,50 @@ contract('Truffle', () => {
         it('returns false', function () {
           Truffle.isTruffleProject(testDir).should.be.false
         })
+      })
+    })
+  })
+
+  describe('getProviderAndDefaults', function () {
+    const configFile = `${process.cwd()}/truffle.js`
+    const configFileBackup = `${configFile}.backup`
+
+    before('backup truffle-config file', function () {
+      FileSystem.copy(configFile, configFileBackup)
+    })
+
+    after('restore truffle-config file', function () {
+      FileSystem.copy(configFileBackup, configFile)
+      FileSystem.remove(configFileBackup)
+    })
+
+    context('when the requested network has default values', function () {
+      beforeEach('create truffle config file', function () {
+        FileSystem.write(configFile, 'module.exports = { networks: { test: { gas: 1, gasPrice: 2, from: \'0x0\' } } }');
+        Truffle.validateAndLoadNetworkConfig('test', true)
+      })
+
+      it('uses network default gas, gas price and from values', function () {
+        const { artifactDefaults } = Truffle.getProviderAndDefaults()
+
+        artifactDefaults.should.have.all.keys('gas', 'gasPrice', 'from')
+        artifactDefaults.gas.should.be.eq(1)
+        artifactDefaults.gasPrice.should.be.eq(2)
+        artifactDefaults.from.should.be.eq('0x0')
+      })
+    })
+
+    context('when the requested network does not have default values', function () {
+      beforeEach('create truffle config file', function () {
+        FileSystem.write(configFile, 'module.exports = { networks: { test: { } } }');
+        Truffle.validateAndLoadNetworkConfig('test', true)
+      })
+
+      it('uses Truffle default gas price', function () {
+        const { artifactDefaults } = Truffle.getProviderAndDefaults()
+
+        artifactDefaults.should.have.all.keys('gasPrice')
+        artifactDefaults.gasPrice.should.be.eq(20000000000)
       })
     })
   })
