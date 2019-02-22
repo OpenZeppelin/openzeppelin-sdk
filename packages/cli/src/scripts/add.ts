1d45b2ee146fd5ffb878e4f6881f57cdb07111bc
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-02-22 19:05:12 -0300

    Refactor in add script

--- packages/cli/src/scripts/add.ts
+++ packages/cli/src/scripts/add.ts
@@ -4,7 +4,6 @@ import { AddParams } from './interfaces';
 
 export default function add({ contractsData, packageFile }: AddParams): void | never {
   if (contractsData.length === 0) throw new Error('At least one contract name must be provided to add.');
-  contractsData = contractsData.map((data) => typeof(data) === 'string' ? { name: data, alias: data } : data);
 
   const controller = ControllerFor(packageFile);
   contractsData.forEach(({ name, alias }) => {
--- packages/cli/src/utils/prompt.ts
+++ packages/cli/src/utils/prompt.ts
@@ -3,18 +3,14 @@ import isEmpty from 'lodash.isempty';
 import Inquirer from 'inquirer';
 import Truffle from '../models/initializer/truffle/Truffle';
 
-interface Prompt {
+interface PromptParams {
   args?: {};
   props?: {};
   defaults?: {};
 }
 
-interface Answers {
-  [key: string]: any;
-}
-
 // TS-TODO: Define a more accurate return type as soon as we know the final structure of it
-export async function promptForArgumentsIfNeeded({ args, defaults, props }: Prompt): Promise<any> {
+export async function promptForArgumentsIfNeeded({ args, defaults, props }: PromptParams): Promise<any> {
   const questions = Object.keys(args)
     .filter((argName) => !args[argName] || isEmpty(args[argName]))
     .map((argName) => promptFor(argName, defaults, props));
