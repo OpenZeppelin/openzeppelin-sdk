09c02101724286655c992605354c49af935dd309
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-02-25 13:39:46 -0300

    Revert changes in add script

--- packages/cli/src/scripts/add.ts
+++ packages/cli/src/scripts/add.ts
@@ -4,6 +4,7 @@ import { AddParams } from './interfaces';
 
 export default function add({ contractsData, packageFile }: AddParams): void | never {
   if (contractsData.length === 0) throw new Error('At least one contract name must be provided to add.');
+  contractsData = contractsData.map((data) => typeof(data) === 'string' ? { name: data, alias: data } : data);
 
   const controller = ControllerFor(packageFile);
   contractsData.forEach(({ name, alias }) => {
