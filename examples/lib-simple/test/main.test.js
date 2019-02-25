931677f11b48e3320c674de5920e3d8b0ba3bf3d
Alejandro J. Santander <palebluedot@gmail.com>
2019-02-25 20:02:38 -0300

    Merged the two test cases in the lib-simple example.

--- examples/lib-simple/test/main.test.js
+++ examples/lib-simple/test/main.test.js
@@ -5,12 +5,9 @@ const main = require('../index.js').main;
 contract('main', function([creatorAddress, initializerAddress]) {
   let instance;
 
-  it('initialize successfully', async function () {
+  it('set and update the value successfully', async function () {
     instance = await main();
     assert.isString(instance.address);
-  })
-  
-  it('set and update the value successfully', async function () {
     const value = await instance.methods.value().call({ from: initializerAddress });
     assert.equal(value, 43);
   })
