6cc56f005e0521f23c18cfd69d7cff07ed1dbd8d
Santiago Palladino <spalladino@gmail.com>
2019-02-26 17:48:11 -0300

    Fix truffle-migrate example (#741)

--- examples/truffle-migrate/migrations/2_deploy_v0.js
+++ examples/truffle-migrate/migrations/2_deploy_v0.js
@@ -13,9 +13,9 @@ async function deploy(options) {
   await create(Object.assign({ contractAlias: 'MyContract', initMethod: 'initialize', initArgs: [42] }, options));
 }
 
-module.exports = function(deployer, network, accounts) {
+module.exports = function(deployer, networkName, accounts) {
   deployer.then(async () => {
-    const { network, txParams } = await ConfigVariablesInitializer.initNetworkConfiguration()({ network, from: accounts[1] })
+    const { network, txParams } = await ConfigVariablesInitializer.initNetworkConfiguration({ network: networkName, from: accounts[1] })
     await deploy({ network, txParams })
   })
 }
