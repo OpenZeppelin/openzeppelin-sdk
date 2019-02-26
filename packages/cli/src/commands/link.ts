7d2a95e126e0f33d9af72a9e55ae7e7ffed4084a
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-02-26 15:01:18 -0300

    Make link and unlink commands interactive

--- packages/cli/src/commands/link.ts
+++ packages/cli/src/commands/link.ts
@@ -1,9 +1,23 @@
 import push from './push';
 import link from '../scripts/link';
+import { promptIfNeeded } from '../utils/prompt';
 
 const name: string = 'link';
 const signature: string = `${name} [dependencies...]`;
 const description: string = 'links project with a list of dependencies each located in its npm package';
+
+const props = {
+  dependencies: {
+    type: 'input',
+    message: 'Provide an EVM-package name and version',
+    default: ['openzeppelin-eth']
+  },
+  installDependencies: {
+    type: 'confirm',
+    message: 'Do you want to install the dependency?',
+    default: true
+  }
+};
 
 const register: (program: any) => any = (program) => program
   .command(signature, undefined, { noHelp: true })
@@ -14,8 +28,13 @@ const register: (program: any) => any = (program) => program
   .action(action);
 
 async function action(dependencies: string[], options: any): Promise<void> {
-  const installDependencies = options.install;
-  await link({ dependencies, installDependencies });
+  const { install: installDependencies, interactive } = options;
+  const promptedArgs = await promptIfNeeded({ args: { dependencies, installDependencies }, props }, interactive);
+  if (promptedArgs.dependencies && typeof promptedArgs.dependencies === 'string') {
+    promptedArgs.dependencies = [promptedArgs.dependencies];
+  }
+
+  await link(promptedArgs);
   await push.tryAction(options);
 }
 
