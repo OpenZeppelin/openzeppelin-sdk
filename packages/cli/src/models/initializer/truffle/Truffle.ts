85f5ecfb354a404eeed70e6c17f6eee4de407391
Levi Morris <siromivel@gmail.com>
2019-03-04 10:21:34 -0700

    Clean up parens on return statement

--- packages/cli/src/models/initializer/truffle/Truffle.ts
+++ packages/cli/src/models/initializer/truffle/Truffle.ts
@@ -75,8 +75,8 @@ const Truffle = {
             let isLibrary = contract.ast ? contract.ast.nodes[1].contractKind === "library" : false;
             const projectDir = buildDir.replace('build/contracts', '');
             return (contract.bytecode.length > 2 &&
-                    contract.sourcePath.indexOf(projectDir) === 0) &&
-                    !isLibrary
+                    contract.sourcePath.indexOf(projectDir) === 0 &&
+                    !isLibrary);
           } else return false;
         })
         .map((name) => name.replace('.json', ''));
