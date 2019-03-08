103b03edbde3e86131d81696dabda6f17c187386
Santiago Palladino <spalladino@gmail.com>
2019-03-08 13:13:58 -0300

    Display validation error stack trace if on verbose

--- packages/cli/src/models/network/NetworkController.ts
+++ packages/cli/src/models/network/NetworkController.ts
@@ -305,7 +305,7 @@ export default class NetworkController {
       contract.schema.storageInfo = getStorageLayout(contract, buildArtifacts);
       return validationPasses(newWarnings);
     } catch (err) {
-      log.error(`Error while validating contract ${contract.schema.contractName}: ${err.message}`);
+      log.error(`Error while validating contract ${contract.schema.contractName}`, err);
       return false;
     }
   }
