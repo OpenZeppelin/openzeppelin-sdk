8ece3a9b26ff8a69ca300e98d4e7018a7aa3e340
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-04-25 19:21:02 -0300

    Avoid looking for parent dir if running zos init Now, we are looking for the root directory for all commands except for init. Also, the custom implementation of `findRootDirectory` has been replaced with find-up

--- packages/cli/src/bin/helpers.ts
+++ packages/cli/src/bin/helpers.ts
@@ -1,14 +1,10 @@
-import fs from 'fs';
+import path from 'path';
+import findUp from 'find-up';
 
-export default function lookForRootDirectory(dirPath: string): string | null {
-  if (dirPath.length === 0) return null;
-  const dirFiles = fs.readdirSync(dirPath);
-
-  if ((dirFiles.includes('package.json') && dirFiles.includes('package-lock.json')) || dirFiles.includes('zos.json')) {
-    return dirPath;
-  } else {
-    const dir = dirPath.split('/');
-    dir.pop();
-    return lookForRootDirectory(dir.join('/'));
-  }
+export default function findRootDirectory(cwd: string): string | null {
+  const filePath = findUp.sync(['package.json', 'zos.json'], { cwd });
+  if (!filePath) return null;
+  const dir = filePath.split(path.sep);
+  dir.pop();
+  return dir.join(path.sep);
 }
