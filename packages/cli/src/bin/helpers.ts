73c3340e19f8b1888ecb4547ad6382389a0287c5
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-04-26 19:00:48 -0300

    Simplified findRootDirectory function

--- packages/cli/src/bin/helpers.ts
+++ packages/cli/src/bin/helpers.ts
@@ -4,7 +4,5 @@ import findUp from 'find-up';
 export default function findRootDirectory(cwd: string): string | null {
   const filePath = findUp.sync(['package.json', 'zos.json'], { cwd });
   if (!filePath) return null;
-  const dir = filePath.split(path.sep);
-  dir.pop();
-  return dir.join(path.sep);
+  return path.dirname(filePath);
 }
