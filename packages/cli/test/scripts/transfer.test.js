33f1ea4ddbc6a35331886b674808b8cfc71dc85f
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-04-29 19:38:52 -0300

    Add more tests

--- packages/cli/test/scripts/transfer.test.js
+++ packages/cli/test/scripts/transfer.test.js
@@ -2,15 +2,16 @@
 require('../setup')
 
 import utils from 'web3-utils';
+import { ZWeb3 } from 'zos-lib';
 
 import transfer from '../../src/scripts/transfer'
 
-contract('transfer script', function(accounts) {
+contract.only('transfer script', function(accounts) {
   const [_, sender, receiver] = accounts.map(utils.toChecksumAddress);
   describe('validations', function() {
     context('when no recipient address is specified', function() {
       it('throws an error', async function() {
-        await transfer({ value: 10, unit: 'ether' }).should.be.rejectedWith('A recipient address must be specified');
+        await transfer({ value: '10', unit: 'ether' }).should.be.rejectedWith('A recipient address must be specified');
       });
     });
 
@@ -22,14 +23,28 @@ contract('transfer script', function(accounts) {
 
     context('when specifying an invalid unit', function() {
       it('throws an error', async function() {
-        await transfer({ to: receiver, value: 10, unit: 'palla' }).should.be.rejectedWith(/Invalid specified unit/);
+        await transfer({ to: receiver, value: '10', unit: 'palla' }).should.be.rejectedWith(/Invalid specified unit/);
       });
     });
   });
 
   describe('transfer of funds', function() {
-    // TODO: test
-    it('does something', function() {
+    context('when exceding the sender account balance', function() {
+      it('throws an error', async function() {
+        await transfer({ to: receiver, value: '1000', unit: 'ether' }).should.be.rejectedWith(/sender doesn't have enough funds to send tx/);
+      });
+    });
+
+    context('when sending a valid amount of ether', function() {
+      it('transfers funds', async function() {
+        (await ZWeb3.getBalance(sender)).should.eq(100e18.toString());
+        (await ZWeb3.getBalance(receiver)).should.eq(100e18.toString());
+
+        await transfer({ from: sender, to: receiver, value: '10', unit: 'ether' });
+
+        (await ZWeb3.getBalance(sender)).should.not.eq(110e18.toString());
+        (await ZWeb3.getBalance(receiver)).should.eq(110e18.toString());
+      });
     });
   });
 });
