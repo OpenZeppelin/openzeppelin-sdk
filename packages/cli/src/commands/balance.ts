a23ad5cb28d89298992baca0271804554ae50518
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-03 17:01:37 -0300

    Improve erc20 flag description

--- packages/cli/src/commands/balance.ts
+++ packages/cli/src/commands/balance.ts
@@ -11,7 +11,7 @@ const register: (program: any) => any = (program) => program
   .command(signature, undefined, { noHelp: true })
   .usage('[options]')
   .description(description)
-  .option('--erc20 <contractAddress>', 'specify an ERC20 token address')
+  .option('--erc20 <contractAddress>', 'specify an ERC20 token address or leave empty to query ETH balance')
   .withNetworkOptions()
   .withNonInteractiveOption()
   .action(action);
