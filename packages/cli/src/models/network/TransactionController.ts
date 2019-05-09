23a1faeda1f5fb8d5a528a67e0de217dd3981e7f
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-09 14:49:26 -0300

    Implement sendTransaction function in TxController to call non-view and non-pure contract methods

--- packages/cli/src/models/network/TransactionController.ts
+++ packages/cli/src/models/network/TransactionController.ts
@@ -1,8 +1,12 @@
-import { Transactions, Logger, ZWeb3, TxParams } from 'zos-lib';
+import { Transactions, Logger, ZWeb3, TxParams, ABI } from 'zos-lib';
 import { isValidUnit, prettifyTokenAmount, toWei, fromWei } from '../../utils/units';
 import { ERC20_PARTIAL_ABI } from '../../utils/constants';
 import { allPromisesOrError } from '../../utils/async';
+import LocalController from '../local/LocalController';
+import ZosPackageFile from '../files/ZosPackageFile';
+import ZosNetworkFile from '../files/ZosNetworkFile';
 
+const { buildCallData, callDescription }  = ABI;
 const log = new Logger('TransactionController');
 
 interface ERC20TokenInfo {
@@ -13,9 +17,15 @@ interface ERC20TokenInfo {
 
 export default class TransactionController {
   public txParams: TxParams;
+  public packageFile: ZosPackageFile;
+  public networkFile: ZosNetworkFile;
 
-  constructor(txParams?: any) {
+  constructor(txParams?: any, network?: string) {
     if (txParams) this.txParams = txParams;
+    if (network) {
+      this.packageFile = new ZosPackageFile();
+      this.networkFile = this.packageFile.networkFile(network);
+    }
   }
 
   public async transfer(to: string, amount: string, unit: string): Promise<void | never> {
@@ -37,6 +47,17 @@ export default class TransactionController {
       const balance = await ZWeb3.getBalance(accountAddress);
       log.info(`Balance: ${fromWei(balance, 'ether')} ETH`);
     }
+  }
+
+  public async sendTransaction(proxyAddress: string, methodName: string, methodArgs: string[]): Promise<any> {
+    const localController = new LocalController(this.packageFile);
+    const { package: packageName, contract: contractName } = this.networkFile.getProxy(proxyAddress);
+    const contract = localController.getContractClass(packageName, contractName).at(proxyAddress);
+    const { method } = buildCallData(contract, methodName, methodArgs);
+
+    log.info(`Calling ${methodName} with: ${callDescription(method, methodArgs)}`);
+    const { transactionHash } = await Transactions.sendTransaction(contract.methods[methodName], methodArgs, this.txParams);
+    log.info(`Function successfully called! Transaction hash: ${transactionHash}`);
   }
 
   private async getTokenInfo(accountAddress: string, contractAddress: string): Promise<ERC20TokenInfo | never> {
