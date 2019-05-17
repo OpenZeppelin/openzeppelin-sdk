5a68c4acfdab95933815c270d02aa94024d6b860
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-17 18:26:05 -0300

    Remove useless question in `zos link` prompt (#890)

--- packages/cli/test/commands/link.test.js
+++ packages/cli/test/commands/link.test.js
@@ -8,10 +8,18 @@ describe('link command', function() {
 
   itShouldParse('should call link script with a list of dependencies and no install', 'link', 'zos link mock-stdlib@1.1.0 mock-stdlib2@1.2.0 --no-install', function(link) {
     const dependencies = ['mock-stdlib@1.1.0', 'mock-stdlib2@1.2.0']
-    link.should.have.been.calledWithExactly({ dependencies, installDependencies: false })
+    link.should.have.been.calledWithExactly({ dependencies, installDependencies: undefined })
   })
 
   itShouldParse('should call push script when passing --push option', 'push', 'zos link mock-stdlib@1.1.0 mock-stdlib2@1.2.0 --push test', function(push) {
-    push.should.have.been.calledWithExactly({ deployProxyAdmin: undefined, deployProxyFactory: undefined, deployDependencies: true, force: undefined, reupload: undefined, network: 'test', txParams: {} })
+    push.should.have.been.calledWithExactly({
+      deployProxyAdmin: undefined,
+      deployProxyFactory: undefined,
+      deployDependencies: true,
+      force: undefined,
+      reupload: undefined,
+      network: 'test',
+      txParams: {}
+    })
   })
 })
