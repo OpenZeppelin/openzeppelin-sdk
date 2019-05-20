d8aa794ee233ba6996a1c83714eab22bd98811c1
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-20 15:21:04 -0300

    Call compile on create command (#894)

--- packages/cli/src/commands/create.ts
+++ packages/cli/src/commands/create.ts
@@ -5,7 +5,7 @@ import add from './add';
 import push from './push';
 import create from '../scripts/create';
 import Session from '../models/network/Session';
-import { parseInit } from '../utils/input';
+import Compiler from '../models/compiler/Compiler';
 import { fromContractFullName } from '../utils/naming';
 import { hasToMigrateProject } from '../prompts/migrations';
 import ConfigVariablesInitializer from '../models/initializer/ConfigVariablesInitializer';
@@ -35,7 +35,9 @@ const register: (program: any) => any = (program) => program
   .action(commandActions);
 
 async function commandActions(contractFullName: string, options: any) {
-  const { init: rawInitMethod } = options;
+  const { init: rawInitMethod, skipCompile } = options;
+  if(!skipCompile) await Compiler.call();
+
   const { network: promptedNewtork, contractFullName: promptedContractFullName } = await promptForCreate(contractFullName, options);
   const { network, txParams } = await ConfigVariablesInitializer.initNetworkConfiguration({ ...options, network: promptedNewtork });
 
