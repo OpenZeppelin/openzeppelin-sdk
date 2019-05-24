41d7a19516810b277987711a61f600fb61e7a6a4
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-24 14:39:03 -0300

    Check hd-wallet-provider version

--- packages/cli/test/initializer/truffle/Truffle.test.js
+++ packages/cli/test/initializer/truffle/Truffle.test.js
@@ -1,11 +1,13 @@
 require('../../setup')
 
+import npm from 'npm-programmatic';
 import sinon from 'sinon'
 
 import { FileSystem } from 'zos-lib'
 import Truffle from '../../../src/models/initializer/truffle/Truffle'
+import CaptureLogs from '../../helpers/captureLogs'
 
-contract('Truffle', () => {
+contract.only('Truffle', () => {;
   const testDir = `${process.cwd()}/test/tmp`
   
   beforeEach('create test dir', function () {
@@ -123,8 +125,8 @@ contract('Truffle', () => {
         Truffle.validateAndLoadNetworkConfig('test', true)
       })
 
-      it('uses network default gas, gas price and from values', function () {
-        const { artifactDefaults } = Truffle.getProviderAndDefaults()
+      it('uses network default gas, gas price and from values', async function () {
+        const { artifactDefaults } = await Truffle.getProviderAndDefaults()
 
         artifactDefaults.should.have.all.keys('gas', 'gasPrice', 'from')
         artifactDefaults.gas.should.be.eq(1)
@@ -139,14 +141,32 @@ contract('Truffle', () => {
         Truffle.validateAndLoadNetworkConfig('test', true)
       })
 
-      it('uses Truffle default gas price', function () {
-        const { artifactDefaults } = Truffle.getProviderAndDefaults()
+      it('uses Truffle default gas price', async function () {
+        const { artifactDefaults } = await Truffle.getProviderAndDefaults()
 
         artifactDefaults.should.have.all.keys('gasPrice')
         artifactDefaults.gasPrice.should.be.eq(20000000000)
       })
     })
-  })
+
+    context('when using truffle-hdwallet-provider', function () {
+      beforeEach(function () {
+        sinon.stub(npm, 'list').resolves(['truffle-hdwallet-provider@0.0.6']);
+        this.logs = new CaptureLogs();
+      });
+
+      afterEach(function () {
+        sinon.restore();
+        this.logs.restore();
+      });
+
+      it('logs a warning message', async function () {
+        await Truffle.getProviderAndDefaults();
+        this.logs.warns.should.have.lengthOf(1);
+        this.logs.warns[0].should.match(/truffle-hdwallet-provider might cause errors when sending transactions/)
+      });
+    });
+  });
 
   describe('getNetworkNamesFromConfig', function () {
     const configFile = `${process.cwd()}/truffle.js`
