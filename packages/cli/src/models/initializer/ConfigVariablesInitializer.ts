6675c366d76c219de3e3acdf40c4361c2151ccea
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-05-28 14:33:15 -0300

    Add ZosConfig#load call in config initializer obj and also, turn it into an object instead of a static class with static methods

--- packages/cli/src/models/initializer/ConfigVariablesInitializer.ts
+++ packages/cli/src/models/initializer/ConfigVariablesInitializer.ts
@@ -8,16 +8,25 @@ export interface NetworkConfig {
   txParams: TxParams;
 }
 
+<<<<<<< HEAD
 export default class ConfigVariablesInitializer {
   public static initStaticConfiguration(): void {
+=======
+const ConfigVariablesInitializer  = {
+  initStaticConfiguration(): void {
+>>>>>>> Add ZosConfig#load call in config initializer obj
     const buildDir = Truffle.getBuildDir();
     Contracts.setLocalBuildDir(buildDir);
-  }
+  },
 
+<<<<<<< HEAD
   public static async initNetworkConfiguration(
     options: any,
     silent?: boolean,
   ): Promise<NetworkConfig> {
+=======
+  async initNetworkConfiguration(options: any, silent?: boolean): Promise<NetworkConfig> {
+>>>>>>> Add ZosConfig#load call in config initializer obj
     this.initStaticConfiguration();
     const { network, from, timeout } = Session.getOptions(options, silent);
     Session.setDefaultNetworkIfNeeded(options.network);
@@ -35,15 +44,17 @@ export default class ConfigVariablesInitializer {
     } = await Truffle.getProviderAndDefaults();
 =======
     if (!network) throw Error('A network name must be provided to execute the requested action.');
+
     let provider, artifactDefaults;
 
-    // these lines could be expanded to support different libraries like embark, ethjs, buidler, etc
-    if (Truffle.existsTruffleConfig() && !ZosConfig.existsZosConfig()) {
+    if (ZosConfig.exists()) {
+      ZosConfig.load(network);
+      ({ provider, artifactDefaults } = ZosConfig.load(network));
+    } else if (Truffle.existsTruffleConfig()) {
       Truffle.validateAndLoadNetworkConfig(network);
       ({ provider, artifactDefaults } = await Truffle.getProviderAndDefaults());
     } else {
-      ZosConfig.load(network);
-      ({ provider, artifactDefaults }  = ZosConfig.load(network));
+      throw Error('Could not find networks.js file, please remember to initialize your project.');
     }
 >>>>>>> First approach of zos-config file
 
@@ -58,4 +69,6 @@ export default class ConfigVariablesInitializer {
     };
     return { network: await ZWeb3.getNetworkName(), txParams };
   }
-}
+};
+
+export default ConfigVariablesInitializer;
