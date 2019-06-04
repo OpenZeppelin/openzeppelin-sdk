686da282fb9c3615b40ab8597c6a488d7d89348f
Santiago Palladino <spalladino@gmail.com>
2019-06-04 17:09:57 -0300

    Remove option to compile with truffle from command line

--- packages/cli/src/commands/compile.ts
+++ packages/cli/src/commands/compile.ts
@@ -11,10 +11,6 @@ const register: (program: any) => any = program =>
     .command(signature, undefined, { noHelp: true })
     .description(description)
     .option(
-      '--using [manager]',
-      'compiler manager to use (options are "zos" or "truffle", defaults to truffle only if truffle.js config file is present)',
-    )
-    .option(
       '--solc-version [version]',
       'version of the solc compiler to use (value is written to configuration file for future runs, defaults to most recent release that satisfies contract pragmas)',
     )
@@ -34,29 +30,18 @@ const register: (program: any) => any = program =>
 
 async function action(options: {
   evmVersion: string;
-  using: string;
   solcVersion: string;
   optimizer: boolean;
   optimizerRuns: string;
 }): Promise<void> {
   const {
     evmVersion,
-    using: manager,
     solcVersion: version,
     optimizer,
     optimizerRuns,
   } = options;
-  if (
-    manager === 'truffle' &&
-    (evmVersion || version || optimizer || optimizerRuns)
-  ) {
-    throw new Error(
-      'Cannot set any compiler settings when compiling with truffle. Please modify your truffle-config file manually and run this command again.',
-    );
-  }
 
   const compilerOptions: ProjectCompilerOptions = {
-    manager,
     evmVersion,
     version,
     optimizer: {
