b1b450ce72759d72eca33931d464e1c812099a45
Santiago Palladino <spalladino@gmail.com>
2019-06-05 19:03:11 -0300

    Handles swarm hash metadata for solidity 0.5.2
    Since Solidity 0.5.2, the swarm hash is included along with the compiler version, CBOR encoded at the end of the runtime bytecode. This handles both metadata versions and removes the whole metadata.
    
    Fixes #844

--- packages/lib/test/src/utils/Bytecode.test.js
+++ packages/lib/test/src/utils/Bytecode.test.js
@@ -2,41 +2,49 @@
 
 require('../../setup');
 
-import { tryRemoveSwarmHash } from '../../../src/utils/Bytecode';
+import { tryRemoveMetadata } from '../../../src/utils/Bytecode';
 
 describe('contracts util functions', function() {
-  describe('tryRemoveSwarmHash', function() {
-    describe('with valid swarm hash', function() {
-      it('removes swarm hash from bytecode', function() {
-        const swarmHash =
-          '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b41b7a2177668e08e1a';
-        const swarmHashWrapper = `a165627a7a72305820${swarmHash}0029`;
-        const bytecode = `0x01234567890abcdef${swarmHashWrapper}`;
+  describe('tryRemoveMetadata', function() {
+    const contractBytecode = '01234567890abcdef';
 
-        tryRemoveSwarmHash(bytecode).should.eq('0x01234567890abcdef');
-      });
+    it('removes metadata from solidity 0.4 bytecode', function() {
+      const swarmHash =
+        '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b41b7a2177668e08e1a';
+      const swarmHashWrapper = `65627a7a72305820${swarmHash}`;
+      const metadata = `a1${swarmHashWrapper}0029`;
+      const bytecode = `0x${contractBytecode}${metadata}`;
+
+      tryRemoveMetadata(bytecode).should.eq(`0x${contractBytecode}`);
     });
 
-    describe('with a swarm hash size different than 32 bytes', function() {
-      it('does not change the bytecode', function() {
-        const invalidSwarmHash =
-          '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b4';
-        const swarmHashWrapper = `27a7a72305820${invalidSwarmHash}0029`;
-        const bytecode = `0x01234567890abcdefa16560029${swarmHashWrapper}`;
+    it('removes metadata from solidity 0.5 bytecode', function() {
+      const swarmHash =
+        '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b41b7a2177668e08e1a';
+      const swarmHashWrapper = `65627a7a72305820${swarmHash}`;
+      const solcVersionWrapper = '64736f6c6343000509';
+      const metadata = `a2${swarmHashWrapper}${solcVersionWrapper}0032`;
+      const bytecode = `0x${contractBytecode}${metadata}`;
 
-        tryRemoveSwarmHash(bytecode).should.eq(bytecode);
-      });
+      tryRemoveMetadata(bytecode).should.eq(`0x${contractBytecode}`);
     });
 
-    describe('with an invalid swarm hash format', function() {
-      it('does not change the bytecode', function() {
-        const swarmHash =
-          '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b41b7a2177668e08e1a';
-        const invalidSwarmHashWrapper = `a165627a7a7230${swarmHash}abab29`;
-        const bytecode = `0x01234567890abcdef${invalidSwarmHashWrapper}`;
+    it('does not change the bytecode if metadata encoding is invalid', function() {
+      const swarmHash =
+        '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b41b7a2177668e08e1a';
+      const invalidSwarmHashWrapper = `a165627a7a72305821${swarmHash}0029`;
+      const bytecode = `0x${contractBytecode}${invalidSwarmHashWrapper}`;
 
-        tryRemoveSwarmHash(bytecode).should.eq(bytecode);
-      });
+      tryRemoveMetadata(bytecode).should.eq(bytecode);
+    });
+
+    it('does not change the bytecode if metadata length is not reliable', function() {
+      const swarmHash =
+        '69b1869ae52f674ffccdd8f6d35de04d578a778e919a1b41b7a2177668e08e1a';
+      const invalidSwarmHashWrapper = `a165627a7a72305821${swarmHash}9929`;
+      const bytecode = `0x${contractBytecode}${invalidSwarmHashWrapper}`;
+
+      tryRemoveMetadata(bytecode).should.eq(bytecode);
     });
   });
 });
