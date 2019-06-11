1f7f0f396bf39faf03b8f0a681f34cfa589db46d
Santiago Palladino <spalladino@gmail.com>
2019-06-11 12:01:24 -0300

    Handle paths with spaces when compiling artifacts
    A project in a path with spaces would fail to lookup any contracts or imports.

---
+++ packages/cli/test/mocks/mock-stdlib with spaces/contracts/GreeterImpl.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+import "./GreeterLib.sol";
+
+contract GreeterImpl {
+  using GreeterLib for string;
+  event Greeting(string greeting);
+
+  function greet(string memory who) public {
+    emit Greeting(greeting(who));
+  }
+
+  function greeting(string memory who) public pure returns (string memory) {
+    return who.wrap();
+  }
+
+  function version() public pure returns (string memory) {
+    return "1.1.0";
+  }
+}
