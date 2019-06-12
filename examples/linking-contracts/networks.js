af5ed0d7bbc684b22f1e090797b385d1bb60dac2
Santiago Palladino <spalladino@gmail.com>
2019-06-12 11:49:54 -0300

    Add first-project example to CI

--- examples/linking-contracts/networks.js
+++ examples/linking-contracts/networks.js
@@ -9,14 +9,5 @@ module.exports = {
       // eslint-disable-next-line @typescript-eslint/camelcase
       network_id: '*',
     },
-    test: {
-      protocol: 'http',
-      host: 'localhost',
-      port: 9555,
-      gas: 5000000,
-      gasPrice: 5e9,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      network_id: '4447',
-    },
   },
 };
