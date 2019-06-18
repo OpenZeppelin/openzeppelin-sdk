8ba92ba9a667435c6de9d3429eb894d58b779b1e
Santiago Palladino <spalladino@gmail.com>
2019-06-18 12:52:37 -0300

    Update packages/lib/src/utils/Solidity.ts

--- packages/lib/src/utils/Solidity.ts
+++ packages/lib/src/utils/Solidity.ts
@@ -1,6 +1,5 @@
 import flatten from 'truffle-flattener';
 
-// TS-TODO: contract might be typed with some sort of web3 typed lib.
 export function flattenSourceCode(
   contractPaths: string[],
   root = process.cwd(),
