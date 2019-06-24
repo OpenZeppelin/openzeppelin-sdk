29c671b28ee55e30344699a67b1d2dfb4b9a0298
Santiago Palladino <spalladino@gmail.com>
2019-06-24 11:26:08 -0300

    Use camelcase for network_id in sample networks.js

--- examples/linking-contracts/networks.js
+++ examples/linking-contracts/networks.js
@@ -6,8 +6,7 @@ module.exports = {
       port: 8545,
       gas: 5000000,
       gasPrice: 5e9,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      network_id: '*',
+      networkId: '*',
     },
   },
 };
