55a8ed27c936cf58bc8852a07c58400e2ab1b754
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-06-28 16:18:53 -0300

    Export admin & implementation slots references

---
+++ packages/lib/src/utils/Constants.ts
@@ -0,0 +1,4 @@
+export const IMPLEMENTATION_LABEL = 'org.openzeppelinupgrades.proxy.implementation';
+export const DEPRECATED_IMPLEMENTATION_LABEL = 'org.zeppelinos.proxy.implementation';
+export const ADMIN_LABEL = 'org.openzeppelinupgrades.proxy.admin';
+export const DEPRECATED_ADMIN_LABEL = 'org.zeppelinos.proxy.admin';
