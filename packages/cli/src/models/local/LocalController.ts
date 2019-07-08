5cfb21ae8c3b3a3ff905fdb85c7f3969be5123e3
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-07-08 20:22:49 -0300

    Remove ZeppelinOS references from strings

--- packages/cli/src/models/local/LocalController.ts
+++ packages/cli/src/models/local/LocalController.ts
@@ -29,7 +29,7 @@ export default class LocalController {
 
   public constructor(projectFile: ProjectFile = new ProjectFile(), init: boolean = false) {
     if (!init && !projectFile.exists()) {
-      throw Error(`ZeppelinOS file ${projectFile.filePath} not found. Run 'zos init' first to initialize the project.`);
+      throw Error(`OpenZeppelin file ${projectFile.filePath} not found. Run 'zos init' first to initialize the project.`);
     }
     this.projectFile = projectFile;
   }
