e35a5d91a1031026163b0d8dd1ea90576103467c
Francisco Giordano <frangio.1@gmail.com>
2019-07-16 17:14:25 -0300

    revert silencing http server

--- packages/docs/scripts/start.sh
+++ packages/docs/scripts/start.sh
@@ -15,4 +15,4 @@ cd ..
 npx concurrently \
   'nodemon -L -e "*" -w ../packages/lib/contracts -w "*.hbs" -x npm run prepare-docs' \
   'env DISABLE_PREPARE_DOCS= nodemon -L -e adoc,yml -i local-playbook.yml openzeppelin-docs/build-local.js' \
-  'http-server -s -c-1 openzeppelin-docs/build/site'
+  'http-server -c-1 openzeppelin-docs/build/site'
