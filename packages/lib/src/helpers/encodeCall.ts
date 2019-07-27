e2249f40addd86bb426f26f75d56fd3817a78d3d
CallMeGwei <34364155+CallMeGwei@users.noreply.github.com>
2019-07-27 18:25:46 -0500

    Update encodeCall helper to use web3 required removal of tests where unsigned ints were expected to fail if given negative numbers, because web3 wraps such numbers around.

--- packages/lib/src/helpers/encodeCall.ts
+++ packages/lib/src/helpers/encodeCall.ts
@@ -1,17 +1,15 @@
-// TODO: Once we migrate to Web3 1.x, we could replace these two dependencies with Web3, since it uses these two under the hood: https://github.com/ethereum/web3.js/blob/1.0/packages/web3-eth-abi/src/index.js
-import { defaultAbiCoder, ParamType } from 'ethers/utils/abi-coder';
-import ZWeb3 from '../artifacts/ZWeb3';
+import abi, { ParamType } from 'web3-eth-abi';
 
 export function encodeParams(types: (string | ParamType)[] = [], rawValues: any[] = []): string {
-  return defaultAbiCoder.encode(types, rawValues);
+  return abi.encodeParameters(types, rawValues);
 }
 
 export default function encodeCall(name: string, types: (string | ParamType)[] = [], rawValues: any[] = []): string {
   const encodedParameters = encodeParams(types, rawValues).substring(2);
-  const signatureHash = ZWeb3.sha3(`${name}(${types.join(',')})`).substring(2, 10);
+  const signatureHash = abi.encodeFunctionSignature(`${name}(${types.join(',')})`).substring(2, 10);
   return `0x${signatureHash}${encodedParameters}`;
 }
 
-export function decodeCall(types: (string | ParamType)[] = [], data: any[] = []): any[] {
-  return defaultAbiCoder.decode(types, data);
+export function decodeCall(types: (string | ParamType)[] = [], data: string = ''): object {
+  return abi.decodeParameters(types, data);
 }
