ef90d3d48e2e9514a48545cee0c6d6eaae9f6df4
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-09-10 18:15:16 -0300

    Check telemetry opt in on local and global files
    Co-authored-by: Francisco Giordano <fran@openzeppelin.com>

--- packages/cli/src/telemetry.ts
+++ packages/cli/src/telemetry.ts
@@ -1,4 +1,16 @@
+import envPaths from 'env-paths';
+import path from 'path';
+import fs from 'fs-extra';
+import inquirer from 'inquirer';
+import ProjectFile from './models/files/ProjectFile';
+
 import ConfigManager from './models/config/ConfigManager';
+
+interface GlobalTelemetry {
+  optIn: boolean;
+  uuid: unknown;
+  salt: unknown;
+}
 
 //TODO: rename
 export async function telemetry(commandName, options) {
@@ -12,5 +24,32 @@ export async function telemetry(commandName, options) {
 }
 
 async function checkOptIn(): Promise<boolean> {
-  return true;
+  const project = new ProjectFile();
+  const localOptIn = project.telemetryOptIn;
+
+  const { data: globalDataDir } = envPaths('openzeppelin-sdk');
+  const globalDataPath = path.join(globalDataDir, 'telemetry.json');
+  let globalOptIn: GlobalTelemetry | undefined = await fs.readJson(globalDataPath).catch(() => undefined);
+
+  if (localOptIn === false) return false;
+
+  if (globalOptIn === undefined) {
+    const { telemetry } = await inquirer.prompt({
+      name: 'telemetry',
+      type: 'confirm',
+      message: 'telemetry?',
+      default: true,
+    });
+
+    globalOptIn = { optIn: telemetry, uuid: 1, salt: 0 };
+    await fs.writeJson(globalDataPath, globalOptIn);
+  }
+
+  if (localOptIn === undefined) {
+    project.telemetryOptIn = globalOptIn.optIn;
+    // Note: following function is sync
+    project.write();
+  }
+
+  return globalOptIn.optIn;
 }
