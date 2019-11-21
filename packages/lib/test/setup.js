86e537341b6710aaf9a7a04109fd839d44103a7b
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-21 17:08:27 -0300

    Remove remnants of old setup and unused coverage

--- packages/lib/test/setup.js
+++ packages/lib/test/setup.js
@@ -11,7 +11,8 @@ import { Loggy } from '../src/utils/Logger';
 Loggy.silent(false);
 Loggy.testing(true);
 ZWeb3.initialize(provider);
-setArtifactDefaults();
+
+Contracts.setArtifactsDefaults({ gas: 6721975, gasPrice: 100000000000 });
 
 require('chai')
   .use(require('chai-as-promised')) // TODO: Remove this dependency
@@ -19,18 +20,3 @@ require('chai')
   .use(helpers.assertions)
   .use(require('sinon-chai'))
   .should();
-
-function setArtifactDefaults() {
-  const DEFAULT_TESTING_TX_PARAMS = {
-    gas: 6721975,
-    gasPrice: 100000000000,
-  };
-
-  const DEFAULT_COVERAGE_TX_PARAMS = {
-    gas: 0xfffffffffff,
-    gasPrice: 0x01,
-  };
-
-  const defaults = process.env.SOLIDITY_COVERAGE ? DEFAULT_COVERAGE_TX_PARAMS : DEFAULT_TESTING_TX_PARAMS;
-  Contracts.setArtifactsDefaults(defaults);
-}
