e49ca0b3d6143c5ad33ab4a5b93c33a6c78ca150
Francisco Giordano <frangio.1@gmail.com>
2019-11-27 20:13:13 -0300

    add network.js to fix tests

---
+++ packages/cli/networks.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    local: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 5000000
+    },
+    testing: {
+      host: 'localhost',
+      network_id: '4447',
+      port: 9555
+    },
+  },
+}
