dac3104d75ff21029b52ba4ad38708a3095cd2e3
Francisco Giordano <frangio.1@gmail.com>
2019-11-27 17:45:51 -0300

    fix duplicate contract

--- packages/lib/contracts/mocks/Invalid.sol
+++ packages/lib/contracts/mocks/Invalid.sol
@@ -58,14 +58,3 @@ contract WithParentWithDelegateCall is WithDelegateCall {
     return "WithParentWithDelegateCall";
   }
 }
-
-contract WithConstructorImplementation {
-  uint256 public value;
-  string public text;
-
-  constructor(uint256 _value, string memory _text) public {
-    require(_value > 0);
-    value = _value;
-    text = _text;
-  }
-}
