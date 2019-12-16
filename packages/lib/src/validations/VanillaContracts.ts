f5e802cb58ac190e1bc8b505432b977625228b6b
Santiago Palladino <spalladino@gmail.com>
2019-12-16 18:04:46 -0300

    Implement suggestions by @frangio

--- packages/lib/src/validations/VanillaContracts.ts
+++ packages/lib/src/validations/VanillaContracts.ts
@@ -3,14 +3,14 @@ import ContractAST from '../utils/ContractAST';
 
 let VANILLA_CONTRACTS = '@openzeppelin/contracts/';
 
-export function importsVanillaContracts(contract: Contract, buildArtifacts?: any): string[] | null {
-  const ast = new ContractAST(contract, buildArtifacts, { nodesFilter: ['ContractDefinition'] });
+export function importsVanillaContracts(contract: Contract, buildArtifacts?: any): string[] | undefined {
+  const ast = new ContractAST(contract, buildArtifacts, { nodesFilter: [] });
   const illegalImports = [...ast.getImports()]
     .filter(i => i.startsWith(VANILLA_CONTRACTS))
     .map(i => i.slice(VANILLA_CONTRACTS.length))
     .map(i => i.replace(/^contracts\//, ''));
 
-  return illegalImports.length > 0 ? illegalImports : null;
+  return illegalImports.length > 0 ? illegalImports : undefined;
 }
 
 // Used for testing purposes;
