c42731bd6488340e92809ab307404986ad412061
Francisco Giordano <frangio.1@gmail.com>
2019-12-26 21:36:15 -0300

    add basic test

--- packages/cli/src/commands/deploy/action.ts
+++ packages/cli/src/commands/deploy/action.ts
@@ -1,3 +1,4 @@
+import { TxParams } from '@openzeppelin/upgrades';
 import { compile } from '../../models/compiler/Compiler';
 import { fromContractFullName } from '../../utils/naming';
 import ConfigManager from '../../models/config/ConfigManager';
@@ -6,11 +7,16 @@ import stdout from '../../utils/stdout';
 import zipWith from 'lodash.zipwith';
 import { MethodArgType } from '../../prompts/prompt';
 import { parseArg } from '../../utils/input';
+import NetworkFile from '../../models/files/NetworkFile';
 
 interface Options {
   network: string;
   skipCompile?: boolean;
   upgradeable?: boolean;
+
+  // The following are not available as CLI flags, and they are only used in tests.
+  networkFile?: NetworkFile;
+  txParams?: TxParams;
 }
 
 export async function preAction(options: Options): Promise<void> {
@@ -24,13 +30,14 @@ export async function action(contractName: string, deployArgs: string[], options
     throw new Error('unimplemented');
   }
 
+  // this is an interactive prompt, so should be handled by the prompts
   // if (!(await hasToMigrateProject(network))) process.exit(0);
 
-  const { network, txParams } = await ConfigManager.initNetworkConfiguration(options);
+  const { network, txParams } = options.txParams ? options : await ConfigManager.initNetworkConfiguration(options);
 
   const { package: packageName, contract: contractAlias } = fromContractFullName(contractName);
 
-  const controller = new NetworkController(network, txParams);
+  const controller = new NetworkController(network, txParams, options.networkFile);
 
   const contract = controller.contractManager.getContractClass(packageName, contractAlias);
   const constructorInputs: MethodArgType[] = contract.schema.abi.find(f => f.type === 'constructor')?.inputs ?? [];
---
+++ packages/cli/test/commands/deploy.test.ts
@@ -0,0 +1,41 @@
+import '../setup';
+
+import { Contracts } from '@openzeppelin/upgrades';
+import { accounts } from '@openzeppelin/test-environment';
+
+// import { should } from 'chai';
+import ProjectFile from '../../src/models/files/ProjectFile';
+import NetworkFile from '../../src/models/files/NetworkFile';
+
+import { action as deploy } from '../../src/commands/deploy/action';
+
+const SimpleNonUpgradeable = Contracts.getFromLocal('SimpleNonUpgradeable');
+
+describe('deploy (action)', function() {
+  const [owner] = accounts;
+
+  const network = 'test';
+  const txParams = { from: owner };
+
+  beforeEach(function() {
+    this.projectFile = new ProjectFile('test/mocks/packages/package-empty.zos.json');
+    this.networkFile = new NetworkFile(this.projectFile, network);
+  });
+
+  it('should deploy a simple contract with no constructor', async function() {
+    const contract = 'SimpleNonUpgradeable';
+    await deploy(contract, [], {
+      network,
+      txParams,
+      networkFile: this.networkFile,
+    });
+    const instances = this.networkFile.getProxies({ contract });
+    instances.should.have.lengthOf(1);
+
+    const instanceAddress = instances[0].address;
+    const instance = SimpleNonUpgradeable.at(instanceAddress);
+    (await instance.methods.answer().call()).should.equal('42');
+  });
+
+  it('should deploy a simple contract with constructor arguments');
+});
