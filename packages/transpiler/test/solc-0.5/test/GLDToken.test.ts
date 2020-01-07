5ebb711791936ea8359fed44b0ff6fda63848067
Igor Yalovoy <igor@yalovoi.net>
2020-01-07 20:57:00 +0800

    Add *.sol files Jest snapshots

--- packages/transpiler/test/solc-0.5/test/GLDToken.test.ts
+++ packages/transpiler/test/solc-0.5/test/GLDToken.test.ts
@@ -7,7 +7,26 @@ const artifacts = fs.readdirSync('./build/contracts/').map(file => {
 
 describe('GLDToken contract', (): void => {
   it('is converted to a valid GLDTokenUpgradable contract', (): void => {
-    const files = transpileContracts(['GLDToken'], artifacts);
-    expect(files.length).toBe(4);
+    const [GLDTokenFile, ERC20DetailedFile, ERC20File, ContextFile] = transpileContracts(['GLDToken'], artifacts);
+
+    expect(GLDTokenFile.source).toMatchSnapshot();
+    expect(GLDTokenFile.fileName).toBe('GLDToken.sol');
+    expect(GLDTokenFile.path).toBe('contracts/GLDTokenUpgradable.sol');
+    expect(GLDTokenFile.contracts).toEqual(['GLDToken']);
+
+    expect(ERC20DetailedFile.source).toMatchSnapshot();
+    expect(ERC20DetailedFile.fileName).toBe('ERC20Detailed.sol');
+    expect(ERC20DetailedFile.path).toBe('@openzeppelin/contracts/token/ERC20/ERC20DetailedUpgradable.sol');
+    expect(ERC20DetailedFile.contracts).toEqual(['ERC20Detailed']);
+
+    expect(ERC20File.source).toMatchSnapshot();
+    expect(ERC20File.fileName).toBe('ERC20.sol');
+    expect(ERC20File.path).toBe('@openzeppelin/contracts/token/ERC20/ERC20Upgradable.sol');
+    expect(ERC20File.contracts).toEqual(['ERC20']);
+
+    expect(ContextFile.source).toMatchSnapshot();
+    expect(ContextFile.fileName).toBe('Context.sol');
+    expect(ContextFile.path).toBe('@openzeppelin/contracts/GSN/ContextUpgradable.sol');
+    expect(ContextFile.contracts).toEqual(['Context']);
   });
 });
