f978d329c0dad77ebe9947439c46386aec89bd05
Santiago Palladino <santiago@openzeppelin.com>
2020-01-14 14:57:34 -0300

    Add url and path properties to network config (#1386)
    * Remove compiler info from network config
    
    * Support URL or fragments in network config

--- packages/cli/test/config/NetworkConfig.test.js
+++ packages/cli/test/config/NetworkConfig.test.js
@@ -21,123 +21,121 @@ describe('NetworkConfig', function() {
 
   afterEach('cleanup files and folders', cleanupfn(tmpDir));
 
-  describe('methods', function() {
-    describe('#initialize', function() {
-      it('creates an empty contracts folder if missing', function() {
-        NetworkConfig.initialize(tmpDir);
+  describe('#initialize', function() {
+    it('creates an empty contracts folder if missing', function() {
+      NetworkConfig.initialize(tmpDir);
 
-        fs.existsSync(contractsDir).should.be.true;
-        fs.readdirSync(contractsDir, 'utf8').should.have.lengthOf(1);
-        fs.readdirSync(contractsDir, 'utf8').should.include('.gitkeep');
-      });
-
-      it('does not create an empty contracts folder if present', function() {
-        fs.mkdirSync(contractsDir, { recursive: true });
-        fs.writeFileSync(`${contractsDir}/Sample.sol`, '');
-        NetworkConfig.initialize(tmpDir);
-
-        fs.existsSync(contractsDir).should.be.true;
-        fs.readdirSync(contractsDir, 'utf8').should.have.lengthOf(1);
-        fs.readdirSync(contractsDir, 'utf8').should.include('Sample.sol');
-      });
-
-      it('creates a networks.js file if missing', function() {
-        NetworkConfig.initialize(tmpDir);
-
-        fs.existsSync(networkConfigPath).should.be.true;
-      });
-
-      it('does not create a networks.js file if present', function() {
-        fs.writeFileSync(networkConfigFile, '');
-        NetworkConfig.initialize(tmpDir);
-
-        fs.readFileSync(networkConfigPath, 'utf8').should.have.lengthOf(0);
-      });
+      fs.existsSync(contractsDir).should.be.true;
+      fs.readdirSync(contractsDir, 'utf8').should.have.lengthOf(1);
+      fs.readdirSync(contractsDir, 'utf8').should.include('.gitkeep');
     });
 
-    describe('#exists', function() {
-      context('when the networks.js file does not exist', function() {
-        it('returns false', function() {
-          NetworkConfig.exists(tmpDir).should.eq(false);
-        });
-      });
+    it('does not create an empty contracts folder if present', function() {
+      fs.mkdirSync(contractsDir, { recursive: true });
+      fs.writeFileSync(`${contractsDir}/Sample.sol`, '');
+      NetworkConfig.initialize(tmpDir);
 
-      context('when the networks.js file exists', function() {
-        it('returns true', function() {
-          NetworkConfig.initialize(tmpDir);
-          NetworkConfig.exists(tmpDir).should.eq(true);
-        });
-      });
+      fs.existsSync(contractsDir).should.be.true;
+      fs.readdirSync(contractsDir, 'utf8').should.have.lengthOf(1);
+      fs.readdirSync(contractsDir, 'utf8').should.include('Sample.sol');
     });
 
-    describe('#getConfig', function() {
-      it('setups the config', function() {
-        NetworkConfig.initialize(tmpDir);
-        const config = NetworkConfig.getConfig(networkConfigDir);
+    it('creates a networks.js file if missing', function() {
+      NetworkConfig.initialize(tmpDir);
 
-        config.should.have.all.keys('networks', 'compilers', 'buildDir');
-        config.should.not.have.key('network');
-        config.buildDir.should.eq(path.resolve(process.cwd(), tmpDir, 'build/contracts'));
-      });
+      fs.existsSync(networkConfigPath).should.be.true;
     });
 
-    describe('#loadNetworkConfig', function() {
-      context('when provided network does not exist', function() {
-        it('throws an error', function() {
-          NetworkConfig.initialize(tmpDir);
-          (() => NetworkConfig.loadNetworkConfig('non-existent', networkConfigDir)).should.throw(
-            /is not defined in your networks.js file/,
-          );
-        });
+    it('does not create a networks.js file if present', function() {
+      fs.writeFileSync(networkConfigFile, '');
+      NetworkConfig.initialize(tmpDir);
+
+      fs.readFileSync(networkConfigPath, 'utf8').should.have.lengthOf(0);
+    });
+  });
+
+  describe('#exists', function() {
+    it('returns false when the networks.js file does not exist', function() {
+      NetworkConfig.exists(tmpDir).should.eq(false);
+    });
+
+    it('returns true when the networks.js file exists', function() {
+      NetworkConfig.initialize(tmpDir);
+      NetworkConfig.exists(tmpDir).should.eq(true);
+    });
+  });
+
+  describe('#getConfig', function() {
+    it('setups the config', function() {
+      NetworkConfig.initialize(tmpDir);
+      const config = NetworkConfig.getConfig(networkConfigDir);
+
+      config.should.have.all.keys('networks', 'buildDir');
+      config.should.not.have.key('network');
+      config.buildDir.should.eq(path.resolve(process.cwd(), tmpDir, 'build/contracts'));
+    });
+  });
+
+  describe('#loadNetworkConfig', function() {
+    afterEach('stubs config', function() {
+      sinon.restore();
+    });
+
+    it('throws an error when provided network does not exist', function() {
+      NetworkConfig.initialize(tmpDir);
+      (() => NetworkConfig.loadNetworkConfig('non-existent', networkConfigDir)).should.throw(
+        /is not defined in your networks.js file/,
+      );
+    });
+
+    it('setups the current selected network config', function() {
+      NetworkConfig.initialize(tmpDir);
+      const config = NetworkConfig.getConfig(networkConfigDir);
+      const networkConfig = NetworkConfig.loadNetworkConfig('development', networkConfigDir);
+
+      networkConfig.provider.should.eq('http://localhost:8545');
+      networkConfig.artifactDefaults.should.include({
+        gas: 5000000,
+        gasPrice: 5000000000,
       });
+      networkConfig.should.deep.include(config);
+    });
 
-      context('when the network exists', function() {
-        it('setups the current selected network config', function() {
-          NetworkConfig.initialize(tmpDir);
-          const config = NetworkConfig.getConfig(networkConfigDir);
-          const networkConfig = NetworkConfig.loadNetworkConfig('development', networkConfigDir);
-
-          networkConfig.provider.should.eq('http://localhost:8545');
-          networkConfig.artifactDefaults.should.include({
-            gas: 5000000,
-            gasPrice: 5000000000,
-          });
-          networkConfig.should.deep.include(config);
-        });
-
-        context('when specifying a diferent provider', function() {
-          afterEach('stubs config', function() {
-            sinon.restore();
-          });
-
-          context('when specifying a function as provider', function() {
-            it('calls the function', function() {
-              const provider = () => 'returned provider';
-              sinon.stub(NetworkConfig, 'getConfig').returns({
-                networks: {
-                  local: { provider, host: 'localhost', port: '1324' },
-                },
-              });
-              NetworkConfig.initialize(tmpDir);
-              const networkConfig = NetworkConfig.loadNetworkConfig('local', networkConfigDir);
-              networkConfig.provider.should.eq('returned provider');
-            });
-          });
-
-          context('when specifying a different protocol', function() {
-            it('setups a different provider', function() {
-              sinon.stub(NetworkConfig, 'getConfig').returns({
-                networks: {
-                  local: { protocol: 'wss', host: 'localhost', port: '1324' },
-                },
-              });
-              NetworkConfig.initialize(tmpDir);
-              const networkConfig = NetworkConfig.loadNetworkConfig('local', networkConfigDir);
-              networkConfig.provider.should.eq('wss://localhost:1324');
-            });
-          });
-        });
+    it('setups a provider from a function', function() {
+      const provider = () => 'returned provider';
+      sinon.stub(NetworkConfig, 'getConfig').returns({
+        networks: { local: { provider, host: 'localhost', port: '1324' } },
       });
+      NetworkConfig.initialize(tmpDir);
+      const networkConfig = NetworkConfig.loadNetworkConfig('local', networkConfigDir);
+      networkConfig.provider.should.eq('returned provider');
+    });
+
+    it('setups a provider from URL parts', function() {
+      sinon.stub(NetworkConfig, 'getConfig').returns({
+        networks: { local: { protocol: 'wss', host: 'localhost', port: '1324', path: 'foo' } },
+      });
+      NetworkConfig.initialize(tmpDir);
+      const networkConfig = NetworkConfig.loadNetworkConfig('local', networkConfigDir);
+      networkConfig.provider.should.eq('wss://localhost:1324/foo');
+    });
+
+    it('setups a provider from just URL host', function() {
+      sinon.stub(NetworkConfig, 'getConfig').returns({
+        networks: { local: { host: 'localhost' } },
+      });
+      NetworkConfig.initialize(tmpDir);
+      const networkConfig = NetworkConfig.loadNetworkConfig('local', networkConfigDir);
+      networkConfig.provider.should.eq('http://localhost');
+    });
+
+    it('setups a provider from plain URL', function() {
+      sinon.stub(NetworkConfig, 'getConfig').returns({
+        networks: { local: { url: 'wss://localhost:1324/foo' } },
+      });
+      NetworkConfig.initialize(tmpDir);
+      const networkConfig = NetworkConfig.loadNetworkConfig('local', networkConfigDir);
+      networkConfig.provider.should.eq('wss://localhost:1324/foo');
     });
   });
 });
