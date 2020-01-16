3ffd905b755a5f77dce970ef778357216dd0568c
Igor Yalovoy <igor@yalovoi.net>
2020-01-16 20:41:31 +0800

    Add Deep.sol test

--- packages/transpiler/test/solc-0.5/test/compile.test.ts
+++ packages/transpiler/test/solc-0.5/test/compile.test.ts
@@ -4,7 +4,7 @@ describe('When all the contracts transpilied and saved to contracts folder', ():
   beforeAll(
     async (): Promise<void> => {
       await transpileAndSaveContracts(
-        ['GLDToken', 'ElementaryTypesWithConstructor', 'ElementaryTypes'],
+        ['GLDToken', 'ElementaryTypesWithConstructor', 'ElementaryTypes', 'Deep'],
         './build/contracts/',
       );
     },
