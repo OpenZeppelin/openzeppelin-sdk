21ee5817dcd2dbca540bfd8391fa0a4e4db153d0
Santiago Palladino <spalladino@gmail.com>
2020-01-28 13:14:55 -0300

    Add failing test for solidity fuzzy parser

---
+++ packages/cli/test/utils/solidity.test.js
@@ -0,0 +1,15 @@
+require('../setup');
+
+import { getImports } from '../../src/utils/solidity';
+
+describe('utils.solidity', function () {
+  describe('getImports', function () {
+    it('returns no imports from empty file', function () {
+      getImports('').should.be.empty;
+    });
+
+    it.skip('returns no imports from commented out file', function () {
+      getImports(`// import "Foo.sol";`).should.be.empty;
+    });
+  });
+});
