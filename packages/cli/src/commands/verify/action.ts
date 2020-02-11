0ffc7c3770a50502aa5ceaafeccf4e421cf063ef
Francisco Giordano <frangio.1@gmail.com>
2020-02-11 16:53:01 -0300

    Add nicer error message to catch verify of regular instances (#1447)
    * add nicer error message to catch verify of regular instances
    
    * Update packages/cli/src/commands/verify/action.ts

--- packages/cli/src/commands/verify/action.ts
+++ packages/cli/src/commands/verify/action.ts
@@ -3,12 +3,20 @@ import { Options, Args } from './spec';
 import ConfigManager from '../../models/config/ConfigManager';
 
 export async function action(params: Options & Args & { dontExitProcess: boolean }): Promise<void> {
+  const userNetworkName = params.network;
+
   if (process.env.NODE_ENV !== 'test') {
     const { network } = await ConfigManager.initNetworkConfiguration(params);
     Object.assign(params, { network });
   }
 
   const controller = new NetworkController(params.network, params.txParams, params.networkFile);
+
+  if (!controller.isContractDeployed(params.contract)) {
+    throw new Error(
+      `Contract '${params.contract}' is not deployed to '${userNetworkName}'.\n\nVerification of regular instances is not yet supported.`,
+    );
+  }
 
   controller.checkLocalContractDeployed(params.contract, true);
 
