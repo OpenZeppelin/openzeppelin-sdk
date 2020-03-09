b2a0e0ce51c4820d47067ac4c8d2c8a349f2dd2a
Igor Yalovoy <to@ylv.io>
2020-03-09 13:01:14 -0300

    Remove create-instances-from-solidity example

--- examples/creating-instances-from-solidity/test/test.js
+++
@@ -1,9 +0,0 @@
-const index = require('../index');
-index();
-
-contract('Creating instances from Solidity', () => {
-  it('should run the main script and receive the expected result value', async () => {
-    const value = await index();
-    assert.equal(value, 42, "final value returned by script is incorrect.");
-  })
-});
