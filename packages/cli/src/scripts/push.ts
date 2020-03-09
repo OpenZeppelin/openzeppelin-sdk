36e3e27533fc0d659df83faeb6f77a8f37827f0f
Igor Yalovoy <to@ylv.io>
2020-03-09 12:58:38 -0300

    Load contracts from Project and Newtork Files for a push script

--- packages/cli/src/scripts/push.ts
+++ packages/cli/src/scripts/push.ts
@@ -1,6 +1,8 @@
 import { transpileAndSave } from '../transpiler';
 import { compile } from '../models/compiler/Compiler';
 
+import ProjectFile from '../models/files/ProjectFile';
+
 import NetworkController from '../models/network/NetworkController';
 import { PushParams } from './interfaces';
 import { fromContractFullName } from '../utils/naming';
@@ -16,13 +18,20 @@ export default async function push({
   txParams = {},
   networkFile,
 }: PushParams): Promise<void | never> {
-  if (!contracts || contracts.length === 0)
-    throw new Error('Push scripts requires at least one contract to be present.');
+  if (!contracts || contracts.length === 0) {
+    if (!!networkFile) {
+      contracts = networkFile.projectFile.contracts;
+    } else {
+      contracts = new ProjectFile().contracts;
+    }
+  }
 
-  // Transpile contract to upgradeable version and save it in contracts folder.
-  await transpileAndSave(contracts);
-  // Compile new contracts.
-  await compile(undefined, undefined, true);
+  if (contracts || contracts.length !== 0) {
+    // Transpile contract to upgradeable version and save it in contracts folder.
+    await transpileAndSave(contracts);
+    // Compile new contracts.
+    await compile(undefined, undefined, true);
+  }
 
   const controller = new NetworkController(network, txParams, networkFile);
 
@@ -37,7 +46,6 @@ export default async function push({
       .map(({ contractName }) => contractName);
 
     await controller.push(projectContracts, { reupload, force });
-    const { appAddress } = controller;
   } finally {
     controller.writeNetworkPackageIfNeeded();
   }
