c7b0847293264fa618fb4d7a2cd861ab7aa7c6a7
Andrew B Coathup <28278242+abcoathup@users.noreply.github.com>
2020-05-16 07:22:50 +1000

    Update docs for OpenZeppelin Contracts Ethereum Package v3.0 (#1536)
    * Update writing-upgradeable.adoc
    
    Update to OpenZeppelin Contracts Ethereum Package 3
    Remove example using App.sol (currently only works in Solidity 0.5)
    Update formatting
    
    * Update dependencies.adoc
    
    Update to OpenZeppelin Contracts Ethereum Package v3.0
    
    Minor issue, when upgrading the Exchange the CLI didn't pick up that there were changes, so I had to compile first.  I added a compile step for this.
    
    * Update README.md
    
    Update doc link
    
    * Update TokenExchange.sol
    
    Update to OpenZeppelin Contracts Ethereum Package v3.0
    
    * Update package.json
    
    Update dependencies
    
    * Update project.json
    
    Update to OpenZeppelin Contracts Ethereum Package v3.0
    
    * Update .gitignore
    
    * Update examples/linking-contracts/contracts/TokenExchange.sol
    
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>
    
    * Update packages/cli/docs/modules/ROOT/pages/dependencies.adoc
    
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>
    
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- examples/linking-contracts/contracts/TokenExchange.sol
+++ examples/linking-contracts/contracts/TokenExchange.sol
@@ -1,37 +1,45 @@
-pragma solidity ^0.5.0;
+// contracts/TokenExchange.sol
+pragma solidity ^0.6.2;
 
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
+// Import base Initializable contract
+import "@openzeppelin/contracts-ethereum-package/Initializable.sol";
 
-// TODO: Update these import paths to @openzeppelin/contracts-ethereum-package once it is released
-import "openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-eth/contracts/math/SafeMath.sol";
+// Import the IERC20 interface and and SafeMath library
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+
 
 contract TokenExchange is Initializable {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  uint256 public rate;
-  IERC20 public token;
+    // Contract state: exchange rate and token
+    uint256 public rate;
+    IERC20 public token;
+    address public owner;
 
-  function initialize(uint256 _rate, IERC20 _token) public initializer {
-    rate = _rate;
-    token = _token;
-  }
+    // Initializer function (replaces constructor)
+    function initialize(uint256 _rate, IERC20 _token) public initializer {
+        rate = _rate;
+        token = _token;
+    }
 
-  function() external payable {
-    uint256 tokens = msg.value.mul(rate);
-    token.transfer(msg.sender, tokens);
-  }
+    // Send tokens back to the sender using predefined exchange rate
+    receive() external payable {
+        uint256 tokens = msg.value.mul(rate);
+        token.transfer(msg.sender, tokens);
+    }
 
-  // Added on subsequent version!
-  address public owner;
+    function withdraw() public {
+        require(
+            msg.sender == owner,
+            "Address not allowed to call this function"
+        );
+        msg.sender.transfer(address(this).balance);
+    }
 
-  function withdraw() public {
-    require(msg.sender == owner);
-    msg.sender.transfer(address(this).balance);
-  }
-
-  function setOwner(address _owner) public {
-    require(owner == address(0));
-    owner = _owner;
-  }
-} 
+    // To be run during upgrade, ensuring it can never be called again
+    function setOwner(address _owner) public {
+        require(owner == address(0), "Owner already set, cannot modify!");
+        owner = _owner;
+    }
+}
