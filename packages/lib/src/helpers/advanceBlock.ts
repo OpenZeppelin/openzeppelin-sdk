fae7cbdafe2e31c5575603f608f4dc575194370b
nebojsa94 <nebojsa94@users.noreply.github.com>
2020-09-02 16:32:41 +0200

    Bump web3 version. (#1581)

--- packages/lib/src/helpers/advanceBlock.ts
+++ packages/lib/src/helpers/advanceBlock.ts
@@ -2,10 +2,15 @@ import ZWeb3 from '../artifacts/ZWeb3';
 
 export default function advanceBlock(): Promise<any> {
   return new Promise((resolve, reject) => {
+    if (typeof ZWeb3.provider == 'string') {
+      return reject('provider not set');
+    }
+
     ZWeb3.provider.send(
       {
         jsonrpc: '2.0',
         method: 'evm_mine',
+        params: [],
         id: Date.now(),
       },
       (err, res) => {
